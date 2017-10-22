8b312a6dd0f1334dbd5f5a2169806c73d5b32464
Hadrien Croubois <hadrien.croubois@gmail.com>
2017-10-22 16:09:32 +0200

    up

--- simulator/stats-profitability-gen.py
+++ simulator/stats-profitability-gen.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-import numpy
+import datetime
+import multiprocessing
+import numpy as np
+import os
+import sys
 
 from argparse   import Namespace
 from statistics import mean
@@ -16,30 +20,42 @@ settings.C_cost    = 1
 settings.C_CR      = lambda x: 0.9
 settings.V_funds   = None          # set by experience
 ###############################################################################
+targets = [0.99, 0.999, 0.9999]
+funds   = [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0]
+runs    = 10000
+sample  = 128
+pwd     = "/home/amxx/Work/iExec/sandbox/simulator/data"
+x       = np.logspace(np.log10(5e-4), np.log10(5e-1), sample)
+###############################################################################
+
+def run(xa): return simulation.SingleRun(scenario=scenari.CoordinatedAttackers(xa), settings=settings).consensus
 
 if __name__ == '__main__':
 
-	targets = [0.99, 0.999, 0.9999]
-	funds   = [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0]
-	runs    = 100000
-	sample  = 64
+	sys.stderr.write("#############################################\n")
+	sys.stderr.write("# Start at %s\n" % datetime.datetime.now().isoformat())
+	sys.stderr.write("# Will use %d cpu\n" % multiprocessing.cpu_count())
+	sys.stderr.write("# ----------------------------------------- #\n")
+	sys.stderr.write("# runs:    %d\n" % runs)
+	sys.stderr.write("# sample:  %d\n" % sample)
+	sys.stderr.write("#############################################\n")
 
-	# ---------------------------------------------------------------------------
-
-	x        = numpy.linspace(0.5, 0.0, sample, endpoint=False)[::-1]
-	x_smooth = numpy.linspace(x.min(), x.max(), 300)
-
-	y_ratio  = numpy.empty(sample)
-	y_count  = numpy.empty((sample, 3))
-	y_profit = { f: numpy.empty((sample,2)) for f in funds }
+	y_ratio  = np.zeros(sample)
+	y_count  = np.zeros((sample, 3))
+	y_profit = { f: np.zeros((sample,2)) for f in funds }
 
 	# ---------------------------------------------------------------------------
 
 	for target in targets:
 		settings.C_target = target
 
+		pool = multiprocessing.Pool()
+
 		for i in range(sample):
-			cns = [ simulation.SingleRun(scenario=scenari.CoordinatedAttackers(x[i]), settings=settings).consensus for _ in range(runs) ]
+			# cns = list(map(run, [ x[i] for _ in range(runs) ]))
+			# cns = list(pool.map(run, [ x[i] for _ in range(runs) ]))
+			cns = list(pool.imap_unordered(run, [ x[i] for _ in range(runs) ]))
+			# cns = [ r.get() for r in [ pool.apply_async(run, [x[i]]) for _ in range(runs) ]]
 
 			y_ratio[i]   = sum (1 for cn in cns if cn.result != 0) / runs
 			y_count[i,0] = mean(len(cn.contribs) for cn in cns)
@@ -62,18 +78,31 @@ if __name__ == '__main__':
 				y_profit[f][i,0] = Wt/Wc
 				y_profit[f][i,1] = At/Ac
 
-		arr_ratio      = numpy.empty((sample,2))
-		arr_ratio[:,0] = x
-		arr_ratio[:,1] = y_ratio
-		numpy.save("data/effectiveness.trg-%f" % (target), arr_ratio)
+			# Snapshot
+			if (sample-i-1)%(sample // 16) == 0:
+				sys.stderr.write("[ SNAPSHOT | %3d/%3d ] target: %f, attackers: %f, runs: %d\n" % (i+1, sample, target, x[i], runs))
 
-		arr_contrib      = numpy.empty((sample,2))
-		arr_contrib[:,0] = x
-		arr_contrib[:,1] = y_count[:,0]
-		numpy.save("data/consensus.trg-%f" % (target), arr_contrib)
+				arr_ratio             = np.empty((i+1,2))
+				arr_ratio[:i+1,0]     = x[:i+1]
+				arr_ratio[:i+1,1]     = y_ratio[:i+1]
+				np.save("%s/effectiveness.trg-%f.checkpoint" % (pwd, target), arr_ratio)
 
+				arr_contrib           = np.empty((i+1,2))
+				arr_contrib[:i+1,0]   = x[:i+1]
+				arr_contrib[:i+1,1]   = y_count[:i+1,0]
+				np.save("%s/consensus.trg-%f.checkpoint" % (pwd, target), arr_contrib)
+
+				for f in funds:
+					arr_profit          = np.empty((i+1,3))
+					arr_profit[:i+1,0 ] = x[:i+1]
+					arr_profit[:i+1,1:] = y_profit[f][:i+1]
+					np.save("%s/profitability.fund-%.2f.trg-%f.checkpoint" % (pwd, f, target), arr_profit)
+
+		# Final
+		os.rename("%s/effectiveness.trg-%f.checkpoint.npy" % (pwd, target),
+		          "%s/effectiveness.trg-%f.npy"            % (pwd, target))
+		os.rename("%s/consensus.trg-%f.checkpoint.npy"     % (pwd, target),
+		          "%s/consensus.trg-%f.npy"                % (pwd, target))
 		for f in funds:
-			arr_profit       = numpy.empty((sample,3))
-			arr_profit[:,0]  = x
-			arr_profit[:,1:] = y_profit[f]
-			numpy.save("data/profitability.fund-%.2f.trg-%f" % (f, target), arr_profit)
+			os.rename("%s/profitability.fund-%.2f.trg-%f.checkpoint.npy" % (pwd, f, target),
+			          "%s/profitability.fund-%.2f.trg-%f.npy"            % (pwd, f, target))
--- simulator/stats-profitability-plot.py
+++ simulator/stats-profitability-plot.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
+
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import ticker
-
-# =============================================================================
 
 colors = [ c['color'] for c in plt.rcParams['axes.prop_cycle'] ]
 
@@ -17,50 +17,56 @@ def endPlot(fname=None):
 		plt.show()
 	else:
 		plt.savefig(fname)
+	plt.close()
 
-# =============================================================================
+###############################################################################
+targets = [0.99, 0.999, 0.9999]
+funds   = [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0]
+pwd_in  = "/home/amxx/Work/iExec/sandbox/simulator/data/10000x128"
+pwd_out = "/home/amxx/Work/iExec/sandbox/simulator/plots"
+###############################################################################
 
 if __name__ == '__main__':
 
-	targets = [0.99, 0.999, 0.9999]
-	funds   = [0.0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0]
 
 	# ---------------------------------------------------------------------------
 
 	for target in targets:
-		data = np.load("data/effectiveness.trg-%f.npy" % (target))
+		data = np.load("%s/effectiveness.trg-%f.npy" % (pwd_in, target))
 		initPlot()
 		plt.plot(data[:,0], data[:,1], color=colors[0])
 		plt.title("Attack effectiveness\n(target credibility: %4.2f%%)" % (target*100))
 		plt.xlabel("Portion of the platform controlled by attackers")
 		plt.ylabel("Attack success rate")
 		plt.xlim(left=data[:,0].min(), right=data[:,0].max())
+		plt.xscale('log')
 		plt.ylim(bottom=0)
 		plt.grid(axis='y', linestyle='--')
-		endPlot("plots/effectiveness.trg-%f.png" % (target))
+		endPlot("%s/effectiveness.trg-%f.png" % (pwd_out, target))
 		# endPlot()
 
 	# ---------------------------------------------------------------------------
 
 	for target in targets:
-		data = np.load("data/consensus.trg-%f.npy" % (target))
+		data = np.load("%s/consensus.trg-%f.npy" % (pwd_in, target))
 		initPlot()
 		plt.plot(data[:,0], data[:,1], color=colors[1])
 		plt.title("Number of contribution to consensus\n(target credibility: %4.2f%%)" % (target*100))
 		plt.xlabel("Portion of the platform controlled by attackers")
 		plt.ylabel("Mean number of votes")
 		plt.xlim(left=data[:,0].min(), right=data[:,0].max())
+		plt.xscale('log')
 		plt.ylim(bottom=0)
 		plt.grid(axis='y', which='major', linestyle='--')
 		plt.grid(axis='y', which='minor', linestyle='--', alpha=0.3)
-		endPlot("plots/consensus.trg-%f.png" % (target))
+		endPlot("%s/consensus.trg-%f.png" % (pwd_out, target))
 		# endPlot()
 
 	# ---------------------------------------------------------------------------
 
 	for target in targets:
 		for f in funds:
-			data = np.load("data/profitability.fund-%.2f.trg-%f.npy" % (f, target))
+			data = np.load("%s/profitability.fund-%.2f.trg-%f.npy" % (pwd_in, f, target))
 			initPlot()
 			plt.plot(data[:,0], data[:,1], label="Good workers", color=colors[2])
 			plt.plot(data[:,0], data[:,2], label="Attackers",    color=colors[3])
@@ -68,6 +74,7 @@ if __name__ == '__main__':
 			plt.xlabel("Portion of the platform controlled by attackers")
 			plt.ylabel("Mean profit per contribution")
 			plt.xlim(left=data[:,0].min(), right=data[:,0].max())
+			plt.xscale('log')
 			sup = np.absolute(data[:,1:]).max()
 			plt.ylim(bottom=-1.2*sup, top=1.2*sup)
 			plt.grid(axis='y', which='major', linestyle='-')
@@ -76,5 +83,5 @@ if __name__ == '__main__':
 			plt.axes().yaxis.set_minor_formatter(plt.axes().yaxis.get_major_formatter())
 			plt.axes().yaxis.set_major_locator(ticker.MultipleLocator(10000000))
 			plt.axes().yaxis.set_major_formatter(ticker.FormatStrFormatter(''))
-			endPlot("plots/profitability.fund-%.2f.trg-%f.png" % (f, target))
+			endPlot("%s/profitability.fund-%.2f.trg-%f.png" % (pwd_out, f, target))
 			# endPlot()
