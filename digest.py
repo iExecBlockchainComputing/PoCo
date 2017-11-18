cabd534219f5c2452062a4b1575d8d3596752292
Hadrien Croubois <hadrien.croubois@gmail.com>
2017-11-17 21:52:31 -0700

    update digest

--- digest.py
+++ digest.py
@@ -3,6 +3,7 @@
 import os
 import hashlib
 
+NONCE_LENGTH = 16
 
 class hash:
 	def _hash(data, nonce): return hashlib.sha256(data+nonce).hexdigest()
@@ -11,31 +12,45 @@ class hash:
 	def file (data, nonce): return hash._hash(open(data, 'rb').read(), nonce)
 
 class digest:
-	def __init__(self, content, split="://"):
+	def __init__(self, content, nonce=None, split="://"):
 		if split not in content: raise ValueError
 		self.content          = content
 		self.type, self.value = content.split(split,1)
-		self.nonce            = os.urandom(256)
-	def voteHash     (self): return getattr(hash, self.type)(data=self.value, nonce=b""       )
-	def signatureHash(self): return getattr(hash, self.type)(data=self.value, nonce=self.nonce)
-	def privateHash  (self): return hash._hash              (data=b"",        nonce=self.nonce)
+		self.nonce            = os.urandom(NONCE_LENGTH) if nonce is None else nonce
+	def voteHash(self): return getattr(hash, self.type)(data=self.value, nonce=b""       )
+	def signHash(self): return getattr(hash, self.type)(data=self.value, nonce=self.nonce)
+	def privHash(self): return hash._hash              (data=b"",        nonce=self.nonce)
 
-def printVote(digest):
-	print("vote:     ", digest.voteHash())
-	print("signature:", digest.signatureHash())
-	print("private:  ", digest.privateHash())
+class contribution:
+	def __init__(self, vote, sign, priv):
+		self.vote = vote
+		self.sign = sign
+		self.priv = priv
+	def make(content):
+		dg = digest(content)
+		tx = contribution(vote=dg.voteHash(), sign=dg.signHash(), priv=dg.privHash())
+		return tx, dg.nonce
+	def check(self, content, nonce):
+		dg = digest(content, nonce)
+		return self.vote == dg.voteHash() \
+		   and self.sign == dg.signHash() \
+		   and self.priv == dg.privHash()
+	def __repr__(self):
+		return "vote: %s\nsign: %s\npriv: %s" % (self.vote, self.sign, self.priv)
 
-def printContribution(digest):
-	print("content:  ", digest.content)
-	print("nonce:    ", digest.nonce)
 
 
 
 if __name__ == '__main__':
 
-	d = digest("file:///home/amxx/todl.txt")
+	content1 = "file:///home/amxx/todl.txt"
+	content2 = "file:///home/amxx/todl2.txt"
 
-	printVote(d)
-	printContribution(d)
+	tx, nonce = contribution.make(content1)
+	valid     = tx.check(content2, nonce)
+
+	print(tx)
+	print(nonce)
+	print(valid)
 
 	pass
