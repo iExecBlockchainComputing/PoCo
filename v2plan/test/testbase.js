a873c4a1e1d09e305ca1a23659655f8e5e31f20b
francois branciard <francois.branciard@iex.ec>
2018-01-19 10:51:54 +0000

    init base test by deploying IexecHub from universalCreator user

--- v2plan/test/testbase.js
+++ v2plan/test/testbase.js
@@ -1,4 +1,9 @@
 var RLC = artifacts.require("../node_modules/rlc-token//contracts/RLC.sol");
+var IexecHub = artifacts.require("./IexecHub.sol");
+var WorkerPoolHub = artifacts.require("./WorkerPoolHub.sol");
+var AppHub = artifacts.require("./AppHub.sol");
+var DatasetHub = artifacts.require("./DatasetHub.sol");
+var TaskRequestHub = artifacts.require("./TaskRequestHub.sol");
 
 const Promise = require("bluebird");
 //extensions.js : credit to : https://github.com/coldice/dbh-b9lab-hackathon/blob/development/truffle/utils/extensions.js
@@ -18,14 +23,16 @@ Extensions.init(web3, assert);
 
 contract('IexecHub', function(accounts) {
 
-  var dappProvider, dappUser, bridge, rlcCreator;
-  var amountGazProvided = 4000000;
+  let scheduler, worker, appProvider, datasetProvider, dappUser, dappProvider, iExecCloudUser, universalCreator;
+  let amountGazProvided = 4000000;
   let isTestRPC;
   let testTimemout = 0;
   let aRLCInstance;
-  let aIexecOracleEscrowInstance;
-  let aFfmpegInstance;
-
+  let aIexecHubInstance;
+  let aWorkerPoolHubInstance;
+  let aAppHubInstance;
+  let aDatasetHubInstance;
+  let aTaskRequestHubInstance;
 
   before("should prepare accounts and check TestRPC Mode", function() {
     assert.isAtLeast(accounts.length, 8, "should have at least 8 accounts");
@@ -36,10 +43,10 @@ contract('IexecHub', function(accounts) {
     dappUser = accounts[4];
     dappProvider = accounts[5];
     iExecCloudUser = accounts[6];
-    rlcCreator = accounts[7];
+    universalCreator = accounts[7];
 
     return Extensions.makeSureAreUnlocked(
-        [scheduler, worker, appProvider, datasetProvider, dappUser, dappProvider,iExecCloudUser])
+        [scheduler, worker, appProvider, datasetProvider, dappUser, dappProvider, iExecCloudUser])
       .then(() => web3.eth.getBalancePromise(scheduler))
       .then(balance => assert.isTrue(
         web3.toWei(web3.toBigNumber(80), "ether").lessThan(balance),
@@ -50,12 +57,12 @@ contract('IexecHub', function(accounts) {
       .then(() => Extensions.refillAccount(scheduler, dappUser, 10))
       .then(() => Extensions.refillAccount(scheduler, dappProvider, 10))
       .then(() => Extensions.refillAccount(scheduler, iExecCloudUser, 10))
-      .then(() => Extensions.refillAccount(scheduler, rlcCreator, 10))
+      .then(() => Extensions.refillAccount(scheduler, universalCreator, 10))
       .then(() => web3.version.getNodePromise())
       .then(node => isTestRPC = node.indexOf("EthereumJS TestRPC") >= 0)
       .then(() => {
         return RLC.new({
-          from: rlcCreator,
+          from: universalCreator,
           gas: amountGazProvided
         });
       })
@@ -64,12 +71,80 @@ contract('IexecHub', function(accounts) {
         console.log("aRLCInstance.address is ");
         console.log(aRLCInstance.address);
         return aRLCInstance.unlock({
-          from: rlcCreator,
+          from: universalCreator,
           gas: amountGazProvided
         });
       })
       .then(txMined => {
         assert.isBelow(txMined.receipt.gasUsed, amountGazProvided, "should not use all gas");
+        return WorkerPoolHub.new({
+          from: universalCreator
+        });
+      })
+      .then(instance => {
+        aWorkerPoolHubInstance = instance;
+        console.log("aWorkerPoolHubInstance.address is ");
+        console.log(aWorkerPoolHubInstance.address);
+        return AppHub.new({
+          from: universalCreator
+        });
+      })
+      .then(instance => {
+        aAppHubInstance = instance;
+        console.log("aAppHubInstance.address is ");
+        console.log(aAppHubInstance.address);
+        return DatasetHub.new({
+          from: universalCreator
+        });
+      })
+      .then(instance => {
+        aDatasetHubInstance = instance;
+        console.log("aDatasetHubInstance.address is ");
+        console.log(aDatasetHubInstance.address);
+        return TaskRequestHub.new({
+          from: universalCreator
+        });
+      })
+      .then(instance => {
+        aTaskRequestHubInstance = instance;
+        console.log("aTaskRequestHubInstance.address is ");
+        console.log(aTaskRequestHubInstance.address);
+        return IexecHub.new(aRLCInstance.address, aWorkerPoolHubInstance.address, aAppHubInstance.address, aDatasetHubInstance.address, aTaskRequestHubInstance.address, {
+          from: universalCreator
+        });
+      })
+      .then(instance => {
+        aIexecHubInstance = instance;
+        console.log("aIexecHubInstance.address is ");
+        console.log(aIexecHubInstance.address);
+        return aWorkerPoolHubInstance.transferOwnership(aIexecHubInstance.address, {
+          from: universalCreator
+        });
+      })
+      .then(txMined => {
+        assert.isBelow(txMined.receipt.gasUsed, amountGazProvided, "should not use all gas");
+        console.log("transferOwnership of WorkerPoolHub to IexecHub");
+        return aAppHubInstance.transferOwnership(aIexecHubInstance.address, {
+          from: universalCreator
+        });
+      })
+      .then(txMined => {
+        assert.isBelow(txMined.receipt.gasUsed, amountGazProvided, "should not use all gas");
+        console.log("transferOwnership of AppHub to IexecHub");
+        return aDatasetHubInstance.transferOwnership(aIexecHubInstance.address, {
+          from: universalCreator
+        });
+      })
+      .then(txMined => {
+        assert.isBelow(txMined.receipt.gasUsed, amountGazProvided, "should not use all gas");
+        console.log("transferOwnership of DatasetHub to IexecHub");
+        return aTaskRequestHubInstance.transferOwnership(aIexecHubInstance.address, {
+          from: universalCreator
+        });
+      })
+      .then(txMined => {
+        assert.isBelow(txMined.receipt.gasUsed, amountGazProvided, "should not use all gas");
+        console.log("transferOwnership of TaskRequestHub to IexecHub")
       });
   });
 
