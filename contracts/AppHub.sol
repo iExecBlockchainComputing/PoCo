400d3c427e6fd7b0fe03181a2bde73bb910231d5
francois branciard <francois.branciard@iex.ec>
2018-05-07 16:32:04 +0200

    #16 add m_totalAppCount m_totalDatasetCount m_totalWorkerPoolCount in Hubs

--- contracts/AppHub.sol
+++ contracts/AppHub.sol
@@ -16,6 +16,9 @@ contract AppHub is OwnableOZ // is Owned by IexecHub
 	mapping(address => mapping(uint256 => address)) m_appByOwnerByIndex;
 	mapping(address => bool)                        m_appRegistered;
 
+	mapping(uint256 => address)                     m_appByIndex;
+	uint256 public                                  m_totalAppCount;
+
 	/**
 	 * Constructor
 	 */
@@ -38,13 +41,19 @@ contract AppHub is OwnableOZ // is Owned by IexecHub
 	{
 		return m_appByOwnerByIndex[_owner][_index];
 	}
+	function getAppByIndex(uint256 _index) public view returns (address)
+	{
+		return m_appByIndex[_index];
+	}
 
 	function addApp(address _owner, address _app) internal
 	{
 		uint id = m_appCountByOwner[_owner].add(1);
-		m_appCountByOwner  [_owner]      = id;
-		m_appByOwnerByIndex[_owner][id]  = _app;
-		m_appRegistered    [_app]        = true;
+		m_totalAppCount=m_totalAppCount.add(1);
+		m_appByIndex       [m_totalAppCount] = _app;
+		m_appCountByOwner  [_owner]          = id;
+		m_appByOwnerByIndex[_owner][id]      = _app;
+		m_appRegistered    [_app]            = true;
 	}
 
 	function createApp(
---
+++ test/byFunctions/AppHub/getAppByIndex.js
@@ -0,0 +1,221 @@
+var RLC = artifacts.require("../node_modules/rlc-token//contracts/RLC.sol");
+var IexecHub = artifacts.require("./IexecHub.sol");
+var WorkerPoolHub = artifacts.require("./WorkerPoolHub.sol");
+var AppHub = artifacts.require("./AppHub.sol");
+var DatasetHub = artifacts.require("./DatasetHub.sol");
+var WorkerPool = artifacts.require("./WorkerPool.sol");
+var Marketplace = artifacts.require("./Marketplace.sol");
+var App = artifacts.require("./App.sol");
+
+const BN = require("bn");
+const keccak256 = require("solidity-sha3");
+const Promise = require("bluebird");
+const fs = require("fs-extra");
+//extensions.js : credit to : https://github.com/coldice/dbh-b9lab-hackathon/blob/development/truffle/utils/extensions.js
+const Extensions = require("../../../utils/extensions.js");
+const addEvmFunctions = require("../../../utils/evmFunctions.js");
+const readFileAsync = Promise.promisify(fs.readFile);
+
+addEvmFunctions(web3);
+Promise.promisifyAll(web3.eth, {
+  suffix: "Promise"
+});
+Promise.promisifyAll(web3.version, {
+  suffix: "Promise"
+});
+Promise.promisifyAll(web3.evm, {
+  suffix: "Promise"
+});
+Extensions.init(web3, assert);
+var constants = require("../../constants");
+
+contract('IexecHub', function(accounts) {
+
+  let scheduleProvider, resourceProvider, appProvider, datasetProvider, dappUser, dappProvider, iExecCloudUser, marketplaceCreator;
+  let isTestRPC;
+  let txMined;
+  let txsMined;
+  let testTimemout = 0;
+  let aRLCInstance;
+  let aIexecHubInstance;
+  let aWorkerPoolHubInstance;
+  let aAppHubInstance;
+  let aDatasetHubInstance;
+  let aMarketplaceInstance;
+
+  beforeEach("should prepare accounts and check TestRPC Mode", async() => {
+    assert.isAtLeast(accounts.length, 8, "should have at least 8 accounts");
+    scheduleProvider = accounts[0];
+    resourceProvider = accounts[1];
+    appProvider = accounts[2];
+    datasetProvider = accounts[3];
+    dappUser = accounts[4];
+    dappProvider = accounts[5];
+    iExecCloudUser = accounts[6];
+    marketplaceCreator = accounts[7];
+
+    await Extensions.makeSureAreUnlocked(
+      [scheduleProvider, resourceProvider, appProvider, datasetProvider, dappUser, dappProvider, iExecCloudUser]);
+    let balance = await web3.eth.getBalancePromise(scheduleProvider);
+    assert.isTrue(
+      web3.toWei(web3.toBigNumber(80), "ether").lessThan(balance),
+      "dappProvider should have at least 80 ether, not " + web3.fromWei(balance, "ether"));
+    await Extensions.refillAccount(scheduleProvider, resourceProvider, 10);
+    await Extensions.refillAccount(scheduleProvider, appProvider, 10);
+    await Extensions.refillAccount(scheduleProvider, datasetProvider, 10);
+    await Extensions.refillAccount(scheduleProvider, dappUser, 10);
+    await Extensions.refillAccount(scheduleProvider, dappProvider, 10);
+    await Extensions.refillAccount(scheduleProvider, iExecCloudUser, 10);
+    await Extensions.refillAccount(scheduleProvider, marketplaceCreator, 10);
+    let node = await web3.version.getNodePromise();
+    isTestRPC = node.indexOf("EthereumJS TestRPC") >= 0;
+    // INIT RLC
+    aRLCInstance = await RLC.new({
+      from: marketplaceCreator,
+      gas: constants.AMOUNT_GAS_PROVIDED
+    });
+    console.log("aRLCInstance.address is ");
+    console.log(aRLCInstance.address);
+    let txMined = await aRLCInstance.unlock({
+      from: marketplaceCreator,
+      gas: constants.AMOUNT_GAS_PROVIDED
+    });
+    assert.isBelow(txMined.receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    txsMined = await Promise.all([
+      aRLCInstance.transfer(scheduleProvider, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      }),
+      aRLCInstance.transfer(resourceProvider, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      }),
+      aRLCInstance.transfer(appProvider, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      }),
+      aRLCInstance.transfer(datasetProvider, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      }),
+      aRLCInstance.transfer(dappUser, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      }),
+      aRLCInstance.transfer(dappProvider, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      }),
+      aRLCInstance.transfer(iExecCloudUser, 1000, {
+        from: marketplaceCreator,
+        gas: constants.AMOUNT_GAS_PROVIDED
+      })
+    ]);
+    assert.isBelow(txsMined[0].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    assert.isBelow(txsMined[1].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    assert.isBelow(txsMined[2].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    assert.isBelow(txsMined[3].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    assert.isBelow(txsMined[4].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    assert.isBelow(txsMined[5].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    assert.isBelow(txsMined[6].receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+    let balances = await Promise.all([
+      aRLCInstance.balanceOf(scheduleProvider),
+      aRLCInstance.balanceOf(resourceProvider),
+      aRLCInstance.balanceOf(appProvider),
+      aRLCInstance.balanceOf(datasetProvider),
+      aRLCInstance.balanceOf(dappUser),
+      aRLCInstance.balanceOf(dappProvider),
+      aRLCInstance.balanceOf(iExecCloudUser)
+    ]);
+    assert.strictEqual(balances[0].toNumber(), 1000, "1000 nRLC here");
+    assert.strictEqual(balances[1].toNumber(), 1000, "1000 nRLC here");
+    assert.strictEqual(balances[2].toNumber(), 1000, "1000 nRLC here");
+    assert.strictEqual(balances[3].toNumber(), 1000, "1000 nRLC here");
+    assert.strictEqual(balances[4].toNumber(), 1000, "1000 nRLC here");
+    assert.strictEqual(balances[5].toNumber(), 1000, "1000 nRLC here");
+    assert.strictEqual(balances[6].toNumber(), 1000, "1000 nRLC here");
+
+    // INIT SMART CONTRACTS BY marketplaceCreator
+    aWorkerPoolHubInstance = await WorkerPoolHub.new({
+      from: marketplaceCreator
+    });
+    console.log("aWorkerPoolHubInstance.address is ");
+    console.log(aWorkerPoolHubInstance.address);
+
+    aAppHubInstance = await AppHub.new({
+      from: marketplaceCreator
+    });
+    console.log("aAppHubInstance.address is ");
+    console.log(aAppHubInstance.address);
+
+
+    // USE marketplaceCreator as owner for test. In real deployement ownership is :
+    /*
+        txMined = await aAppHubInstance.transferOwnership(aIexecHubInstance.address, {
+          from: marketplaceCreator
+        });
+        assert.isBelow(txMined.receipt.gasUsed, constants.AMOUNT_GAS_PROVIDED, "should not use all gas");
+        console.log("transferOwnership of AppHub to IexecHub");
+    */
+
+
+
+  });
+
+
+
+  it("getAppByIndex_01 : is a public function returning the app by index", async function() {
+
+    let createdApp = await aAppHubInstance.createApp.call(
+      "app Name",
+      0,
+      "app Params", {
+        from: marketplaceCreator,
+      });
+
+    m_totalAppCount = await aAppHubInstance.m_totalAppCount.call();
+    assert.strictEqual(m_totalAppCount.toNumber(), 0, "check m_totalAppCount");
+
+    getApp = await aAppHubInstance.getAppByIndex.call(1);
+    assert.notEqual(getApp, createdApp, "check getWorkerPool");
+    assert.strictEqual(getApp, '0x0000000000000000000000000000000000000000', "check 0x0000000000000000000000000000000000000000");
+
+
+    txMined = await aAppHubInstance.createApp(
+      "app Name",
+      0,
+      "app Params", {
+        from: marketplaceCreator,
+      });
+
+    getApp = await aAppHubInstance.getAppByIndex.call(1);
+    assert.strictEqual(getApp, createdApp, "check getApp");
+
+    m_totalAppCount = await aAppHubInstance.m_totalAppCount.call();
+    assert.strictEqual(m_totalAppCount.toNumber(), 1, "check m_totalAppCount");
+
+    let createdApp2 = await aAppHubInstance.createApp.call(
+      "app Name 2",
+      1,
+      "app Params 2", {
+        from: marketplaceCreator,
+      });
+
+    txMined = await aAppHubInstance.createApp(
+      "app Name 2",
+      1,
+      "app Params 2", {
+        from: marketplaceCreator,
+      });
+
+    getApp = await aAppHubInstance.getAppByIndex.call(2);
+    assert.strictEqual(getApp, createdApp2, "check getApp");
+
+    m_totalAppCount = await aAppHubInstance.m_totalAppCount.call();
+    assert.strictEqual(m_totalAppCount.toNumber(), 2, "check m_totalAppCount");
+
+
+  });
+
+
+});
