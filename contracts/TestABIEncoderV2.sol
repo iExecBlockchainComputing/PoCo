f4cd0c85a8c1a2287f4a4e71f8db102af30bc708
Hadrien Croubois <hadrien.croubois@ens-lyon.fr>
2018-05-24 20:50:32 +0200

    update

--- contracts/TestABIEncoderV2.sol
+++ contracts/TestABIEncoderV2.sol
@@ -1,25 +1,14 @@
 pragma solidity ^0.4.21;
 pragma experimental ABIEncoderV2;
 
-contract TestABIEncoderV2
-{
+import "./SafeMathOZ.sol";
+import "./OwnableOZ.sol";
 
-	struct UserMarket
-	{
-		uint256 category;
-		uint256 trust;
-		uint256 value;
-		address app;
-		address dataset;
-		address callback;
-		address beneficiary;
-		address requester;
-		string  params;
-		uint256 salt;
-		uint8   v;
-		bytes32 r;
-		bytes32 s;
-	}
+library Iexec0xLib
+{
+	/**
+	 * Structures
+	 */
 	struct PoolMarket
 	{
 		uint256 category;
@@ -27,16 +16,202 @@ contract TestABIEncoderV2
 		uint256 value;
 		uint256 volume;
 		address workerpool;
-		uint256 salt;
+		bytes32 salt;
+		uint8   v;
+		bytes32 r;
+		bytes32 s;
+	}
+	struct UserMarket
+	{
+		uint256 category;
+		uint256 trust;
+		uint256 value;
+		address app;
+		address dataset;
+		address requester;
+		address beneficiary;
+		address callback;
+		string  params;
+		bytes32 salt;
 		uint8   v;
 		bytes32 r;
 		bytes32 s;
 	}
 
+	enum WorkOrderStatusEnum
+	{
+		UNSET,     // Work order not yet initialized (invalid address)
+		ACTIVE,    // Marketed → constributions are open
+		REVEALING, // Starting consensus reveal
+		CLAIMED,   // Failled consensus
+		COMPLETED  // Concensus achieved
+	}
+
+	enum ContributionStatusEnum
+	{
+		UNSET,
+		AUTHORIZED,
+		CONTRIBUTED,
+		PROVED,
+		REJECTED
+	}
+	struct Contribution
+	{
+		ContributionStatusEnum status;
+		bytes32                resultHash;
+		bytes32                resultSign;
+		address                enclaveChallenge;
+		uint256                score;
+		uint256                weight;
+	}
+}
 
 
 
-	function getUserMarketHash(UserMarket usermarket)
+
+contract WorkOrder
+{
+	using SafeMathOZ for uint256;
+
+	/**
+	 * Market
+	 */
+	Iexec0xLib.WorkOrderStatusEnum status;
+	// common
+	uint256 category;
+	uint256 trust;
+	// workerpool
+	address workerpool;
+	address workerpoolOwner;
+	uint256 workerpoolReward;
+	// app
+	address app;
+	uint256 appReward;
+	// dataset
+	address dataset;
+	uint256 datasetReward;
+	// execution
+	address requester;
+	address beneficiary;
+	address callback;
+	string  params;
+
+	/**
+	 * Worker pool settings
+	 */
+	uint256 workerStakeRatio;
+	uint256 schedulerRewardRatio;
+
+	/**
+	 * Consensus
+	 */
+	mapping(address => Iexec0xLib.Contribution) contributions;
+	address[] contributors;
+
+	bytes32 consensus;
+	uint256 winnerCounter;
+	uint256 revealCounter;
+
+	/**
+	 * Timers
+	 */
+	uint256 consensusDeadline;
+	uint256 revealDeadline;
+
+
+
+
+	modifier onlyWorkerPool
+	{
+		require(msg.sender == workerpool);
+		_;
+	}
+
+	/**
+	 * Constructor
+	 */
+	constructor(
+		Iexec0xLib.PoolMarket _poolmarket,
+		Iexec0xLib.UserMarket _usermarket)
+	public
+	{
+		status           = Iexec0xLib.WorkOrderStatusEnum.UNSET;
+		category         = _poolmarket.category;
+		trust            = _poolmarket.trust;
+		workerpool       = _poolmarket.workerpool;
+		workerpoolOwner  = OwnableOZ(workerpool).m_owner();
+		workerpoolReward = _poolmarket.value;
+		app              = _usermarket.app;
+		/* if (app     != address(0)) { appReward      = App(app).price();         } */
+		dataset          = _usermarket.dataset;
+		/* if (dataset != address(0)) { datasetReward  = Dataset(dataset).price(); } */
+		requester        = _usermarket.requester;
+		beneficiary      = _usermarket.beneficiary;
+		callback         = _usermarket.callback;
+		params           = _usermarket.params;
+	}
+
+	function initialize(
+		uint256 _workerStakeRatio,
+		uint256 _schedulerRewardRatio,
+		uint256 _consensusDeadline)
+	public onlyWorkerPool returns (bool)
+	{
+		require(status == Iexec0xLib.WorkOrderStatusEnum.UNSET);
+		status = Iexec0xLib.WorkOrderStatusEnum.ACTIVE;
+
+		workerStakeRatio     = _workerStakeRatio;
+		schedulerRewardRatio = _schedulerRewardRatio;
+		consensusDeadline    = _consensusDeadline;
+	}
+
+
+
+}
+
+
+
+
+contract TestABIEncoderV2
+{
+	using SafeMathOZ for uint256;
+
+	/**
+	 * Marketplace data
+	 */
+	mapping(bytes32 => uint256) m_consumed;
+
+	uint256 public constant POOL_STAKE_RATIO = 30;
+
+	/**
+	 * Events
+	 */
+	event MarketsMatched  (bytes32 poolHash, bytes32 userHash);
+	event PoolMarketClosed(bytes32 poolHash);
+	event UserMarketClosed(bytes32 userHash);
+
+	/**
+	 * Constructor
+	 */
+	constructor() public
+	{
+	}
+
+	/**
+	 * Hashing and signature tools
+	 */
+	function isValidSignature(
+		address signer,
+		bytes32 hash,
+		uint8   v,
+		bytes32 r,
+		bytes32 s)
+	public pure returns (bool)
+	{
+		return signer == ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash), v, r, s);
+	}
+
+	function getUserMarketHash(Iexec0xLib.UserMarket usermarket)
 	public view returns (bytes32)
 	{
 		return keccak256(
@@ -46,15 +221,15 @@ contract TestABIEncoderV2
 			usermarket.value,
 			usermarket.app,
 			usermarket.dataset,
-			usermarket.callback,
-			usermarket.beneficiary,
 			usermarket.requester,
+			usermarket.beneficiary,
+			usermarket.callback,
 			usermarket.params,
 			usermarket.salt
 		);
 	}
 
-	function getPoolMarketHash(PoolMarket poolmarket)
+	function getPoolMarketHash(Iexec0xLib.PoolMarket poolmarket)
 	public view returns (bytes32)
 	{
 		return keccak256(
@@ -68,27 +243,33 @@ contract TestABIEncoderV2
 		);
 	}
 
-	function isValidSignature(
-		address signer,
-		bytes32 hash,
-		uint8   v,
-		bytes32 r,
-		bytes32 s)
-	public pure returns (bool)
-	{
-		/* return signer == ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash), v, r, s); */
-		return signer == ecrecover(keccak256("\x19Ethereum Signed Message:\n0"), v, r, s);
-	}
-
-
+	/**
+	 * Marketplace methods
+	 */
 	function matchOrders(
-		UserMarket usermarket,
-		PoolMarket poolmarket)
-	public view returns (bool)
+		Iexec0xLib.PoolMarket poolmarket,
+		Iexec0xLib.UserMarket usermarket)
+	public returns (bool)
 	{
-		require(usermarket.category == poolmarket.category);
-		require(usermarket.trust    == poolmarket.trust   );
-		require(usermarket.value    == poolmarket.value   );
+		/**
+		 * Check compatibility
+		 */
+		require(poolmarket.category == usermarket.category);
+		require(poolmarket.trust    == usermarket.trust   );
+		require(poolmarket.value    == usermarket.value   );
+
+		/**
+		 * Check authenticity
+		 */
+		bytes32 poolmarketHash = getPoolMarketHash(poolmarket);
+		require(isValidSignature(
+		// 	OwnableOZ(poolmarket.workerpool).m_owner(), // workerpoolOwner
+			poolmarket.workerpool, // workerpoolOwner
+			poolmarketHash,
+			poolmarket.v,
+			poolmarket.r,
+			poolmarket.s
+		));
 
 		bytes32 usermarketHash = getUserMarketHash(usermarket);
 		require(isValidSignature(
@@ -99,53 +280,79 @@ contract TestABIEncoderV2
 			usermarket.s
 		));
 
+		/**
+		 * Check and update availability
+		 */
+		require(m_consumed[poolmarketHash] <  poolmarket.volume);
+		require(m_consumed[usermarketHash] == 0);
+		m_consumed[poolmarketHash] = m_consumed[poolmarketHash].add(1);
+		m_consumed[usermarketHash] = 1;
+
+		/**
+		 * Lock
+		 */
+		// TODO: lock funds
+
+		emit MarketsMatched(poolmarketHash, usermarketHash);
+		return true;
+	}
+
+	function cancelPoolMarket(Iexec0xLib.PoolMarket poolmarket)
+	public returns (bool)
+	{
+		/**
+		 * Only workerpool owner can cancel
+		 */
+		require(msg.sender == OwnableOZ(poolmarket.workerpool).m_owner());
+
+		/**
+		 * Check authenticity
+		 */
 		bytes32 poolmarketHash = getPoolMarketHash(poolmarket);
 		require(isValidSignature(
-			poolmarket.workerpool, // TODO: workerpoolOwner
+			msg.sender, // workerpool owner
 			poolmarketHash,
 			poolmarket.v,
 			poolmarket.r,
 			poolmarket.s
 		));
 
+		/**
+		 * Cancel market by marking it consumed
+		 */
+		m_consumed[poolmarketHash] = poolmarket.volume;
+
+		emit PoolMarketClosed(poolmarketHash);
 		return true;
 	}
 
-
-
-	/*
-	function verifyUserMarket(UserMarket usermarket)
-	public view returns (bool)
+	function cancelUserMarket(Iexec0xLib.UserMarket usermarket)
+	public returns (bool)
 	{
-		return usermarket.requester == ecrecover(
-			keccak256(
-				"\x19Ethereum Signed Message:\n0"
-				// "\x19Ethereum Signed Message:\n32",
-				// getUserMarketHash(usermarket)
-			),
+		/**
+		 * Only requester can cancel
+		 */
+		require(msg.sender == usermarket.requester);
+
+		/**
+		 * Check authenticity
+		 */
+		bytes32 usermarketHash = getUserMarketHash(usermarket);
+		require(isValidSignature(
+			msg.sender, // requester
+			usermarketHash,
 			usermarket.v,
 			usermarket.r,
 			usermarket.s
-		);
+		));
+
+		/**
+		 * Cancel market by marking it consumed
+		 */
+		m_consumed[usermarketHash] = 1;
+
+		emit UserMarketClosed(usermarketHash);
+		return true;
 	}
-	function verifyPoolMarket(PoolMarket poolmarket)
-	public view returns (bool)
-	{
-		return poolmarket.requester == ecrecover(
-			keccak256(
-				"\x19Ethereum Signed Message:\n0"
-				// "\x19Ethereum Signed Message:\n32",
-				// getPoolMarketHash(usermarket)
-			),
-			poolmarket.v,
-			poolmarket.r,
-			poolmarket.s
-		);
-	}
-	*/
-
-
-
-
 
 }
--- test/000_1_ABIEncoderV2.js
+++ test/000_1_ABIEncoderV2.js
@@ -1,27 +1,20 @@
 var TestABIEncoderV2 = artifacts.require("./TestABIEncoderV2.sol");
 
 const ethers    = require('ethers');
-const web3utils = require('web3-utils');
+// const web3utils = require('web3-utils');
 
-function getUserMarketHash(marketplace, usermarket)
-{
-	return web3utils.soliditySha3(
-		marketplace,
-		usermarket.category,
-		usermarket.trust,
-		usermarket.value,
-		usermarket.app,
-		usermarket.dataset,
-		usermarket.callback,
-		usermarket.beneficiary,
-		usermarket.requester,
-		usermarket.params,
-		usermarket.salt,
-	);
-}
+
 function getPoolMarketHash(marketplace, usermarket)
 {
-	return web3utils.soliditySha3(
+	return ethers.utils.solidityKeccak256([
+		'address',
+		'uint256',
+		'uint256',
+		'uint256',
+		'uint256',
+		'address',
+		'bytes32',
+	],[
 		marketplace,
 		usermarket.category,
 		usermarket.trust,
@@ -29,11 +22,40 @@ function getPoolMarketHash(marketplace, usermarket)
 		usermarket.volume,
 		usermarket.workerpool,
 		usermarket.salt,
-	);
+	]);
+}
+function getUserMarketHash(marketplace, usermarket)
+{
+	return ethers.utils.solidityKeccak256([
+		'address',
+		'uint256',
+		'uint256',
+		'uint256',
+		'address',
+		'address',
+		'address',
+		'address',
+		'address',
+		'string',
+		'bytes32',
+	],[
+		marketplace,
+		usermarket.category,
+		usermarket.trust,
+		usermarket.value,
+		usermarket.app,
+		usermarket.dataset,
+		usermarket.requester,
+		usermarket.beneficiary,
+		usermarket.callback,
+		usermarket.params,
+		usermarket.salt,
+	]);
 }
 function signObject(wallet, object, hash)
 {
-	var sig = ethers.utils.splitSignature(wallet.signMessage(hash));
+	// var sig = ethers.utils.splitSignature(wallet.signMessage(hash));
+	var sig = ethers.utils.splitSignature(web3.eth.sign(wallet, hash));
 	object.r = sig.r;
 	object.s = sig.s;
 	object.v = sig.v;
@@ -43,45 +65,96 @@ function signObject(wallet, object, hash)
 
 
 
-it("TestABIEncoderV2", async function() {
 
+let wallet   = web3.eth.accounts[0];
+var contract = null;
+
+
+
+var poolmarket = {
+	category:    4,
+	trust:       1000,
+	value:       25,
+	volume:      3,
+	workerpool:  wallet, //'0x0000000000000000000000000000000000000000',
+	salt:        ethers.utils.randomBytes(32)
+	// salt:        web3utils.randomHex(32)
+};
+
+var usermarket = {
+	category:    4,
+	trust:       1000,
+	value:       25,
+	app:         '0x0000000000000000000000000000000000000000',
+	dataset:     '0x0000000000000000000000000000000000000000',
+	requester:   wallet,
+	beneficiary: wallet,
+	callback:    '0x0000000000000000000000000000000000000000',
+	params:      "echo HelloWorld",
+	salt:        ethers.utils.randomBytes(32)
+	// salt:        web3utils.randomHex(32)
+};
+
+
+
+
+it("ContractCreate", async function() {
 	instance = await TestABIEncoderV2.new();
 	console.log("instance.address is", instance.address);
-
-	let abi      = JSON.stringify(TestABIEncoderV2.abi);
-	let provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
-	let contract = new ethers.Contract(instance.address, abi, provider);
-	let wallet   = ethers.Wallet.createRandom();
-
-	var usermarket = {
-		category:    4,
-		trust:       1000,
-		value:       25,
-		app:         '0x0000000000000000000000000000000000000000',
-		dataset:     '0x0000000000000000000000000000000000000000',
-		callback:    '0x0000000000000000000000000000000000000000',
-		beneficiary: '0x0000000000000000000000000000000000000000',
-		requester:   wallet.address,
-		params:      "echo HelloWorld",
-		salt:        web3utils.randomHex(32)
-	};
-	// signObject(wallet, usermarket, getUserMarketHash(contract.address, usermarket));
-	signObject(wallet, usermarket, "");
-
-	var poolmarket = {
-		category:    4,
-		trust:       1000,
-		value:       25,
-		volume:      3,
-		workerpool:  wallet.address, //'0x0000000000000000000000000000000000000000',
-		salt:        web3utils.randomHex(32)
-	};
-	// signObject(wallet, poolmarket, getPoolMarketHash(contract.address, poolmarket));
-	signObject(wallet, poolmarket, "");
+	abi                  = JSON.stringify(TestABIEncoderV2.abi);
+	web3.currentProvider = new web3.providers.HttpProvider('http://localhost:8545');
+	provider             = new ethers.providers.Web3Provider(web3.currentProvider);
+	// provider             = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+	contract             = new ethers.Contract(instance.address, abi, provider);
+});
 
 
-	contract.matchOrders(usermarket, poolmarket).then(function(result) {
+
+
+it("sign markets", async function() {
+	signObject(wallet, poolmarket, getPoolMarketHash(contract.address, poolmarket));
+	signObject(wallet, usermarket, getUserMarketHash(contract.address, usermarket));
+});
+
+it("verify hashs", async function() {
+	contract.getPoolMarketHash(poolmarket).then(function(result) {
+		console.log("poolmarket hash:", result);
+		assert.strictEqual(result, getPoolMarketHash(contract.address, poolmarket), "Error with poolmarket hash computation");
+	});
+	contract.getUserMarketHash(usermarket).then(function(result) {
+		console.log("usermarket hash:", result);
+		assert.strictEqual(result, getUserMarketHash(contract.address, usermarket), "Error with usermarket hash computation");
+	});
+});
+
+it("verify signatures", async function() {
+	contract.isValidSignature(
+		wallet,
+		getPoolMarketHash(contract.address, poolmarket),
+		poolmarket.v,
+		poolmarket.r,
+		poolmarket.s
+	).then(function(result) {
+		assert(result, "Error with the validation of the poolmarket signature");
+	});
+	contract.isValidSignature(
+		wallet,
+		getUserMarketHash(contract.address, usermarket),
+		usermarket.v,
+		usermarket.r,
+		usermarket.s
+	).then(function(result) {
+		assert(result, "Error with the validation of the usermarket signature");
+	});
+});
+
+it("make market", async function() {
+	contract.matchOrders(poolmarket, usermarket).then(function(result) {
+		console.log("MatchOrder!");
 		console.log(result);
 	});
-
+	contract.matchOrders(poolmarket, usermarket).then(function(result) {
+		console.log("MatchOrder!");
+		console.log(result);
+	});
 });
