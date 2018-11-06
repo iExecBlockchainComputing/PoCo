766ef7e7663b32c07852f61d8ae8e553d7d4bbd7
francois branciard <francois.branciard@iex.ec>
2018-11-06 16:57:14 +0100

    WIP test callback

--- test/callback/deployIexecAPI.js
+++ test/callback/deployIexecAPI.js
@@ -51,7 +51,7 @@ async function run() {
     .on('receipt', function(receipt){
        console.log(receipt.contractAddress) // contains the new contract address
     })
-    .on('confirmation', function(confirmationNumber, receipt){ ... })
+    .on('confirmation', function(confirmationNumber, receipt){  console.log("confirmation");  console.log(confirmation) })
     .then(function(newContractInstance){
         console.log(newContractInstance.options.address) // instance with the new contract address
     });
