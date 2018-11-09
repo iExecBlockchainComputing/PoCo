e11fa8cf837f9909a38d5918a13c9ff4b753aa8e
francois branciard <francois.branciard@iex.ec>
2018-11-09 16:21:17 +0100

    WIP callbacl test

--- test/callback/approveIexecHub.js
+++ test/callback/approveIexecHub.js
@@ -19,7 +19,7 @@ Promise.promisifyAll(web3.eth, {
 async function getAbiContent() {
   try {
     var abiFileContent = await readFileAsync("../../deployed/contracts/IexecAPI.json");
-    return JSON.parse(JSON.stringify(abiFileContent)).abi;
+    return JSON.parse(abiFileContent);
   } catch (err) {
     console.error(err)
   }
@@ -29,10 +29,11 @@ async function run() {
   try {
     var abi = await getAbiContent();
     console.log("abi");
-    console.log(abi);
-    console.log("JSON.parse(abi)");
-    console.log(JSON.parse(abi));
-    var contract = new web3.eth.Contract(JSON.parse(abi.abi), SMART_CONTRACT_ADDRESS);
+    var abiString =JSON.stringify(abi);
+    console.log(abiString);
+    console.log("abiString.abi");
+    console.log(abiString.abi);
+    var contract = new web3.eth.Contract(abiString.abi, SMART_CONTRACT_ADDRESS);
     //var balanceOfBefore = await contract.methods.balanceOf(MSG_SENDER).call();
     //console.log("MSG_SENDER [" + MSG_SENDER + "] balanceOf before is [" + balanceOfBefore + "]");
 
