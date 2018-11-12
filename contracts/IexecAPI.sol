5d10f44bfd5f85c5ee787ee3fc34f8b3a1f42d29
francois branciard <francois.branciard@iex.ec>
2018-11-12 17:38:00 +0100

    WIP callback test

--- contracts/IexecAPI.sol
+++ contracts/IexecAPI.sol
@@ -33,9 +33,8 @@ contract IexecAPI is OwnableOZ, IexecHubAccessor, IexecCallbackInterface
 		address _beneficiary)
 	public
 	{
-		//address woid = iexecHubInterface.buyForWorkOrder(_marketorderIdx, _workerpool, _app, _dataset, _params, _callback, _beneficiary);
-		//emit WorkOrderActivated(woid);
-		emit WorkOrderActivated(_workerpool);
+		address woid = iexecHubInterface.buyForWorkOrder(2, '0xecb64b809257138dbedc41d45bde27fa323016a2', '0x88f29bef874957012ed55fd4968c296c9e4ec69e', '0x0000000000000000000000000000000000000000', "ace", '0x0000000000000000000000000000000000000000', '0x8bd535d49b095ef648cd85ea827867d358872809');
+		emit WorkOrderActivated(woid);
 	}
 
 	function workOrderCallback(
