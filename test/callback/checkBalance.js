37acdb650d27017fb13ad27339bab704a402f914
francois branciard <francois.branciard@iex.ec>
2018-11-13 15:50:33 +0100

    WIP test callback

--- test/callback/checkBalance.js
+++ test/callback/checkBalance.js
@@ -7,7 +7,7 @@ const readFileAsync = Promise.promisify(fs.readFile);
 const writeFileAsync = Promise.promisify(fs.writeFile);
 
 var MSG_SENDER = process.argv[2] || "0x8bd535d49b095ef648cd85ea827867d358872809";
-var SMART_CONTRACT_ADDRESS = process.argv[3] || "0xeae99b010f8b8852ab47ba883f4c5157633c5ac6";
+var SMART_CONTRACT_ADDRESS = process.argv[3] || "0xc4e4a08bf4c6fd11028b714038846006e27d7be8";
 var NODE_TARGET = process.argv[3] || "http://localhost:8545";
 
 web3 = new Web3(new Web3.providers.HttpProvider(NODE_TARGET));
