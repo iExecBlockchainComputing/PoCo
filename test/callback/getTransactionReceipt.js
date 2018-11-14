4dbf71e2e3c13f15f60e79eb754a47cafb96aeae
francois branciard <francois.branciard@iex.ec>
2018-11-14 10:30:01 +0100

    WIP test callback

--- test/callback/getTransactionReceipt.js
+++ test/callback/getTransactionReceipt.js
@@ -6,7 +6,7 @@ const writeAsync = Promise.promisify(fs.write);
 const readFileAsync = Promise.promisify(fs.readFile);
 const writeFileAsync = Promise.promisify(fs.writeFile);
 
-var MSG_SENDER = process.argv[2] || "0x8bd535d49b095ef648cd85ea827867d358872809";
+
 var TX = process.argv[2] || '0x7e5dbb8378272faa7f3112fee66c9e1f04151fe44cdc574ef937d42210760f78';
 var NODE_TARGET = process.argv[3] || "http://localhost:8545";
 
@@ -19,15 +19,16 @@ Promise.promisifyAll(web3.eth, {
 
 async function run() {
   try {
-
-    var receipt = web3.eth.getTransactionReceipt(TX)
-    .then( tx =>{
-      console.log("tx");
-      console.log(tx);
-      console.log("JSON.stringify(tx);");
-      console.log(JSON.stringify(tx));
-    });
-
+    console.log("getTransactionReceiptPromise for :"+TX);
+    var receipt = await web3.eth.getTransactionReceiptPromise(TX);
+    if (receipt == null) {
+      console.log("receipt is null");
+    } else {
+      console.log("receipt");
+      console.log(receipt);
+      console.log("JSON.stringify(receipt);");
+      console.log(JSON.stringify(receipt));
+    };
   } catch (err) {
     console.error(err);
     process.exit(1);
