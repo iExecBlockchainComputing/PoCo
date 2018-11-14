9d5cef0d1270e84fd41262a09f5cd97b9e4f5912
francois branciard <francois.branciard@iex.ec>
2018-11-14 21:23:04 +0100

    WIP test callback

--- test/callbackweb3old/buyForWorkOrder.js
+++ test/callbackweb3old/buyForWorkOrder.js
@@ -36,7 +36,7 @@ async function run() {
     var workerpool="0xecb64b809257138dbedc41d45bde27fa323016a2";
     var app="0x88f29bef874957012ed55fd4968c296c9e4ec69e";
     var dataset="0x0000000000000000000000000000000000000000";
-    var params="10";
+    var params='{"cmdline": "10"}';
     var callback='0xeae99b010f8b8852ab47ba883f4c5157633c5ac6';//IexecAPI contract
     //var callback="0x0000000000000000000000000000000000000000";
     var beneficiary=MSG_SENDER;
