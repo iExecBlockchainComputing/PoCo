2a7532f26bb65eec25b9e760797d17872d56f11f
Hadrien Croubois <hc@iex.ec>
2018-11-19 10:46:15 +0100

    remove duplicated contract

--- contracts/permissions/UnionGroup.sol
+++ contracts/permissions/UnionGroup.sol
@@ -29,31 +29,3 @@ contract UnionGroup is GroupInterface, OwnableMutable
 		return permissions;
 	}
 }
-
-contract IntersectionGroup is GroupInterface, OwnableMutable
-{
-	GroupInterface[] public m_subgroups;
-
-	/**
-	 * Constructor
-	 */
-	constructor() public {}
-
-	function setSubGroup(GroupInterface[] _subgroups)
-	public onlyOwner
-	{
-		m_subgroups = _subgroups;
-	}
-	function viewPermissions(address _user)
-	public view returns (bytes1)
-	{
-		if (m_subgroups.length == 0) return bytes1(0);
-
-		bytes1 permissions = bytes1(-1);
-		for (uint256 i = 0; i < m_subgroups.length; ++i)
-		{
-			permissions = permissions & m_subgroups[i].viewPermissions(_user);
-		}
-		return permissions;
-	}
-}
