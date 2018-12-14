dbb1845aa18c8b8fedae41a00a2bb8ff23f1135c
Hadrien Croubois <hc@iex.ec>
2018-12-14 14:25:49 +0100

    SMS Directory smart contract for users and ressources

---
+++ contracts/SMSDirectory.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import './tools/Ownable.sol';
+
+contract SMSDirectory
+{
+	mapping(address => bytes) m_entries;
+
+	/**
+	 * Constructor
+	 */
+	constructor()
+	public
+	{
+	}
+
+	function getSMS(address _ressource)
+	external view returns (bytes memory)
+	{
+		return m_entries[_ressource];
+	}
+
+	function setSMS(address _ressource, bytes calldata _sms)
+	external
+	{
+		require(_ressource == msg.sender || OwnableImmutable(_ressource).m_owner() == msg.sender);
+		m_entries[_ressource] = _sms;
+	}
+
+}
