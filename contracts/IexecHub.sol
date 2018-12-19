5ffc66e4fede0d1c4fe43838ee718641dcd1fdd1
Hadrien Croubois <hc@iex.ec>
2018-12-19 16:36:43 +0100

    adding an initializeAndClaimArray method

--- contracts/IexecHub.sol
+++ contracts/IexecHub.sol
@@ -495,4 +495,17 @@ contract IexecHub is CategoryManager, IOracle
 		}
 		return true;
 	}
+
+	function initializeAndClaimArray(
+		bytes32[] calldata _dealid,
+		uint256[] calldata _idx)
+	external returns (bool)
+	{
+		require(_dealid.length == _idx.length);
+		for (uint i = 0; i < _dealid.length; ++i)
+		{
+			claim(initialize(_dealid[i], _idx[i]));
+		}
+		return true;
+	}
 }
