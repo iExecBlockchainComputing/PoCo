669b2c02651367745b7d6c0b58b1b21fe4bea5e8
Ugo Plouviez <up@iex.ec>
2019-02-14 11:49:53 +0100

    Add the 20s image for the chain

---
+++ parity_20sec/migrate_20s.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+nohup /bin/parity --chain /iexec-poco/parity/spec_20s.json --config /iexec-poco/parity/authority.toml -d /iexec-poco/parity/data --geth > deployed.txt 2>&1 &
+sleep 4
+cd /iexec-poco && sed -i '/ethereumjs-util/d' package.json && bash -i -c "npm i" && bash -i -c "./node_modules/.bin/truffle migrate" && rm -R contracts && rm -R build
+
+
