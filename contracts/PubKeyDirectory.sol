c7dbfd6c0b03033639e25b2122d11a5d6db1dc4e
Hadrien Croubois <hc@iex.ec>
2019-03-03 17:14:59 +0100

    adding PubKeyDirectory

---
+++ contracts/PubKeyDirectory.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.3;
+
+import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract PubKeyDirectory
+{
+	mapping(address => bytes) m_pubkeys;
+
+	/**
+	 * Constructor
+	 */
+	constructor()
+	public
+	{
+	}
+
+	function getKey(address _addr)
+	external view returns (bytes memory)
+	{
+		return m_pubkeys[_addr];
+	}
+
+	function setKey(bytes calldata _key)
+	external
+	{
+		// Extract address from public key
+		require(_key.length == 0x40);
+		bytes20 addr = bytes20(keccak256(_key) << 0x60);
+		// Store public key
+		m_pubkeys[address(addr)] = _key;
+	}
+
+	function setContractKey(address _contract, bytes calldata _key)
+	external
+	{
+		// Check _contract is in fact a contract
+		uint size;
+		assembly { size := extcodesize(addr) }
+		require(size > 0);
+		// Check operation is performed by the owner
+		require(Ownable(_contract).owner() == msg.sender);
+		// Store public key
+		m_pubkeys[_contract] = _key;
+	}
+
+}
