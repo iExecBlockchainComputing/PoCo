a3a2fe424907f4f60cc908995cd8554c59f09a78
Hadrien Croubois <hc@iex.ec>
2019-03-06 10:02:56 +0100

    need a non reverting signature verfiication

--- contracts/SignatureVerifier.sol
+++ contracts/SignatureVerifier.sol
@@ -20,7 +20,15 @@ contract SignatureVerifier is ECDSA
 		ECDSA.signature memory _signature)
 	public view returns (bool)
 	{
-		return _identity == recover(_hash, _signature) || IERC1271(_identity).isValidSignature(_hash, abi.encodePacked(_signature.r, _signature.s, _signature.v));
+		return recoverCheck(_identity, _hash, _signature) || IERC1271(_identity).isValidSignature(_hash, abi.encodePacked(_signature.r, _signature.s, _signature.v));
+	}
+
+	// recoverCheck does not revert if signature has invalid format
+	function recoverCheck(address candidate, bytes32 hash, signature memory sign)
+	internal pure returns (bool)
+	{
+		if (sign.v != 27 && sign.v != 28) return false;
+		return candidate == ecrecover(hash, sign.v, sign.r, sign.s);
 	}
 
 }
