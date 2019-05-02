059c9a6dace4e1c1b060658ba011bb2df4b22735
Hadrien Croubois <hc@iex.ec>
2019-05-02 18:10:08 +0200

    Adding mainnet addresses

--- truffle.js
+++ truffle.js
@@ -6,39 +6,41 @@ module.exports =
 	{
 		docker:
 		{
-			network_id: "*", // Match any network id,
 			host:       "iexec-geth-local",
 			port:       8545,
-			gas:        8000000,
-			gasPrice:   22000000000,
+			network_id: "*", // Match any network id,
+			gasPrice:   22000000000, //22Gwei
 		},
 		development:
 		{
-			network_id: "*", // Match any network id,
 			host:       "localhost",
 			port:       8545,
-			gas:        8000000,
-			gasPrice:   1000000000, //1Gwei
+			network_id: "*", // Match any network id,
+			gasPrice:   22000000000, //22Gwei
 		},
 		coverage:
 		{
-			network_id: "*",
 			host:       "localhost",
-			port:       8555,            // <-- If you change this, also set the port option in .solcover.js.
+			port:       8555,          // <-- If you change this, also set the port option in .solcover.js.
+			network_id: "*",
 			gas:        0xFFFFFFFFFFF, // <-- Use this high gas value
-			gasPrice:   0x01        // <-- Use this low gas price
+			gasPrice:   0x01           // <-- Use this low gas price
 		},
 		mainnet:
 		{
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.MAINNET_NODE),//"https://kovan.infura.io/v3/b2fd33d1c9cc440ba84752c2a4cf949d"
-			network_id: 1,
-			gas:        8000000,
-			gasPrice:   4000000000, //4Gwei
+			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
+			network_id: '1',
+			gasPrice:   22000000000, //22Gwei
+		},
+		ropsten:
+		{
+			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
+			network_id: '3',
+			gasPrice:   22000000000, //22Gwei
 		},
 		kovan: {
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),//"https://kovan.infura.io/v3/b2fd33d1c9cc440ba84752c2a4cf949d"
+			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
 			network_id: '42',
-			gas:        8000000,
 			gasPrice:   10000000000, //10Gwei
 		}
 	},
