8451786be7729c46cdd54b8f38e45a57a1ed8c4f
Hadrien Croubois <hc@iex.ec>
2019-05-02 11:27:03 +0200

    adding mainnet to truffle networks

--- truffle.js
+++ truffle.js
@@ -28,21 +28,19 @@ module.exports =
 			gas:        0xFFFFFFFFFFF, // <-- Use this high gas value
 			gasPrice:   0x01        // <-- Use this low gas price
 		},
-		ropsten:
+		mainnet:
 		{
-			network_id: 3,
-			host:       "localhost",
-			port:       8545,
+			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.MAINNET_NODE),//"https://kovan.infura.io/v3/b2fd33d1c9cc440ba84752c2a4cf949d"
+			network_id: 1,
 			gas:        8000000,
 			gasPrice:   4000000000, //4Gwei
-		},
-	    kovan: {
-			provider: () =>
-				new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),//"https://kovan.infura.io/v3/b2fd33d1c9cc440ba84752c2a4cf949d"
+		}
+		kovan: {
+			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),//"https://kovan.infura.io/v3/b2fd33d1c9cc440ba84752c2a4cf949d"
 			network_id: '42',
 			gas:        8000000,
 			gasPrice:   10000000000, //10Gwei
-	    }
+		}
 	},
 	compilers: {
 		solc: {
