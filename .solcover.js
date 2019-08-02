e215c549dba0e5e865d2c12796145e1fae1dea6b
Hadrien Croubois <hc@iex.ec>
2019-08-02 14:27:10 +0200

    solcover relative path to truffle

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 8555,
 	testrpcOptions: '-p 8555 -m "actual surround disorder swim upgrade devote digital misery truly verb slide final" -i 1544020727674',
-	testCommand: 'truffle migrate --network coverage && truffle test --network coverage',
+	testCommand: '../node_modules/.bin/truffle migrate --network coverage && ../node_modules/.bin/truffle test --network coverage',
 	norpc: false,
 	copyPackages: [
 		'@ensdomains/ens',
