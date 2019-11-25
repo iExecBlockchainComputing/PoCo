28657efa9030de948696cfa52db328dbf03441fb
Hadrien Croubois <hc@iex.ec>
2019-11-25 19:06:49 +0100

    signature verification refactor

--- contracts/modules/delegates/SignatureVerifier.sol
+++ contracts/modules/delegates/SignatureVerifier.sol
@@ -8,6 +8,16 @@ import "../DelegateBase.sol";
 
 contract SignatureVerifier is DelegateBase
 {
+	using IexecODBLibOrders_v4 for bytes32;
+
+	function _isContract(address _addr)
+	internal view returns (bool)
+	{
+		uint32 size;
+		assembly { size := extcodesize(_addr) }
+		return size > 0;
+	}
+
 	function _addrToKey(address _addr)
 	internal pure returns (bytes32)
 	{
@@ -23,7 +33,14 @@ contract SignatureVerifier is DelegateBase
 	function _checkSignature(address _identity, bytes32 _hash, bytes memory _signature)
 	internal view returns (bool)
 	{
-		return _isValidSignature(_identity, _hash, _signature) || IERC1271(_identity).isValidSignature(_hash, _signature);
+		if (_isContract(_identity))
+		{
+			return IERC1271(_identity).isValidSignature(_hash, _signature);
+		}
+		else
+		{
+			return _hash.recover(_signature) == _identity;
+		}
 	}
 
 	function _checkPresignature(address _identity, bytes32 _hash)
@@ -36,24 +53,5 @@ contract SignatureVerifier is DelegateBase
 	internal view returns (bool)
 	{
 		return _checkPresignature(_identity, _hash) || _checkSignature(_identity, _hash, _signature);
-	}
-
-	// Does not revert if signature has invalid format
-	function _isValidSignature(address signer, bytes32 hash, bytes memory sign)
-	internal pure returns (bool)
-	{
-		bytes32 r;
-		bytes32 s;
-		uint8   v;
-		if (sign.length != 65) return false;
-		assembly
-		{
-			r :=         mload(add(sign, 0x20))
-			s :=         mload(add(sign, 0x40))
-			v := byte(0, mload(add(sign, 0x60)))
-		}
-		if (v < 27) v += 27;
-		if (v != 27 && v != 28) return false;
-		return signer == ecrecover(hash, v, r, s);
 	}
 }
