f94497d6e6532f2303d5e3907c09cdba4689a380
Hadrien Croubois <hc@iex.ec>
2020-01-17 14:08:52 +0100

    ganache hotfix

--- migrations/2_deploy_factory.js
+++ migrations/2_deploy_factory.js
@@ -21,7 +21,7 @@ module.exports = async function(deployer, network, accounts)
 		{
 			console.log(`→ Factory is not yet deployed on ${chaintype} (${chainid})`)
 			await web3.eth.sendTransaction({ from: accounts[0], to: FACTORY.deployer, value: FACTORY.cost });
-			await web3.eth.sendSignedTransaction(FACTORY.tx[1]); // TODO replace 1 with chainid when fixed
+			await web3.eth.sendSignedTransaction(FACTORY.tx[chainid]);
 			GenericFactory.address = FACTORY.address;
 			console.log(`→ Factory deployed at address: ${(await GenericFactory.deployed()).address}`)
 		}
