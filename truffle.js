8aa52d76971aa531a9b7d97321ec70a3d5128c33
Hadrien Croubois <hc@iex.ec>
2020-03-09 12:02:27 +0100

    goerli beta deployment

--- truffle.js
+++ truffle.js
@@ -21,40 +21,40 @@ module.exports =
 		},
 		mainnet:
 		{
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.MAINNET_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.MAINNET_NODE),
 			network_id: '1',
 			gasPrice:   22000000000, //22Gwei
 		},
 		ropsten:
 		{
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.ROPSTEN_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.ROPSTEN_NODE),
 			network_id: '3',
 			gasPrice:   22000000000, //22Gwei
 		},
 		rinkeby:
 		{
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.RINKEBY_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_NODE),
 			network_id: '4',
 			gasPrice:   22000000000, //22Gwei
 		},
 		goerli:
 		{
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.GOERLI_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.GOERLI_NODE),
 			network_id: '5',
 			gasPrice:   22000000000, //22Gwei
 		},
 		kovan: {
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.KOVAN_NODE),
 			network_id: '42',
 			gasPrice:   10000000000, //10Gwei
 		},
 		viviani: {
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.VIVIANI_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.VIVIANI_NODE),
 			network_id: '133',
 			gasPrice:   "0", //1Gwei
 		},
 		bellecour: {
-			provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.BELLECOUR_NODE),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.BELLECOUR_NODE),
 			network_id: '134',
 			gasPrice:   "0", //1Gwei
 		}
