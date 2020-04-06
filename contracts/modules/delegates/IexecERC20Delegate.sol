e7921879198d8b97fe82bf3096dc6302efa85ad7
Hadrien Croubois <hc@iex.ec>
2020-04-06 17:36:54 +0200

    lint

--- contracts/modules/delegates/IexecERC20Delegate.sol
+++ contracts/modules/delegates/IexecERC20Delegate.sol
@@ -12,38 +12,38 @@ contract IexecERC20Delegate is IexecERC20, DelegateBase, IexecERC20Common
 	function transfer(address recipient, uint256 amount)
 	external override returns (bool)
 	{
-			_transfer(_msgSender(), recipient, amount);
-			return true;
+		_transfer(_msgSender(), recipient, amount);
+		return true;
 	}
 
 	function approve(address spender, uint256 value)
 	external override returns (bool)
 	{
-			_approve(_msgSender(), spender, value);
-			return true;
+		_approve(_msgSender(), spender, value);
+		return true;
 	}
 
 	function approveAndCall(address spender, uint256 value, bytes calldata extraData)
 	external override returns (bool)
 	{
-			_approve(_msgSender(), spender, value);
-			require(IexecTokenSpender(spender).receiveApproval(_msgSender(), value, address(this), extraData), 'approval-refused');
-			return true;
+		_approve(_msgSender(), spender, value);
+		require(IexecTokenSpender(spender).receiveApproval(_msgSender(), value, address(this), extraData), 'approval-refused');
+		return true;
 	}
 
 	function transferFrom(address sender, address recipient, uint256 amount)
 	external override returns (bool)
 	{
-			_transfer(sender, recipient, amount);
-			_approve(sender, _msgSender(), m_allowances[sender][_msgSender()].sub(amount));
-			return true;
+		_transfer(sender, recipient, amount);
+		_approve(sender, _msgSender(), m_allowances[sender][_msgSender()].sub(amount));
+		return true;
 	}
 
 	function increaseAllowance(address spender, uint256 addedValue)
 	external override returns (bool)
 	{
-			_approve(_msgSender(), spender, m_allowances[_msgSender()][spender].add(addedValue));
-			return true;
+		_approve(_msgSender(), spender, m_allowances[_msgSender()][spender].add(addedValue));
+		return true;
 	}
 
 
