715e2e1ce170e6157358351712b7c888a0ae0d1c
Hadrien Croubois <hc@iex.ec>
2020-04-21 21:50:39 +0200

    ens scripts updated with v5 addresses

--- scripts/ens/mainnet.sh
+++ scripts/ens/mainnet.sh
@@ -71,10 +71,10 @@ $CMD lookup workerpools.v5.iexec.eth
 # setup workerpools.v3.iexec.eth 0xc398052563469e6Ea7C442aBf124aADE7ec2CC92
 #
 # setup v5.iexec.eth # subnode only
-# setup core.v5.iexec.eth        0x
-# setup apps.v5.iexec.eth        0x
-# setup datasets.v5.iexec.eth    0x
-# setup workerpools.v5.iexec.eth 0x
+# setup core.v5.iexec.eth        0x3eca1B216A7DF1C7689aEb259fFB83ADFB894E7f
+# setup apps.v5.iexec.eth        0xB1C52075b276f87b1834919167312221d50c9D16
+# setup datasets.v5.iexec.eth    0x799DAa22654128d0C64d5b79eac9283008158730
+# setup workerpools.v5.iexec.eth 0xC76A18c78B7e530A165c5683CB1aB134E21938B4
 
 # Reset old subdomains
 # reset hub.iexec.eth
