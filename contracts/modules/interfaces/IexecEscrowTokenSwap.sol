91bb2bd2abca9914afa9ce943accc735ae2e6982
Hadrien Croubois <hc@iex.ec>
2020-08-27 14:56:50 +0200

    copyright uniswap

--- contracts/modules/interfaces/IexecEscrowTokenSwap.sol
+++ contracts/modules/interfaces/IexecEscrowTokenSwap.sol
@@ -1,3 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/******************************************************************************
+ * Copyright 2020 IEXEC BLOCKCHAIN TECH                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
@@ -15,7 +33,7 @@ interface IexecEscrowTokenSwap
 	function estimateDepositTokenWanted(uint256) external view returns (uint256);
 	function estimateWithdrawTokenSent (uint256) external view returns (uint256);
 	function estimateWithdrawEthWanted (uint256) external view returns (uint256);
-	
+
 	function depositEth       (                         ) external payable;
 	function depositEthFor    (                  address) external payable;
 	function safeDepositEth   (         uint256         ) external payable;
