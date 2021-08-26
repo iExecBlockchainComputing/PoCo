b9d5f3a1a8012ec7f80a263e7ae278f2879e0cf7
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2021-08-26 15:51:46 +0200

    refactor

--- migrations/7_fillwallets.js
+++ migrations/7_fillwallets.js
@@ -1,42 +1,52 @@
-const assert   = require('assert')
+const assert = require("assert");
 // CONFIG
-const CONFIG   = require('../config/config.json')
-const ACCOUNTS = require('../config/accounts.json')
+const CONFIG = require("../config/config.json");
+const ACCOUNTS = require("../config/accounts.json");
 // Token
-var RLC                 = artifacts.require('rlc-faucet-contract/RLC')
+const RLC = artifacts.require("rlc-faucet-contract/RLC");
 // ERC1538 core & delegates
-var ERC1538Proxy        = artifacts.require('@iexec/solidity/ERC1538Proxy')
+const ERC1538Proxy = artifacts.require("@iexec/solidity/ERC1538Proxy");
 // Interface
-var IexecInterfaceToken = artifacts.require('IexecInterfaceToken')
+const IexecInterfaceToken = artifacts.require("IexecInterfaceToken");
 
 /*****************************************************************************
  *                                   Main                                    *
  *****************************************************************************/
-module.exports = async function(deployer, network, accounts)
-{
-	console.log('# web3 version:', web3.version);
-	const chainid   = await web3.eth.net.getId();
-	const chaintype = await web3.eth.net.getNetworkType();
-	console.log('Chainid is:', chainid);
-	console.log('Chaintype is:', chaintype);
-	console.log('Deployer is:', accounts[0]);
+module.exports = async function (deployer, network, accounts) {
+  console.log("# web3 version:", web3.version);
+  const chainid = await web3.eth.net.getId();
+  const chaintype = await web3.eth.net.getNetworkType();
+  console.log("Chainid is:", chainid);
+  console.log("Chaintype is:", chaintype);
+  console.log("Deployer is:", accounts[0]);
 
-	const deploymentOptions = CONFIG.chains[chainid] || CONFIG.chains.default;
+  const deploymentOptions = CONFIG.chains[chainid] || CONFIG.chains.default;
 
-	switch (deploymentOptions.asset)
-	{
-		case 'Token':  IexecInterfaceInstance = await IexecInterfaceToken.at((await ERC1538Proxy.deployed()).address);  break;
-	}
+  //only for standard token private chains
+  if (
+    chainid > 134 &&
+    deploymentOptions.asset === "Token" &&
+    !process.env.KYC
+  ) {
+    const IexecInterfaceInstance = await IexecInterfaceToken.at(
+      (
+        await ERC1538Proxy.deployed()
+      ).address
+    );
+    const totalAmount = 1000000000; // 1RLC
 
-    //only for standard token private chains
-	if (chainid > 134 && deploymentOptions.asset == 'Token'  && !process.env.KYC) {
-		const totalAmount = 1000000000; // 1RLC
-        RLCInstance = await RLC.deployed();
-        await RLCInstance.approveAndCall(IexecInterfaceInstance.address, totalAmount, "0x");
-        // all transfers
-        await Promise.all(ACCOUNTS.map(({address, amount}) => {
-            console.log('Transferring for address ' + address + ": " + amount)
-            IexecInterfaceInstance.transfer(address, amount)
-        }))
-	}
+    const RLCInstance = await RLC.deployed();
+    await RLCInstance.approveAndCall(
+      IexecInterfaceInstance.address,
+      totalAmount,
+      "0x"
+    );
+    // all transfers
+    await Promise.all(
+      ACCOUNTS.map(({ address, amount }) => {
+        console.log("Transferring for address " + address + ": " + amount);
+        IexecInterfaceInstance.transfer(address, amount);
+      })
+    );
+  }
 };
