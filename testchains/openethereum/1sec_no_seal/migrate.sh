4f7047e01d756510b65fabf1f80a60e7b8bccb8f
Zied <26070035+zguesmi@users.noreply.github.com>
2021-11-24 16:18:50 +0100

    Clean legacy files

--- testchains/openethereum/1sec_no_seal/migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-nohup /home/openethereum/openethereum --chain /iexec-poco/testchains/openethereum/1sec_no_seal/spec.json --config /iexec-poco/testchains/openethereum/1sec_no_seal/authority.toml -d /iexec-poco/testchains/openethereum/1sec_no_seal/data > deployed.txt 2>&1 &
-sleep 4
-cd /iexec-poco && \
-  sed -i '/ethereumjs-util/d' package.json && \
-  bash -i -c "npm i --no-progress" && \
-  bash -i -c "./node_modules/.bin/truffle migrate" && \
-  rm -R contracts && \
-  rm -R build
--- testchains/openethereum/5sec/migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-nohup /home/openethereum/openethereum --chain /iexec-poco/testchains/openethereum/5sec/spec.json --config /iexec-poco/testchains/openethereum/5sec/authority.toml -d /iexec-poco/testchains/openethereum/5sec/data > deployed.txt 2>&1 &
-sleep 4
-cd /iexec-poco && \
-  sed -i '/ethereumjs-util/d' package.json && \
-  bash -i -c "npm i --no-progress" && \
-  bash -i -c "./node_modules/.bin/truffle migrate" && \
-  rm -R contracts && \
-  rm -R build
