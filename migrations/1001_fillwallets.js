0ed36828d631bd8f8b7af028dc7b9afe19df052c
Zied <26070035+zguesmi@users.noreply.github.com>
2023-03-20 10:27:39 +0100

    Disable transfers temporarily

--- migrations/1001_fillwallets.js
+++ migrations/1001_fillwallets.js
@@ -20,47 +20,47 @@ module.exports = async function (deployer, network, accounts) {
   console.log("Chaintype is:", chaintype);
   console.log("Deployer is:", accounts[0]);
 
-  const deploymentOptions = CONFIG.chains[chainid] || CONFIG.chains.default;
+//   const deploymentOptions = CONFIG.chains[chainid] || CONFIG.chains.default;
 
-  //only for standard private chains
-  if (chainid > 1000 && !process.env.KYC) {
-    let IexecInterfaceInstance;
-    const IexecProxyInstance = await ERC1538Proxy.deployed();
-    const totalAmount = ACCOUNTS.reduce(
-      (acc, { amount }) => acc.add(web3.utils.toBN(amount)),
-      web3.utils.toBN(0)
-    );
+//   //only for standard private chains
+//   if (chainid > 1000 && !process.env.KYC) {
+//     let IexecInterfaceInstance;
+//     const IexecProxyInstance = await ERC1538Proxy.deployed();
+//     const totalAmount = ACCOUNTS.reduce(
+//       (acc, { amount }) => acc.add(web3.utils.toBN(amount)),
+//       web3.utils.toBN(0)
+//     );
 
-    // deposit
-    console.log("Depositing " + totalAmount);
-    switch (deploymentOptions.asset) {
-      case "Token":
-        IexecInterfaceInstance = await IexecInterfaceToken.at(
-          IexecProxyInstance.address
-        );
-        const RLCInstance = await RLC.deployed();
-        await RLCInstance.approveAndCall(
-          IexecInterfaceInstance.address,
-          totalAmount,
-          "0x"
-        );
-        break;
-      case "Native":
-        IexecInterfaceInstance = await IexecInterfaceNative.at(
-          IexecProxyInstance.address
-        );
-        await IexecInterfaceInstance.deposit({
-          from: accounts[0],
-          value: totalAmount.mul(web3.utils.toBN(10 ** 9)),
-        });
-        break;
-    }
+//     // deposit
+//     console.log("Depositing " + totalAmount);
+//     switch (deploymentOptions.asset) {
+//       case "Token":
+//         IexecInterfaceInstance = await IexecInterfaceToken.at(
+//           IexecProxyInstance.address
+//         );
+//         const RLCInstance = await RLC.deployed();
+//         await RLCInstance.approveAndCall(
+//           IexecInterfaceInstance.address,
+//           totalAmount,
+//           "0x"
+//         );
+//         break;
+//       case "Native":
+//         IexecInterfaceInstance = await IexecInterfaceNative.at(
+//           IexecProxyInstance.address
+//         );
+//         await IexecInterfaceInstance.deposit({
+//           from: accounts[0],
+//           value: totalAmount.mul(web3.utils.toBN(10 ** 9)),
+//         });
+//         break;
+//     }
 
-    // all transfers
-    for (account of ACCOUNTS) {
-      const { address, amount } = account
-      console.log("Transferring for address " + address + ": " + amount);
-      await IexecInterfaceInstance.transfer(address, amount);
-    }
-  }
+//     // all transfers
+//     for (account of ACCOUNTS) {
+//       const { address, amount } = account
+//       console.log("Transferring for address " + address + ": " + amount);
+//       await IexecInterfaceInstance.transfer(address, amount);
+//     }
+//   }
 };
