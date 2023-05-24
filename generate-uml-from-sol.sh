0ed8bb1d73c59508d8d806f658d7300a1d8d223a
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2023-05-24 12:37:28 +0200

    Generate class UML only connected to set of given contracts

--- generate-uml-from-sol.sh
+++ generate-uml-from-sol.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-ROOT=$(pwd $(dirname $0))
+ROOT=$(pwd "$(dirname "$0")")
 SOL2UML="$ROOT/node_modules/sol2uml/lib/sol2uml.js"
+cd "$ROOT/contracts/" || exit
 
-generate_class_uml() {
+# For sol2uml documentaiton, see https://github.com/naddison36/sol2uml#usage
+
+# Generate class UML for contracts in a given directory
+generate_class_uml_dir() {
     contracts_to_display=$1
-    cd $ROOT/contracts/
-    output_filename="${contracts_to_display/\//-}" # convert slash to dash of needed
-    $SOL2UML class $contracts_to_display/ -o "$ROOT/uml/class-uml-$output_filename.svg"
+    output_filename=${contracts_to_display/\//-} # convert slash to dash of needed
+    $SOL2UML class "$contracts_to_display/" -o "$ROOT/uml/class-uml-$output_filename.svg"
 }
 
-generate_class_uml libs
-generate_class_uml modules
-generate_class_uml registries
+# Generate class UML only connected to set of given contracts
+generate_class_uml_base() {
+    base_contracts_to_display=$1
+    output_filename=$2
+    # -b, --baseContractNames <value> 
+    # only output contracts connected to these comma separated base contract names
+    $SOL2UML class . -b "$base_contracts_to_display" -o "$ROOT/uml/class-uml-$output_filename.svg"
+}
+
+generate_class_uml_dir libs
+generate_class_uml_dir modules
+generate_class_uml_dir registries
+
+generate_class_uml_base \
+    IexecPoco1Delegate,IexecPoco2Delegate \
+    IexecPocoDelegates # out
+generate_class_uml_base \
+    IexecEscrowNativeDelegate,IexecEscrowTokenDelegateKYC,IexecEscrowTokenDelegate,IexecEscrowTokenSwapDelegate \
+    IexecEscrows # out
+    
