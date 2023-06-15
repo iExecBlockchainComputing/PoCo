4db10b54b7703b679092b325f8b389d08ca48ef0
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2023-06-15 11:35:36 +0200

    Init deploy nominal with hardhat-truffle5

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,1 +1,24 @@
-// TODO
+import hre from "hardhat";
+import {
+    ERC1538Update__factory, ERC1538Update,
+    ERC1538Proxy
+} from "../typechain";
+import { deployAllContracts } from "../test/truffle-fixture-deployer"
+const erc1538Proxy: ERC1538Proxy = hre.artifacts.require('@iexec/solidity/ERC1538Proxy')
+
+async function main() {
+    console.log("About to deploy PoCo Nominal..")
+    await deployAllContracts()
+    const erc1538ProxyAddress = (await erc1538Proxy.deployed()).address;
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+    const erc1538: ERC1538Update = ERC1538Update__factory
+        .connect(erc1538ProxyAddress, owner);
+    console.log(`IexecInstance found at address: ${erc1538.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
