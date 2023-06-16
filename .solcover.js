af8fff62305deed09217c08da9c1185bd21caa76
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2023-06-16 16:20:55 +0200

    Clean solidity coverage config

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,3 @@
-//const fs = require("fs");
-
 module.exports = {
     mocha: {
         timeout: 600000, // double timeout
@@ -9,24 +7,4 @@ module.exports = {
         'tools/testing/TestClient.sol',
         'tools/testing/TestReceiver.sol',
     ],
-    /*
-    onCompileComplete: () => {
-        files = [
-            { repo: 'rlc-faucet-contract',  name: 'RLC'                   },
-            { repo: '@ensdomains/ens',      name: 'ENSRegistry'           },
-            { repo: '@ensdomains/ens',      name: 'FIFSRegistrar'         },
-            { repo: '@ensdomains/ens',      name: 'ReverseRegistrar'      },
-            { repo: '@ensdomains/resolver', name: 'PublicResolver'        },
-            { repo: '@iexec/erlc',          name: 'ERLCTokenSwap'         },
-            { repo: '@iexec/solidity',      name: 'ERC1538Proxy'          },
-            { repo: '@iexec/solidity',      name: 'ERC1538UpdateDelegate' },
-            { repo: '@iexec/solidity',      name: 'ERC1538QueryDelegate'  },
-            { repo: '@iexec/solidity',      name: 'GenericFactory'        },
-        ];
-        for (file of files)
-        {
-            fs.copyFileSync(`node_modules/${file.repo}/build/contracts/${file.name}.json`, `.coverage_artifacts/contracts/${file.name}.json`);
-        }
-    }
-    */
 };
