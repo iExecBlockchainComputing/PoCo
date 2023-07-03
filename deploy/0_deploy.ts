57fdde95b5af9ce579231511756ce05f41fcb576
Gabriel Fournier <gabriel.fournier@iex.ec>
2023-07-03 10:10:12 +0200

    pretier

--- deploy/0_deploy.ts
+++ deploy/0_deploy.ts
@@ -1,87 +1,86 @@
-import hre from "hardhat";
-import fs from "fs";
-import path from "path";
+import hre from 'hardhat';
+import fs from 'fs';
+import path from 'path';
 import initial_migration from '../migrations/1_initial_migration';
 import deploy_token from '../migrations/3_deploy_token';
 import deploy_core from '../migrations/4_deploy_core';
 import deploy_ens from '../migrations/5_deploy_ens';
 import whitelisting from '../migrations/6_whitelisting';
 import functions from '../migrations/999_functions';
-import { getFunctionSignatures } from "../migrations/utils/getFunctionSignatures";
-const erc1538Proxy: ERC1538Proxy =
-    hre.artifacts.require('@iexec/solidity/ERC1538Proxy')
+import { getFunctionSignatures } from '../migrations/utils/getFunctionSignatures';
+const erc1538Proxy: ERC1538Proxy = hre.artifacts.require('@iexec/solidity/ERC1538Proxy');
 import {
-    ERC1538Proxy,
-    IexecAccessors__factory,
-    IexecPocoBoostDelegate, IexecPocoBoostDelegate__factory,
-    ERC1538Update, ERC1538Update__factory,
-    ERC1538Query, ERC1538Query__factory
-} from "../typechain";
+  ERC1538Proxy,
+  IexecAccessors__factory,
+  IexecPocoBoostDelegate,
+  IexecPocoBoostDelegate__factory,
+  ERC1538Update,
+  ERC1538Update__factory,
+  ERC1538Query,
+  ERC1538Query__factory,
+} from '../typechain';
 
 /**
- * @dev Deploying contracts with `npx hardhat deploy` task brought by 
+ * @dev Deploying contracts with `npx hardhat deploy` task brought by
  * `hardhat-deploy` plugin.
- * Previous deployments made with `npx hardhat run scripts/deploy.ts` used to 
+ * Previous deployments made with `npx hardhat run scripts/deploy.ts` used to
  * hang at the end of deployments (terminal did not return at the end).
- * 
+ *
  * Note:
- * The`hardhat-deploy` plugin is currently being under used compared to all 
+ * The`hardhat-deploy` plugin is currently being under used compared to all
  * features available in it.
  */
 module.exports = async function () {
-    console.log("Deploying PoCo Nominal..")
-    const accounts = await hre.web3.eth.getAccounts()
-    await initial_migration()
-    await deploy_token(accounts)
-    await deploy_core(accounts)
-    await deploy_ens(accounts)
-    await whitelisting(accounts)
-    // Retrieve proxy address from previous truffle-fixture deployment
-    const { address: erc1538ProxyAddress } = await erc1538Proxy.deployed();
-    if (!erc1538ProxyAddress) {
-        console.error("Failed to retrieve deployed address of ERC1538Proxy")
-        process.exitCode = 1;
-    }
-    console.log(`ERC1538Proxy found: ${erc1538ProxyAddress}`);
-    // Save addresses of deployed PoCo Nominal contracts for later use
-    saveDeployedAddress("ERC1538Proxy", erc1538ProxyAddress);
+  console.log('Deploying PoCo Nominal..');
+  const accounts = await hre.web3.eth.getAccounts();
+  await initial_migration();
+  await deploy_token(accounts);
+  await deploy_core(accounts);
+  await deploy_ens(accounts);
+  await whitelisting(accounts);
+  // Retrieve proxy address from previous truffle-fixture deployment
+  const { address: erc1538ProxyAddress } = await erc1538Proxy.deployed();
+  if (!erc1538ProxyAddress) {
+    console.error('Failed to retrieve deployed address of ERC1538Proxy');
+    process.exitCode = 1;
+  }
+  console.log(`ERC1538Proxy found: ${erc1538ProxyAddress}`);
+  // Save addresses of deployed PoCo Nominal contracts for later use
+  saveDeployedAddress('ERC1538Proxy', erc1538ProxyAddress);
 
-    console.log("Deploying PoCo Boost..")
-    const [owner] = await hre.ethers.getSigners();
-    const iexecPocoBoostInstance: IexecPocoBoostDelegate =
-        await new IexecPocoBoostDelegate__factory()
-            .connect(owner)
-            .deploy()
-            .then(instance => instance.deployed());
-    console.log(`IexecPocoBoostDelegate deployed: ${iexecPocoBoostInstance.address}`)
+  console.log('Deploying PoCo Boost..');
+  const [owner] = await hre.ethers.getSigners();
+  const iexecPocoBoostInstance: IexecPocoBoostDelegate = await new IexecPocoBoostDelegate__factory()
+    .connect(owner)
+    .deploy()
+    .then((instance) => instance.deployed());
+  console.log(`IexecPocoBoostDelegate deployed: ${iexecPocoBoostInstance.address}`);
 
-    // Show proxy functions
-    await functions(accounts)
+  // Show proxy functions
+  await functions(accounts);
 
-    const erc1538: ERC1538Update = ERC1538Update__factory
-            .connect(erc1538ProxyAddress, owner);
-        console.log(`IexecInstance found at address: ${erc1538.address}`);
-        // Link IexecPocoBoost methods to ERC1538Proxy
-        await erc1538.updateContract(
-            iexecPocoBoostInstance.address,
-            getFunctionSignatures(IexecPocoBoostDelegate__factory.abi),
-            'Linking ' + IexecPocoBoostDelegate__factory.name
-        );
-        // Verify linking on ERC1538Proxy
-        const erc1538QueryInstance: ERC1538Query = ERC1538Query__factory
-            .connect(erc1538ProxyAddress, owner);
-        const functionCount = await erc1538QueryInstance.totalFunctions();
-        console.log(`The deployed ERC1538Proxy now supports ${functionCount} functions:`);
-        await Promise.all(
-            [...Array(functionCount.toNumber()).keys()].map(async (i) => {
-                const [method, _, contract] = await erc1538QueryInstance.functionByIndex(i);
-                if (contract == iexecPocoBoostInstance.address) {
-                    console.log(`[${i}] ${contract} (IexecPocoBoostDelegate) ${method}`);
-                }
-            })
-        );
-    
-    return iexecPocoBoostInstance
+  const erc1538: ERC1538Update = ERC1538Update__factory.connect(erc1538ProxyAddress, owner);
+  console.log(`IexecInstance found at address: ${erc1538.address}`);
+  // Link IexecPocoBoost methods to ERC1538Proxy
+  await erc1538.updateContract(
+    iexecPocoBoostInstance.address,
+    getFunctionSignatures(IexecPocoBoostDelegate__factory.abi),
+    'Linking ' + IexecPocoBoostDelegate__factory.name,
+  );
+  // Verify linking on ERC1538Proxy
+  const erc1538QueryInstance: ERC1538Query = ERC1538Query__factory.connect(erc1538ProxyAddress, owner);
+  const functionCount = await erc1538QueryInstance.totalFunctions();
+  console.log(`The deployed ERC1538Proxy now supports ${functionCount} functions:`);
+  await Promise.all(
+    [...Array(functionCount.toNumber()).keys()].map(async (i) => {
+      const [method, _, contract] = await erc1538QueryInstance.functionByIndex(i);
+      if (contract == iexecPocoBoostInstance.address) {
+        console.log(`[${i}] ${contract} (IexecPocoBoostDelegate) ${method}`);
+      }
+    }),
+  );
+
+  return iexecPocoBoostInstance;
 };
 
 // TODO [optional]: Use hardhat-deploy to save addresses automatically
@@ -92,17 +91,17 @@ module.exports = async function () {
  * @param deployedAddress address where contract where deployed
  */
 function saveDeployedAddress(contractName: string, deployedAddress: string) {
-    const chainId = hre.network.config.chainId || 0;
-    const BUILD_DIR = '../build';
-    fs.writeFileSync(
-        path.resolve(__dirname, BUILD_DIR, `${contractName}.json`),
-        JSON.stringify({
-            "networks": {
-                [chainId]: {
-                    "address": deployedAddress
-                }
-            }
+  const chainId = hre.network.config.chainId || 0;
+  const BUILD_DIR = '../build';
+  fs.writeFileSync(
+    path.resolve(__dirname, BUILD_DIR, `${contractName}.json`),
+    JSON.stringify({
+      networks: {
+        [chainId]: {
+          address: deployedAddress,
         },
-        ));
-    console.log(`Saved deployment at ${deployedAddress} for ${contractName}`)
+      },
+    }),
+  );
+  console.log(`Saved deployment at ${deployedAddress} for ${contractName}`);
 }
--- test/000_fullchain-boost.ts
+++ test/000_fullchain-boost.ts
@@ -14,96 +14,94 @@
  * limitations under the License.                                             *
  ******************************************************************************/
 
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
-import hre, { ethers } from "hardhat";
-import {
-    IexecLibOrders_v5,
-    IexecPocoBoostDelegate,
-    AppRegistry__factory, AppRegistry
-} from "../typechain";
-import deployPocoBoostFixture from "../deploy/0_deploy";
-import constants from "../utils/constants";
-import { extractEventsFromReceipt } from "../utils/tools";
-import { createEmptyRequestOrder, createEmptyAppOrder } from "../utils/createOrders";
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import hre, { ethers } from 'hardhat';
+import { IexecLibOrders_v5, IexecPocoBoostDelegate, AppRegistry__factory, AppRegistry } from '../typechain';
+import deployPocoBoostFixture from '../deploy/0_deploy';
+import constants from '../utils/constants';
+import { extractEventsFromReceipt } from '../utils/tools';
+import { createEmptyRequestOrder, createEmptyAppOrder } from '../utils/createOrders';
 
-describe("IexecPocoBoostDelegate", function () {
-    let iexecPocoBoostInstance: IexecPocoBoostDelegate;
-    let requestOrder: IexecLibOrders_v5.RequestOrderStruct;
-    let appOrder: IexecLibOrders_v5.AppOrderStruct;
-    let appAddress = "";
-    before("Deploy IexecPocoBoostDelegate", async () => {
-        // We define a fixture to reuse the same setup in every test.
-        // We use loadFixture to run this setup once, snapshot that state,
-        // and reset Hardhat Network to that snapshot in every test.
+describe('IexecPocoBoostDelegate', function () {
+  let iexecPocoBoostInstance: IexecPocoBoostDelegate;
+  let requestOrder: IexecLibOrders_v5.RequestOrderStruct;
+  let appOrder: IexecLibOrders_v5.AppOrderStruct;
+  let appAddress = '';
+  before('Deploy IexecPocoBoostDelegate', async () => {
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshot in every test.
 
-        const [owner, appProvider, otherAccount] = await hre.ethers.getSigners();
-        iexecPocoBoostInstance = await deployPocoBoostFixture()
-        console.log("iexecPocoBoostInstance.addr",iexecPocoBoostInstance.address)
+    const [owner, appProvider, otherAccount] = await hre.ethers.getSigners();
+    iexecPocoBoostInstance = await deployPocoBoostFixture();
+    console.log('iexecPocoBoostInstance.addr', iexecPocoBoostInstance.address);
 
-        const appRegistryInstance: AppRegistry =
-        AppRegistry__factory.connect(await getContractAddress('AppRegistry'), owner)
-        const receipt = await appRegistryInstance.createApp(appProvider.address,
-            "my-app",
-            "APP_TYPE_0",
-            constants.NULL.BYTES32,
-            constants.NULL.BYTES32,
-            constants.NULL.BYTES32)
-            .then(tx => tx.wait())
-        const events = extractEventsFromReceipt(receipt,
-            appRegistryInstance.address, "Transfer")
-        appAddress = events[0].args['tokenId'].toHexString()
+    const appRegistryInstance: AppRegistry = AppRegistry__factory.connect(
+      await getContractAddress('AppRegistry'),
+      owner,
+    );
+    const receipt = await appRegistryInstance
+      .createApp(
+        appProvider.address,
+        'my-app',
+        'APP_TYPE_0',
+        constants.NULL.BYTES32,
+        constants.NULL.BYTES32,
+        constants.NULL.BYTES32,
+      )
+      .then((tx) => tx.wait());
+    const events = extractEventsFromReceipt(receipt, appRegistryInstance.address, 'Transfer');
+    appAddress = events[0].args['tokenId'].toHexString();
 
-        requestOrder = createEmptyRequestOrder()
-        appOrder = createEmptyAppOrder()
+    requestOrder = createEmptyRequestOrder();
+    appOrder = createEmptyAppOrder();
+  });
+
+  describe('MatchOrders', function () {
+    it('Should match orders', async function () {
+      requestOrder.app = appAddress;
+      appOrder.app = appAddress;
+      const expectedDealId = '0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5';
+      await expect(iexecPocoBoostInstance.matchOrdersBoost(requestOrder, appOrder))
+        .to.emit(iexecPocoBoostInstance, 'OrdersMatchedBoost')
+        .withArgs(expectedDealId);
+      expect((await iexecPocoBoostInstance.viewDealBoost(expectedDealId)).tag).is.equal(
+        '0x0000000000000000000000000000000000000000000000000000000000000000',
+      );
     });
 
-    describe("MatchOrders", function () {
-        it("Should match orders", async function () {
-            requestOrder.app = appAddress;
-            appOrder.app = appAddress;
-            const expectedDealId =
-                "0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5";
-            await expect(iexecPocoBoostInstance.matchOrdersBoost(requestOrder, appOrder))
-                .to.emit(iexecPocoBoostInstance, "OrdersMatchedBoost")
-                .withArgs(expectedDealId);
-            expect((await iexecPocoBoostInstance.viewDealBoost(expectedDealId)).tag)
-                .is.equal("0x0000000000000000000000000000000000000000000000000000000000000000")
-        });
+    it('Should not match orders', async function () {
+      requestOrder.tag = '0x0000000000000000000000000000000000000000000000000000000000000001';
+      await expect(iexecPocoBoostInstance.matchOrdersBoost(requestOrder, appOrder)).to.be.revertedWith(
+        'Incompatible request and app orders',
+      );
+    });
+  });
 
-        it("Should not match orders", async function () {
-            requestOrder.tag = "0x0000000000000000000000000000000000000000000000000000000000000001"
-            await expect(iexecPocoBoostInstance.matchOrdersBoost(requestOrder, appOrder))
-                .to.be.revertedWith("Incompatible request and app orders");
-        });
+  describe('PushResult', function () {
+    it('Should push result', async function () {
+      requestOrder = createEmptyRequestOrder();
+      appOrder = createEmptyAppOrder();
+      requestOrder.app = appAddress;
+      appOrder.app = appAddress;
+      const dealId: string = '0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5';
+      const index: number = 0;
+      // 0xf2e081861701d912a7a1365bc24c79a52c1ea122b05776aa47471f6d660d233d
+      const result: string = ethers.utils.sha256(ethers.utils.toUtf8Bytes('the-result'));
+      await iexecPocoBoostInstance.matchOrdersBoost(requestOrder, appOrder);
+      await expect(iexecPocoBoostInstance.pushResultBoost(dealId, index, result))
+        .to.emit(iexecPocoBoostInstance, 'ResultPushedBoost')
+        .withArgs(dealId, index, result);
     });
 
-    describe("PushResult", function () {
-        it("Should push result", async function () {
-            requestOrder = createEmptyRequestOrder()
-            appOrder = createEmptyAppOrder()
-            requestOrder.app = appAddress;
-            appOrder.app = appAddress;
-            const dealId: string =
-                "0xad3228b676f7d3cd4284a5443f17f1962b36e491b30a40b2405849e597ba5fb5";
-            const index: number = 0;
-            // 0xf2e081861701d912a7a1365bc24c79a52c1ea122b05776aa47471f6d660d233d
-            const result: string = ethers.utils.sha256(ethers.utils.toUtf8Bytes("the-result"));
-            await iexecPocoBoostInstance.matchOrdersBoost(requestOrder, appOrder)
-            await expect(iexecPocoBoostInstance.pushResultBoost(dealId, index, result))
-                .to.emit(iexecPocoBoostInstance, "ResultPushedBoost")
-                .withArgs(dealId, index, result);
-        });
-
-        it("Should not push result when deal not found", async function () {
-            // No match order
-            const badDealId: string =
-                "0xff6973ffda6bcc1a4ace058b4a62bf5e179ea78fd58a1ccd71c22cc9b688aaaa"
-            const result: string = ethers.utils.sha256(ethers.utils.toUtf8Bytes("the-result"));
-            await expect(iexecPocoBoostInstance.pushResultBoost(badDealId, 0, result))
-                .to.be.revertedWith("Deal not found");
-        });
+    it('Should not push result when deal not found', async function () {
+      // No match order
+      const badDealId: string = '0xff6973ffda6bcc1a4ace058b4a62bf5e179ea78fd58a1ccd71c22cc9b688aaaa';
+      const result: string = ethers.utils.sha256(ethers.utils.toUtf8Bytes('the-result'));
+      await expect(iexecPocoBoostInstance.pushResultBoost(badDealId, 0, result)).to.be.revertedWith('Deal not found');
     });
+  });
 });
 
 /**
@@ -112,6 +110,7 @@ describe("IexecPocoBoostDelegate", function () {
  * @returns deployed address
  */
 async function getContractAddress(contractName: string): Promise<string> {
-    return await ((await hre.artifacts.require(contractName)
-        .deployed()).address);
+  return await (
+    await hre.artifacts.require(contractName).deployed()
+  ).address;
 }
