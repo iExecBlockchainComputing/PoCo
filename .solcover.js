663f67d60e780c0bdbc92ef2e06c33fa0bd08bea
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2023-08-21 16:37:19 +0200

    Update yul optimizer config

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,17 @@
 const BASE_FOLDER = './coverage'
 
 module.exports = {
+    /**
+     * See https://github.com/sc-forks/solidity-coverage/issues/715 issue if 
+     * coverage is 0% when IR enabled.
+     */
     configureYulOptimizer: true,
+    solcOptimizerDetails: {
+        yul: true,
+        yulDetails: {
+            optimizerSteps: ''
+        },
+    },
     mocha: {
         timeout: 600000, // double timeout
     },
