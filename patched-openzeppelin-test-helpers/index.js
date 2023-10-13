525937b68632d5c22e83bb37352cf6593ecf62b7
Zied <26070035+zguesmi@users.noreply.github.com>
2023-10-13 16:45:42 +0200

    Remove patched test helpers

--- patched-openzeppelin-test-helpers/index.js
+++ patched-openzeppelin-test-helpers/index.js
@@ -1,36 +1,36 @@
 const ozTestHelper = require("@openzeppelin/test-helpers");
 const { expect } = require("chai");
 
-const ozExpectRevert = ozTestHelper.expectRevert;
+// const ozExpectRevert = ozTestHelper.expectRevert;
 
-const patchedExpectRevert = async function (...args) {
-  return ozExpectRevert(...args);
-};
+// const patchedExpectRevert = async function (...args) {
+//   return ozExpectRevert(...args);
+// };
 
-Object.entries(ozExpectRevert).forEach(([key, val]) => {
-  patchedExpectRevert[key] = val;
-});
+// Object.entries(ozExpectRevert).forEach(([key, val]) => {
+//   patchedExpectRevert[key] = val;
+// });
 
-patchedExpectRevert.unspecified = async function (promise) {
-  console.warn(
-    `Using patched 'exceptRevert.unspecified', error message will not be checked by @openzeppelin/test-helpers`
-  );
-  try {
-    await promise;
-  } catch (error) {
-    console.info(`got error: ${error}`);
-    return;
-  }
-  expect.fail("Expected an exception but none was received");
-};
+// patchedExpectRevert.unspecified = async function (promise) {
+//   console.warn(
+//     `Using patched 'exceptRevert.unspecified', error message will not be checked by @openzeppelin/test-helpers`
+//   );
+//   try {
+//     await promise;
+//   } catch (error) {
+//     console.info(`got error: ${error}`);
+//     return;
+//   }
+//   expect.fail("Expected an exception but none was received");
+// };
 
-const patchedTestHelper = {
-  ...ozTestHelper,
-  expectRevert: patchedExpectRevert,
-};
+// const patchedTestHelper = {
+//   ...ozTestHelper,
+//   expectRevert: patchedExpectRevert,
+// };
 
-console.log(
-  "using patched-openzeppelin-test-helpers based on @openzeppelin/test-helpers with patched 'exceptRevert.unspecified'"
-);
+// console.log(
+//   "using patched-openzeppelin-test-helpers based on @openzeppelin/test-helpers with patched 'exceptRevert.unspecified'"
+// );
 
-module.exports = patchedTestHelper;
+module.exports = ozTestHelper;
