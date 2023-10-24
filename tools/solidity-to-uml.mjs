5385a5dffb446e394c38ea480844949db23b8f3a
Zied <26070035+zguesmi@users.noreply.github.com>
2023-10-24 16:46:02 +0200

    Add generation for specific contracts

--- tools/solidity-to-uml.mjs
+++ tools/solidity-to-uml.mjs
@@ -1,39 +1,54 @@
 #!/usr/bin/env zx
 
-// Usage: npm run sol-to-uml
-// For sol2uml documentaiton, see https://github.com/naddison36/sol2uml#usage
+// Usage: "npm run sol-to-uml" or "npx run tools/sol-to-uml.mjs"
+// For sol2uml documentation, see https://github.com/naddison36/sol2uml#usage
+
 
 $.verbose = false // Disable bash commands logging.
 
-
 const rootDir = await $`dirname ${__dirname}`
 
-generateClassDiagramForDirectory('libs')
-generateClassDiagramForDirectory('modules')
-generateClassDiagramForDirectory('registries')
+await generateClassDiagramForDirectory('libs')
+await generateClassDiagramForDirectory('modules')
+await generateClassDiagramForDirectory('registries')
 
-// generate_class_uml_base \
-//     IexecPoco1Delegate,IexecPoco2Delegate \
-//     IexecPocoDelegates # out
-// generate_class_uml_base \
-//     IexecEscrowNativeDelegate,IexecEscrowTokenDelegateKYC,IexecEscrowTokenDelegate,IexecEscrowTokenSwapDelegate \
-//     IexecEscrows # out
+await generateClassDiagramForContracts(
+    [
+        'IexecPoco1Delegate',
+        'IexecPoco2Delegate'
+    ],
+    'IexecPocoDelegates',
+)
+
+await generateClassDiagramForContracts(
+    [
+        'IexecEscrowNativeDelegate',
+        'IexecEscrowTokenDelegateKYC',
+        'IexecEscrowTokenDelegate',
+        'IexecEscrowTokenSwapDelegate'
+    ],
+    'IexecEscrows',
+)
 
 /**
  * Generate UML class diagrams for contracts in a given directory.
- * @param dirName name of the target directory.
+ * @param directory 
  */
-async function generateClassDiagramForDirectory(dirName) {
-    console.log(`Generating class diagram for directory : ${dirName}`);
-    const filename = dirName.replace('/', '-');
-    await $`npx sol2uml class contracts/${dirName}/ -o "${rootDir}/uml/class-uml-dir-${filename}.svg"`
+async function generateClassDiagramForDirectory(directory) {
+    console.log(`Generating class diagram for directory : ${directory}`);
+    const filename = directory.replace('/', '-');
+    await $`npx sol2uml class contracts/${directory}/ -o ${rootDir}/uml/class-uml-dir-${filename}.svg`
 }
 
-// # Generate class UML only connected to set of given contracts
-// generate_class_uml_base() {
-//     base_contracts_to_display=$1
-//     output_filename=$2
-//     # -b, --baseContractNames <value> 
-//     # only output contracts connected to these comma separated base contract names
-//     $SOL2UML class . -b "$base_contracts_to_display" -o "$ROOT/uml/class-uml-$output_filename.svg"
-// }
+/**
+ * Generate class UML only connected to set of given contracts
+ * @param contractsList 
+ * @param filename 
+ */
+async function generateClassDiagramForContracts(contractsList, filename) {
+    console.log(`Generating class diagram for contracts : ${contractsList}`);
+    const baseContracts = contractsList.join(','); // => c1,c2,c3
+    // -b, --baseContractNames <value> 
+    // only output contracts connected to these comma separated base contract names
+    await $`npx sol2uml class contracts/ -b ${baseContracts} -o ${rootDir}/uml/class-uml-${filename}.svg`
+}
