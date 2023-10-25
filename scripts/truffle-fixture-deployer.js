a808476d1c5a23106eef2694bb183457c1de88b7
Gabriel Fournier <gabriel.fournier@iex.ec>
2023-10-25 11:32:27 +0200

    update copyright 2023 text to spdx

--- scripts/truffle-fixture-deployer.js
+++ scripts/truffle-fixture-deployer.js
@@ -1,18 +1,4 @@
-/******************************************************************************
- * Copyright 2023 IEXEC BLOCKCHAIN TECH                                       *
- *                                                                            *
- * Licensed under the Apache License, Version 2.0 (the "License");            *
- * you may not use this file except in compliance with the License.           *
- * You may obtain a copy of the License at                                    *
- *                                                                            *
- *     http://www.apache.org/licenses/LICENSE-2.0                             *
- *                                                                            *
- * Unless required by applicable law or agreed to in writing, software        *
- * distributed under the License is distributed on an "AS IS" BASIS,          *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- * See the License for the specific language governing permissions and        *
- * limitations under the License.                                             *
- ******************************************************************************/
+// SPDX-FileCopyrightText: 2023 IEXEC BLOCKCHAIN TECH
 
 const initial_migration = require('../migrations/1_initial_migration.js')
 const deploy_token = require('../migrations/3_deploy_token.js')
