6abaa4599bee88a5a3c902d550731e5ba53a3514
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2023-10-25 11:12:45 +0200

    Apply comment suggestions
    Co-authored-by: Zied Guesmi <26070035+zguesmi@users.noreply.github.com>

--- contracts/external/interfaces/IERC734.sol
+++ contracts/external/interfaces/IERC734.sol
@@ -22,11 +22,12 @@ pragma solidity ^0.8.0;
  * @notice The IERC734 interface of onchain-id requires a static version of solidity:
  * https://github.com/onchain-id/solidity/blob/2.2.0/contracts/interface/IERC734.sol
  * Pragma of the interface is updated here to maintain consistency accross the project.
- * @dev interface of the ERC734 (Key Holder) standard as defined in the EIP.
+ * @dev Relevant part of ERC734 interface (Key Holder) standard as defined in the EIP.
  */
 interface IERC734 {
     /**
-     * @dev Returns TRUE if a key is present and has the given purpose. If the key is not present it returns FALSE.
+     * @dev Returns TRUE if a key is present and has the given purpose. If the key is not
+     * present or does not have the given purpose it returns FALSE.
      */
     function keyHasPurpose(bytes32 key, uint256 purpose) external view returns (bool exists);
 }
