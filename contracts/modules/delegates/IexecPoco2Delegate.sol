617118fd9473271f7fc6ac59530d381f81dc91f4
Zied <26070035+zguesmi@users.noreply.github.com>
2024-03-12 10:12:52 +0100

    Update copyright notice

--- contracts/modules/delegates/IexecPoco2Delegate.sol
+++ contracts/modules/delegates/IexecPoco2Delegate.sol
@@ -1,20 +1,5 @@
+// SPDX-FileCopyrightText: 2020-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
-
-/******************************************************************************
- * Copyright 2020 IEXEC BLOCKCHAIN TECH                                       *
- *                                                                            *
- * Licensed under the Apache License, Version 2.0 (the "License");            *
- * you may not use this file except in compliance with the License.           *
- * You may obtain a copy of the License at                                    *
- *                                                                            *
- *     http://www.apache.org/licenses/LICENSE-2.0                             *
- *                                                                            *
- * Unless required by applicable law or agreed to in writing, software        *
- * distributed under the License is distributed on an "AS IS" BASIS,          *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- * See the License for the specific language governing permissions and        *
- * limitations under the License.                                             *
- ******************************************************************************/
 
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
@@ -54,8 +39,7 @@ contract IexecPoco2Delegate is IexecPoco2, DelegateBase, IexecERC20Core, Signatu
          */
         uint256 kitty = m_frozens[KITTY_ADDRESS];
         if (kitty > 0) {
-            kitty = kitty
-            .percentage(KITTY_RATIO).max(KITTY_MIN).min(kitty); // fraction // at least this // but not more than available
+            kitty = kitty.percentage(KITTY_RATIO).max(KITTY_MIN).min(kitty); // fraction // at least this // but not more than available
             seize(KITTY_ADDRESS, kitty, _taskid);
             reward(deal.workerpool.owner, kitty, _taskid);
         }
