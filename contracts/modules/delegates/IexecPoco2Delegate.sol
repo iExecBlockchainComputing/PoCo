197099cb957bb6d81283a95f51571afbc3b25db7
Zied <26070035+zguesmi@users.noreply.github.com>
2024-04-08 16:32:49 +0200

    Update solc version & remove ABIEncoderV2

--- contracts/modules/delegates/IexecPoco2Delegate.sol
+++ contracts/modules/delegates/IexecPoco2Delegate.sol
@@ -1,8 +1,7 @@
 // SPDX-FileCopyrightText: 2020-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-pragma solidity ^0.6.0;
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.0;
 
 import "@iexec/solidity/contracts/ERC1154/IERC1154.sol";
 import "./IexecERC20Core.sol";
