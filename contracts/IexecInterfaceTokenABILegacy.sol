a0fb56bd889628f57d3bc425b45261e85c470a55
Zied <26070035+zguesmi@users.noreply.github.com>
2024-04-22 17:19:39 +0200

    Format solidity interfaces

--- contracts/IexecInterfaceTokenABILegacy.sol
+++ contracts/IexecInterfaceTokenABILegacy.sol
@@ -1,20 +1,5 @@
+// SPDX-FileCopyrightText: 2020-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
-
-/******************************************************************************
- * Copyright 2020 IEXEC BLOCKCHAIN TECH                                       *
- *                                                                            *
- * Licensed under the Apache License, Version 2.0 (the "License");            *
- * you may not use this file except in compliance with the License.           *
- * You may obtain a copy of the License at                                    *
- *                                                                            *
- *     http://www.apache.org/licenses/LICENSE-2.0                             *
- *                                                                            *
- * Unless required by applicable law or agreed to in writing, software        *
- * distributed under the License is distributed on an "AS IS" BASIS,          *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
- * See the License for the specific language governing permissions and        *
- * limitations under the License.                                             *
- ******************************************************************************/
 
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
@@ -34,23 +19,23 @@ import "./modules/interfaces/IexecRelay.sol";
 import "./modules/interfaces/IexecTokenSpender.sol";
 import "./modules/interfaces/ENSIntegration.sol";
 
-
 interface IexecInterfaceTokenABILegacy is
-	IOwnable,
-	IexecAccessors,
-	IexecAccessorsABILegacy,
-	IexecCategoryManager,
-	IexecERC20,
-	IexecEscrowToken,
-	IexecEscrowTokenSwap,
-	IexecMaintenance,
-	IexecOrderManagement,
-	IexecPoco1,
-	IexecPoco2,
-	IexecRelay,
-	IexecTokenSpender,
-	ENSIntegration
+    IOwnable,
+    IexecAccessors,
+    IexecAccessorsABILegacy,
+    IexecCategoryManager,
+    IexecERC20,
+    IexecEscrowToken,
+    IexecEscrowTokenSwap,
+    IexecMaintenance,
+    IexecOrderManagement,
+    IexecPoco1,
+    IexecPoco2,
+    IexecRelay,
+    IexecTokenSpender,
+    ENSIntegration
 {
-	receive()  external override(IexecEscrowToken, IexecEscrowTokenSwap) payable;
-	fallback() external override(IexecEscrowToken, IexecEscrowTokenSwap) payable;
+    receive() external payable override(IexecEscrowToken, IexecEscrowTokenSwap);
+
+    fallback() external payable override(IexecEscrowToken, IexecEscrowTokenSwap);
 }
