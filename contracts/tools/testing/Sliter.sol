cb813fa8f8980f74dd9127c6318648fc9940190b
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-06-06 16:03:24 +0200

    create slither contract for analyse

---
+++ contracts/tools/testing/Sliter.sol
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+import {IexecPocoAccessorsDelegate} from "../../modules/delegates/IexecPocoAccessorsDelegate.sol";
+import {IexecPoco1Delegate} from "../../modules/delegates/IexecPoco1Delegate.sol";
+import {IexecPoco2Delegate} from "../../modules/delegates/IexecPoco2Delegate.sol";
+
+/**
+ * @notice This contract is dedicated to slither testing.
+ */
+contract AnalyseSlither is IexecPocoAccessorsDelegate, IexecPoco1Delegate, IexecPoco2Delegate {}
