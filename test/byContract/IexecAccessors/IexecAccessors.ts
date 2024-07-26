d5afde94674ab7d2f0cc4a18814bb064b0d15ef8
Zied <26070035+zguesmi@users.noreply.github.com>
2024-07-26 15:21:15 +0200

    Reorder tests according to source file functions order

--- test/byContract/IexecAccessors/IexecAccessors.ts
+++ test/byContract/IexecAccessors/IexecAccessors.ts
@@ -29,87 +29,76 @@ describe('IexecAccessors', async () => {
         iexecPocoAsAnyone = IexecInterfaceNative__factory.connect(proxyAddress, anyone);
     }
 
-    describe('Config', function () {
-        it('token', async function () {
-            expect(await iexecPocoAsAnyone.token()).to.equal(
-                '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-            );
-        });
-        it('teeBroker', async function () {
-            expect(await iexecPocoAsAnyone.teebroker()).to.equal(ethers.constants.AddressZero);
-        });
-        it('callbackGas', async function () {
-            expect(await iexecPocoAsAnyone.callbackgas()).to.equal(100_000n);
-        });
-        it('contributionDeadlineRatio', async function () {
-            expect(await iexecPocoAsAnyone.contribution_deadline_ratio()).to.equal(7);
-        });
-        it('revealDeadlineRatio', async function () {
-            expect(await iexecPocoAsAnyone.reveal_deadline_ratio()).to.equal(2n);
-        });
-        it('finalDeadlineRatio', async function () {
-            expect(await iexecPocoAsAnyone.final_deadline_ratio()).to.equal(10n);
-        });
-        it('workerpoolStakeRatio', async function () {
-            expect(await iexecPocoAsAnyone.workerpool_stake_ratio()).to.equal(30n);
-        });
-        it('kittyRatio', async function () {
-            expect(await iexecPocoAsAnyone.kitty_ratio()).to.equal(10n);
-        });
-        it('kittyMin', async function () {
-            expect(await iexecPocoAsAnyone.kitty_min()).to.equal(1_000_000_000n);
-        });
-        it('kittyAddress', async function () {
-            expect(await iexecPocoAsAnyone.kitty_address()).to.equal(
-                '0x99c2268479b93fDe36232351229815DF80837e23',
-            );
-        });
-        it('groupMemberPurpose', async function () {
-            expect(await iexecPocoAsAnyone.groupmember_purpose()).to.equal(4n);
-        });
-        it('eip712domainSeparator', async function () {
-            expect(await iexecPocoAsAnyone.eip712domain_separator()).to.equal(
-                '0xfc2178d8b8300e657cb9f8b5a4d1957174cf1392e294f3575b82a9cea1da1c4b',
-            );
-        });
+    it('name', async function () {
+        expect(await iexecPocoAsAnyone.name()).to.equal('Staked RLC');
     });
-
-    describe('ERC20', function () {
-        it('name', async function () {
-            expect(await iexecPocoAsAnyone.name()).to.equal('Staked RLC');
-        });
-        it('symbol', async function () {
-            expect(await iexecPocoAsAnyone.symbol()).to.equal('SRLC');
-        });
-        it('decimals', async function () {
-            expect(await iexecPocoAsAnyone.decimals()).to.equal(9n);
-        });
-        it('totalSupply', async function () {
-            expect(await iexecPocoAsAnyone.totalSupply()).to.equal(0n);
-        });
+    it('symbol', async function () {
+        expect(await iexecPocoAsAnyone.symbol()).to.equal('SRLC');
     });
-
-    describe('Categories', function () {
-        it('countCategory', async function () {
-            expect(await iexecPocoAsAnyone.countCategory()).to.equal(5);
-        });
+    it('decimals', async function () {
+        expect(await iexecPocoAsAnyone.decimals()).to.equal(9n);
     });
-
-    describe('Registries', function () {
-        it('appRegistry', async function () {
-            expect(await iexecPocoAsAnyone.appregistry()).to.equal(
-                (await deployments.get('AppRegistry')).address,
-            );
-        });
-        it('datasetRegistry', async function () {
-            expect(await iexecPocoAsAnyone.datasetregistry()).to.equal(
-                (await deployments.get('DatasetRegistry')).address,
-            );
-        });
-        it('workerpoolRegistry', async function () {
-            expect(await iexecPocoAsAnyone.workerpoolregistry()).to.equal(
-                (await deployments.get('WorkerpoolRegistry')).address,
-            );
-        });
+    it('totalSupply', async function () {
+        expect(await iexecPocoAsAnyone.totalSupply()).to.equal(0n);
+    });
+    it('token', async function () {
+        expect(await iexecPocoAsAnyone.token()).to.equal(
+            '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+        );
+    });
+    it('countCategory', async function () {
+        expect(await iexecPocoAsAnyone.countCategory()).to.equal(5);
+    });
+    it('appRegistry', async function () {
+        expect(await iexecPocoAsAnyone.appregistry()).to.equal(
+            (await deployments.get('AppRegistry')).address,
+        );
+    });
+    it('datasetRegistry', async function () {
+        expect(await iexecPocoAsAnyone.datasetregistry()).to.equal(
+            (await deployments.get('DatasetRegistry')).address,
+        );
+    });
+    it('workerpoolRegistry', async function () {
+        expect(await iexecPocoAsAnyone.workerpoolregistry()).to.equal(
+            (await deployments.get('WorkerpoolRegistry')).address,
+        );
+    });
+    it('teeBroker', async function () {
+        expect(await iexecPocoAsAnyone.teebroker()).to.equal(ethers.constants.AddressZero);
+    });
+    it('callbackGas', async function () {
+        expect(await iexecPocoAsAnyone.callbackgas()).to.equal(100_000n);
+    });
+    it('contributionDeadlineRatio', async function () {
+        expect(await iexecPocoAsAnyone.contribution_deadline_ratio()).to.equal(7);
+    });
+    it('revealDeadlineRatio', async function () {
+        expect(await iexecPocoAsAnyone.reveal_deadline_ratio()).to.equal(2n);
+    });
+    it('finalDeadlineRatio', async function () {
+        expect(await iexecPocoAsAnyone.final_deadline_ratio()).to.equal(10n);
+    });
+    it('workerpoolStakeRatio', async function () {
+        expect(await iexecPocoAsAnyone.workerpool_stake_ratio()).to.equal(30n);
+    });
+    it('kittyRatio', async function () {
+        expect(await iexecPocoAsAnyone.kitty_ratio()).to.equal(10n);
+    });
+    it('kittyMin', async function () {
+        expect(await iexecPocoAsAnyone.kitty_min()).to.equal(1_000_000_000n);
+    });
+    it('kittyAddress', async function () {
+        expect(await iexecPocoAsAnyone.kitty_address()).to.equal(
+            '0x99c2268479b93fDe36232351229815DF80837e23',
+        );
+    });
+    it('groupMemberPurpose', async function () {
+        expect(await iexecPocoAsAnyone.groupmember_purpose()).to.equal(4n);
+    });
+    it('eip712domainSeparator', async function () {
+        expect(await iexecPocoAsAnyone.eip712domain_separator()).to.equal(
+            '0xfc2178d8b8300e657cb9f8b5a4d1957174cf1392e294f3575b82a9cea1da1c4b',
+        );
     });
 });
