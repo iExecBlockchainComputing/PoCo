7b43a7e0a47a4e15ec08256c56b26eaaf05b3c53
Zied <26070035+zguesmi@users.noreply.github.com>
2024-08-08 19:34:36 +0200

    Deploy ENS in tests

--- scripts/hardhat-fixture-deployer.ts
+++ scripts/hardhat-fixture-deployer.ts
@@ -5,12 +5,14 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { deployments } from 'hardhat';
 const { resetNetworkToInitialState } = require('./common-test-snapshot');
 const deploy = require('../deploy/0_deploy');
+const deployEns = require('../deploy/1_deploy-ens');
 
 // Anonymous functions cannot be used as fixtures, hence we need to wrap body
 // in a method which will be called by `loadFixture`.
 async function resetNetworkAndDeployAllContracts() {
     await resetNetworkToInitialState();
     await deploy();
+    await deployEns();
 }
 
 /**
