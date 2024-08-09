76cf1791784589945c5c2139d38ae93c2f5ade68
Zied <26070035+zguesmi@users.noreply.github.com>
2024-08-09 11:57:18 +0200

    Migrate ens integration tests file

---
+++ test/byContract/ENSIntegration/ENSIntegration.ts
@@ -0,0 +1,114 @@
+// SPDX-FileCopyrightText: 2020-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-License-Identifier: Apache-2.0
+
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { deployments, ethers, expect } from 'hardhat';
+import { loadHardhatFixtureDeployment } from '../../../scripts/hardhat-fixture-deployer';
+import {
+    ENSRegistry,
+    ENSRegistry__factory,
+    IexecInterfaceNative,
+    IexecInterfaceNative__factory,
+    PublicResolver__factory,
+    ReverseRegistrar__factory,
+} from '../../../typechain';
+import { getIexecAccounts } from '../../../utils/poco-tools';
+const CONFIG = require('../../../config/config.json');
+
+describe('ENSIntegration', async () => {
+    let proxyAddress: string;
+    let iexecPoco: IexecInterfaceNative;
+    let iexecPocoAsAdmin: IexecInterfaceNative;
+    let ensRegistry: ENSRegistry;
+    let iexecAdmin: SignerWithAddress;
+    let anyone: SignerWithAddress;
+
+    beforeEach('Deploy', async () => {
+        // Deploy all contracts
+        proxyAddress = await loadHardhatFixtureDeployment();
+        // Initialize test environment
+        await loadFixture(initFixture);
+    });
+
+    async function initFixture() {
+        const accounts = await getIexecAccounts();
+        ({ iexecAdmin, anyone } = accounts);
+        const ensRegistryAddress = (await deployments.get('ENSRegistry')).address;
+        ensRegistry = ENSRegistry__factory.connect(ensRegistryAddress, anyone);
+        iexecPoco = IexecInterfaceNative__factory.connect(proxyAddress, anyone);
+        iexecPocoAsAdmin = iexecPoco.connect(iexecAdmin);
+    }
+
+    describe('Forward resolution', async () => {
+        it('Should resolve initial names', async () => {
+            if (CONFIG.chains.default.asset === 'Token') {
+                expect(await resolve('rlc.iexec.eth')).to.equal(await iexecPoco.token());
+            }
+            expect(await resolve('admin.iexec.eth')).to.equal(iexecAdmin.address);
+            expect(await resolve('core.v5.iexec.eth')).to.equal(proxyAddress);
+            expect(await resolve('apps.v5.iexec.eth')).to.equal(await iexecPoco.appregistry());
+            expect(await resolve('datasets.v5.iexec.eth')).to.equal(
+                await iexecPoco.datasetregistry(),
+            );
+            expect(await resolve('workerpools.v5.iexec.eth')).to.equal(
+                await iexecPoco.workerpoolregistry(),
+            );
+        });
+    });
+
+    describe('Reverse resolution', async () => {
+        it('Should lookup initial addresses', async () => {
+            expect(await lookup(iexecAdmin.address)).to.equal('admin.iexec.eth');
+            expect(await lookup(proxyAddress)).to.equal('core.v5.iexec.eth');
+            expect(await lookup(await iexecPoco.appregistry())).to.equal('apps.v5.iexec.eth');
+            expect(await lookup(await iexecPoco.datasetregistry())).to.equal(
+                'datasets.v5.iexec.eth',
+            );
+            expect(await lookup(await iexecPoco.workerpoolregistry())).to.equal(
+                'workerpools.v5.iexec.eth',
+            );
+        });
+
+        it('Should register reverse resolution name', async () => {
+            const name = 'test.domain.eth';
+            const reverseNameHash = ethers.utils.namehash(
+                `${proxyAddress.substring(2)}.addr.reverse`,
+            );
+            const reverseRootNameHash = ethers.utils.namehash('addr.reverse');
+            const reverseRegistrarAddress = await ensRegistry.owner(reverseRootNameHash);
+            const reverseResolverAddress = await ReverseRegistrar__factory.connect(
+                reverseRegistrarAddress,
+                anyone,
+            ).defaultResolver();
+            const reverseResolver = PublicResolver__factory.connect(reverseResolverAddress, anyone);
+            await expect(iexecPocoAsAdmin.setName(ensRegistry.address, name))
+                .to.emit(reverseResolver, 'NameChanged')
+                .withArgs(reverseNameHash, name);
+            expect(await lookup(proxyAddress)).to.equal(name);
+        });
+
+        it('Should not register reverse resolution name when sender is not the owner', async () => {
+            await expect(
+                iexecPoco.setName(ensRegistry.address, 'some.name.eth'),
+            ).to.be.revertedWith('Ownable: caller is not the owner');
+        });
+    });
+
+    async function resolve(domain: string) {
+        const nameHash = ethers.utils.namehash(domain);
+        const resolver = await getResolver(nameHash);
+        return await resolver['addr(bytes32)'](nameHash);
+    }
+
+    async function lookup(address: string) {
+        const nameHash = ethers.utils.namehash(`${address.substring(2)}.addr.reverse`);
+        const reverseResolver = await getResolver(nameHash);
+        return await reverseResolver.name(nameHash);
+    }
+
+    async function getResolver(nameHash: string) {
+        const resolverAddress = await ensRegistry.resolver(nameHash);
+        return PublicResolver__factory.connect(resolverAddress, anyone);
+    }
+});
