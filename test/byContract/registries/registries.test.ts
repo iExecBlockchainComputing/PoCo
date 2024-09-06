09e25d23f9ec5664004d5a052e7a7659b1286a99
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-09-06 16:07:21 +0200

    migrate registries file test

--- test/byContract/registries/registries.test.ts
+++ test/byContract/registries/registries.test.ts
@@ -1,43 +1,40 @@
 // SPDX-FileCopyrightText: 2020-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
+import { BytesLike } from '@ethersproject/bytes';
+
 import { AddressZero } from '@ethersproject/constants';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import hre, { deployments, expect } from 'hardhat';
+import hre, { deployments, ethers, expect } from 'hardhat';
 import { loadHardhatFixtureDeployment } from '../../../scripts/hardhat-fixture-deployer';
 import {
-    App,
     AppRegistry,
     AppRegistry__factory,
-    Dataset,
     DatasetRegistry,
     DatasetRegistry__factory,
     IexecInterfaceNative,
     IexecInterfaceNative__factory,
-    Workerpool,
+    Ownable__factory,
     WorkerpoolRegistry,
     WorkerpoolRegistry__factory,
 } from '../../../typechain';
+import { FactoryDeployerHelper } from '../../../utils/FactoryDeployerHelper';
 import { getIexecAccounts } from '../../../utils/poco-tools';
-import { IexecWrapper } from '../../utils/IexecWrapper';
-const CONFIG = require('../../../config/config.json');
+const constants = require('../../../utils/constants');
+const randomAddress = () => ethers.Wallet.createRandom().address;
 
 describe('Registries', () => {
     let proxyAddress: string;
     let [iexecPoco, iexecPocoAsAdmin]: IexecInterfaceNative[] = [];
     let [iexecAdmin, appProvider, datasetProvider, scheduler, anyone]: SignerWithAddress[] = [];
-    let iexecWrapper: IexecWrapper;
 
     let ensRegistryAddress: string;
-
     let appRegistry: AppRegistry;
     let datasetRegistry: DatasetRegistry;
     let workerpoolRegistry: WorkerpoolRegistry;
-
-    let Apps: App[];
-    let Datasets: Dataset[];
-    let Workerpools: Workerpool[];
+    let factoryDeployer: FactoryDeployerHelper;
+    let newAppRegistry: AppRegistry;
 
     beforeEach(async () => {
         proxyAddress = await loadHardhatFixtureDeployment();
@@ -63,7 +60,7 @@ describe('Registries', () => {
     }
 
     describe('Registry', () => {
-        it('Should retreive base URI', async () => {
+        it('Should retrieve base URI', async () => {
             const chainId = hre.network.config.chainId;
             expect(await appRegistry.baseURI()).to.equal(
                 `https://nfts-metadata.iex.ec/app/${chainId}/`,
@@ -101,10 +98,260 @@ describe('Registries', () => {
                 workerpoolRegistry.connect(anyone).setBaseURI(`https://new.url.iex.ec/workerpool/`),
             ).to.be.revertedWith('Ownable: caller is not the owner');
         });
+        it('Should not call initialize when user is not the owner', async () => {
+            await expect(appRegistry.connect(anyone).initialize(AddressZero)).to.be.revertedWith(
+                'Ownable: caller is not the owner',
+            );
+            await expect(
+                datasetRegistry.connect(anyone).initialize(AddressZero),
+            ).to.be.revertedWith('Ownable: caller is not the owner');
+            await expect(
+                workerpoolRegistry.connect(anyone).initialize(AddressZero),
+            ).to.be.revertedWith('Ownable: caller is not the owner');
+        });
         it('Should not reinitialize', async () => {
             await expect(appRegistry.initialize(AddressZero)).to.be.revertedWithoutReason();
             await expect(datasetRegistry.initialize(AddressZero)).to.be.revertedWithoutReason();
             await expect(workerpoolRegistry.initialize(AddressZero)).to.be.revertedWithoutReason();
+        });
+    });
+
+    describe('App Registry', () => {
+        const createAppArgs = [
+            `App`,
+            'DOCKER',
+            constants.MULTIADDR_BYTES,
+            ethers.utils.keccak256(ethers.utils.toUtf8Bytes(`Content of my app`)),
+            '0x1234',
+        ] as [string, string, BytesLike, BytesLike, BytesLike];
+
+        it('Should predict the correct address for future app creation', async () => {
+            const code = await appRegistry.proxyCode();
+            const proxyCodeHash = ethers.utils.keccak256(code);
+
+            const iface = new ethers.utils.Interface([
+                'function initialize(string,string,bytes,bytes32,bytes)',
+            ]);
+            const encodedInitializer = iface.encodeFunctionData('initialize', createAppArgs);
+
+            const salt = ethers.utils.solidityKeccak256(
+                ['bytes', 'address'],
+                [encodedInitializer, appProvider.address],
+            );
+
+            const predictedAddress = ethers.utils.getCreate2Address(
+                appRegistry.address,
+                salt,
+                proxyCodeHash,
+            );
+            expect(await appRegistry.predictApp(appProvider.address, ...createAppArgs)).to.equal(
+                predictedAddress,
+            );
+        });
+
+        it('Should create the app', async () => {
+            const predictedAddress = await appRegistry.predictApp(
+                appProvider.address,
+                ...createAppArgs,
+            );
+            await expect(await appRegistry.createApp(appProvider.address, ...createAppArgs))
+                .to.emit(appRegistry, 'Transfer')
+                .withArgs(
+                    AddressZero,
+                    appProvider.address,
+                    ethers.BigNumber.from(predictedAddress).toString(),
+                );
+        });
+
+        it('Should check that a new app is well registered', async () => {
+            const appCreatedAddress = await appRegistry.callStatic.createApp(
+                appProvider.address,
+                ...createAppArgs,
+            );
+            await appRegistry.createApp(appProvider.address, ...createAppArgs);
+            const isRegistered = await appRegistry.isRegistered(appCreatedAddress);
+            expect(isRegistered).to.be.true;
+        });
+        it('Should not have a random address registered in app registry', async () => {
+            expect(await appRegistry.isRegistered(randomAddress())).to.be.false;
+        });
+
+        it('Should not allow creating the same app twice', async () => {
+            await appRegistry.createApp(appProvider.address, ...createAppArgs);
+
+            await expect(
+                appRegistry.createApp(appProvider.address, ...createAppArgs),
+            ).to.be.revertedWith('Create2: Failed on deploy');
+        });
+        it('Should check that a new app is well registered on new app registry', async () => {
+            const salt = ethers.utils.hexZeroPad('0x' + Date.now().toString(), 32);
+            factoryDeployer = new FactoryDeployerHelper(iexecAdmin, salt);
+
+            const transferOwnershipCall = await Ownable__factory.connect(
+                ethers.constants.AddressZero, // any is fine
+                iexecAdmin, // any is fine
+            )
+                .populateTransaction.transferOwnership(iexecAdmin.address)
+                .then((tx) => tx.data)
+                .catch(() => {
+                    throw new Error('Failed to prepare transferOwnership data');
+                });
+
+            const newAppRegistryAddress = await factoryDeployer.deployWithFactory(
+                new AppRegistry__factory(),
+                [],
+                transferOwnershipCall,
+            );
+            newAppRegistry = AppRegistry__factory.connect(newAppRegistryAddress, iexecAdmin);
+            console.log('newAppRegistryAddress =>', newAppRegistryAddress);
+            console.log('appRegistry.address =>', appRegistry.address);
+            const newAppRegistryInitialized = await newAppRegistry.initialized();
+            console.log('newAppRegistryInitialized', newAppRegistryInitialized);
+            await newAppRegistry.initialize(appRegistry.address);
+
+            const appCreatedAddress = await appRegistry.callStatic.createApp(
+                appProvider.address,
+                ...createAppArgs,
+            );
+            await appRegistry.createApp(appProvider.address, ...createAppArgs);
+            const isRegistered = await newAppRegistry.isRegistered(appCreatedAddress);
+            expect(isRegistered).to.be.true;
+        });
+    });
+
+    describe('Dataset Registry', () => {
+        const createDatasetArgs = [
+            `Dataset`,
+            constants.MULTIADDR_BYTES,
+            ethers.utils.keccak256(ethers.utils.toUtf8Bytes(`Content of my dataset`)),
+        ] as [string, BytesLike, BytesLike];
+
+        it('Should predict the correct address for future dataset creation', async () => {
+            const code = await datasetRegistry.proxyCode();
+            const proxyCodeHash = ethers.utils.keccak256(code);
+
+            const iface = new ethers.utils.Interface(['function initialize(string,bytes,bytes32)']);
+            const encodedInitializer = iface.encodeFunctionData('initialize', createDatasetArgs);
+
+            const salt = ethers.utils.solidityKeccak256(
+                ['bytes', 'address'],
+                [encodedInitializer, datasetProvider.address],
+            );
+
+            const predictedAddress = ethers.utils.getCreate2Address(
+                datasetRegistry.address,
+                salt,
+                proxyCodeHash,
+            );
+            expect(
+                await datasetRegistry.predictDataset(datasetProvider.address, ...createDatasetArgs),
+            ).to.equal(predictedAddress);
+        });
+
+        it('Should create the dataset', async () => {
+            const predictedAddress = await datasetRegistry.predictDataset(
+                datasetProvider.address,
+                ...createDatasetArgs,
+            );
+            await expect(
+                await datasetRegistry.createDataset(datasetProvider.address, ...createDatasetArgs),
+            )
+                .to.emit(datasetRegistry, 'Transfer')
+                .withArgs(
+                    AddressZero,
+                    datasetProvider.address,
+                    ethers.BigNumber.from(predictedAddress).toString(),
+                );
+        });
+        it('Should check that a new dataset is well registered', async () => {
+            const datasetCreatedAddress = await datasetRegistry.callStatic.createDataset(
+                datasetProvider.address,
+                ...createDatasetArgs,
+            );
+            await datasetRegistry.createDataset(datasetProvider.address, ...createDatasetArgs);
+            const isRegistered = await datasetRegistry.isRegistered(datasetCreatedAddress);
+            expect(isRegistered).to.be.true;
+        });
+        it('Should not have a random address registered in dataset registry', async () => {
+            expect(await datasetRegistry.isRegistered(randomAddress())).to.be.false;
+        });
+        it('Should not allow creating the same dataset twice', async () => {
+            await datasetRegistry.createDataset(datasetProvider.address, ...createDatasetArgs);
+
+            await expect(
+                datasetRegistry.createDataset(datasetProvider.address, ...createDatasetArgs),
+            ).to.be.revertedWith('Create2: Failed on deploy');
+        });
+    });
+
+    describe('Workerpool Registry', () => {
+        const createWorkerpooltArgs = [`Workerpool description`] as [string];
+
+        it('Should predict the correct address for future workerpool creation', async () => {
+            const proxyCode = await workerpoolRegistry.proxyCode();
+            const proxyCodeHash = ethers.utils.keccak256(proxyCode);
+
+            const iface = new ethers.utils.Interface(['function initialize(string)']);
+            const encodedInitializer = iface.encodeFunctionData(
+                'initialize',
+                createWorkerpooltArgs,
+            );
+
+            const salt = ethers.utils.solidityKeccak256(
+                ['bytes', 'address'],
+                [encodedInitializer, scheduler.address],
+            );
+
+            const expectedAddress = ethers.utils.getCreate2Address(
+                workerpoolRegistry.address,
+                salt,
+                proxyCodeHash,
+            );
+
+            const predictedAddress = await workerpoolRegistry.predictWorkerpool(
+                scheduler.address,
+                ...createWorkerpooltArgs,
+            );
+
+            expect(predictedAddress).to.equal(expectedAddress);
+        });
+
+        it('Should create the workerpool', async () => {
+            const predictedAddress = await workerpoolRegistry.predictWorkerpool(
+                scheduler.address,
+                ...createWorkerpooltArgs,
+            );
+            await expect(
+                await workerpoolRegistry.createWorkerpool(
+                    scheduler.address,
+                    ...createWorkerpooltArgs,
+                ),
+            )
+                .to.emit(workerpoolRegistry, 'Transfer')
+                .withArgs(
+                    AddressZero,
+                    scheduler.address,
+                    ethers.BigNumber.from(predictedAddress).toString(),
+                );
+        });
+        it('Should check that a new workerpool is well registered', async () => {
+            const workerpoolCreatedAddress = await workerpoolRegistry.callStatic.createWorkerpool(
+                scheduler.address,
+                ...createWorkerpooltArgs,
+            );
+            await workerpoolRegistry.createWorkerpool(scheduler.address, ...createWorkerpooltArgs);
+            const isRegistered = await workerpoolRegistry.isRegistered(workerpoolCreatedAddress);
+            expect(isRegistered).to.be.true;
+        });
+        it('Should not have a random address registered in workerpool registry', async () => {
+            expect(await workerpoolRegistry.isRegistered(randomAddress())).to.be.false;
+        });
+        it('Should not allow creating the same workerpool twice', async () => {
+            await workerpoolRegistry.createWorkerpool(scheduler.address, ...createWorkerpooltArgs);
+
+            await expect(
+                workerpoolRegistry.createWorkerpool(scheduler.address, ...createWorkerpooltArgs),
+            ).to.be.revertedWith('Create2: Failed on deploy');
         });
     });
 });
