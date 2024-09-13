4ec05d7bfd7992b0cc0aaf3a91ed5f52626dae58
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-09-13 10:48:43 +0200

    set ens test for all, restrict tests

--- test/byContract/registries/resources.test.ts
+++ test/byContract/registries/resources.test.ts
@@ -74,7 +74,6 @@ describe('Resources', () => {
             ethers.utils.id(`Content of my app`),
             '0x1234',
         ] as [string, string, BytesLike, BytesLike, BytesLike];
-
         beforeEach(async () => {
             const appAddress = await appRegistry.callStatic.createApp(
                 appProvider.address,
@@ -86,46 +85,53 @@ describe('Resources', () => {
             app = App__factory.connect(appAddress, anyone);
         });
 
-        it('Should create an app and verify its details', async () => {
-            expect(await app.registry()).to.equal(appRegistry.address);
-            expect(await app.owner()).to.equal(appProvider.address);
-            expect(await app.m_appName()).to.equal(createAppArgs[0]);
-            expect(await app.m_appType()).to.equal(createAppArgs[1]);
-            expect(await app.m_appMultiaddr()).to.equal(createAppArgs[2]);
-            expect(await app.m_appChecksum()).to.equal(createAppArgs[3]);
-            expect(await app.m_appMREnclave()).to.equal(createAppArgs[4]);
+        describe('creation and initialization', () => {
+            it('Should create an app and verify its details', async () => {
+                expect(await app.registry()).to.equal(appRegistry.address);
+                expect(await app.owner()).to.equal(appProvider.address);
+                expect(await app.m_appName()).to.equal(createAppArgs[0]);
+                expect(await app.m_appType()).to.equal(createAppArgs[1]);
+                expect(await app.m_appMultiaddr()).to.equal(createAppArgs[2]);
+                expect(await app.m_appChecksum()).to.equal(createAppArgs[3]);
+                expect(await app.m_appMREnclave()).to.equal(createAppArgs[4]);
+            });
+
+            it('Should not reinitialize created app', async () => {
+                await expect(app.initialize(...createAppArgs)).to.be.revertedWith(
+                    'already initialized',
+                );
+            });
         });
 
-        it('Should set the ENS name for the app', async () => {
-            const newENSName = 'myApp.eth';
-            const reverseRootNameHash = ethers.utils.namehash('addr.reverse');
-            const reverseRegistrarAddress = await ensRegistry.owner(reverseRootNameHash);
-            const reverseResolverAddress = await ReverseRegistrar__factory.connect(
-                reverseRegistrarAddress,
-                anyone,
-            ).defaultResolver();
-            const reverseResolver = PublicResolver__factory.connect(reverseResolverAddress, anyone);
+        describe('setName', () => {
+            it('Should set name for the ENS reverse registration of the app', async () => {
+                const newENSName = 'myApp.eth';
+                const reverseRootNameHash = ethers.utils.namehash('addr.reverse');
+                const reverseRegistrarAddress = await ensRegistry.owner(reverseRootNameHash);
+                const reverseResolverAddress = await ReverseRegistrar__factory.connect(
+                    reverseRegistrarAddress,
+                    anyone,
+                ).defaultResolver();
+                const reverseResolver = PublicResolver__factory.connect(
+                    reverseResolverAddress,
+                    anyone,
+                );
 
-            await app
-                .connect(appProvider)
-                .setName(ensRegistry.address, newENSName)
-                .then((tx) => tx.wait());
+                await app
+                    .connect(appProvider)
+                    .setName(ensRegistry.address, newENSName)
+                    .then((tx) => tx.wait());
 
-            const nameHash = ethers.utils.namehash(`${app.address.substring(2)}.addr.reverse`);
-            expect(await reverseResolver.name(nameHash)).to.equal(newENSName);
-        });
+                const nameHash = ethers.utils.namehash(`${app.address.substring(2)}.addr.reverse`);
+                expect(await reverseResolver.name(nameHash)).to.equal(newENSName);
+            });
 
-        it('Should not reinitialize created app', async () => {
-            await expect(app.initialize(...createAppArgs)).to.be.revertedWith(
-                'already initialized',
-            );
-        });
-
-        it('Should revert when a non-owner tries to set the ENS name', async () => {
-            const newENSName = 'unauthorized.eth';
-            await expect(app.setName(ensRegistry.address, newENSName)).to.be.revertedWith(
-                'caller is not the owner',
-            );
+            it('Should revert when a non-owner tries to set the ENS name', async () => {
+                const newENSName = 'unauthorized.eth';
+                await expect(app.setName(ensRegistry.address, newENSName)).to.be.revertedWith(
+                    'caller is not the owner',
+                );
+            });
         });
     });
 
@@ -135,7 +141,6 @@ describe('Resources', () => {
             MULTIADDR_BYTES,
             ethers.utils.id(`Content of my dataset`),
         ] as [string, BytesLike, BytesLike];
-
         beforeEach(async () => {
             const datasetAddress = await datasetRegistry.callStatic.createDataset(
                 datasetProvider.address,
@@ -147,18 +152,53 @@ describe('Resources', () => {
             dataset = Dataset__factory.connect(datasetAddress, anyone);
         });
 
-        it('Should create a dataset and verify its details', async () => {
-            expect(await dataset.registry()).to.equal(datasetRegistry.address);
-            expect(await dataset.owner()).to.equal(datasetProvider.address);
-            expect(await dataset.m_datasetName()).to.equal(createDatasetArgs[0]);
-            expect(await dataset.m_datasetMultiaddr()).to.equal(createDatasetArgs[1]);
-            expect(await dataset.m_datasetChecksum()).to.equal(createDatasetArgs[2]);
+        describe('creation and initialization', () => {
+            it('Should create a dataset and verify its details', async () => {
+                expect(await dataset.registry()).to.equal(datasetRegistry.address);
+                expect(await dataset.owner()).to.equal(datasetProvider.address);
+                expect(await dataset.m_datasetName()).to.equal(createDatasetArgs[0]);
+                expect(await dataset.m_datasetMultiaddr()).to.equal(createDatasetArgs[1]);
+                expect(await dataset.m_datasetChecksum()).to.equal(createDatasetArgs[2]);
+            });
+
+            it('Should not reinitialize created dataset', async () => {
+                await expect(dataset.initialize(...createDatasetArgs)).to.be.revertedWith(
+                    'already initialized',
+                );
+            });
         });
 
-        it('Should not reinitialize created dataset', async () => {
-            await expect(dataset.initialize(...createDatasetArgs)).to.be.revertedWith(
-                'already initialized',
-            );
+        describe('setName', () => {
+            it('Should set name for the ENS reverse registration of the dataset', async () => {
+                const newENSName = 'myDataset.eth';
+                const reverseRootNameHash = ethers.utils.namehash('addr.reverse');
+                const reverseRegistrarAddress = await ensRegistry.owner(reverseRootNameHash);
+                const reverseResolverAddress = await ReverseRegistrar__factory.connect(
+                    reverseRegistrarAddress,
+                    anyone,
+                ).defaultResolver();
+                const reverseResolver = PublicResolver__factory.connect(
+                    reverseResolverAddress,
+                    anyone,
+                );
+
+                await dataset
+                    .connect(datasetProvider)
+                    .setName(ensRegistry.address, newENSName)
+                    .then((tx) => tx.wait());
+
+                const nameHash = ethers.utils.namehash(
+                    `${dataset.address.substring(2)}.addr.reverse`,
+                );
+                expect(await reverseResolver.name(nameHash)).to.equal(newENSName);
+            });
+
+            it('Should revert when a non-owner tries to set the ENS name', async () => {
+                const newENSName = 'unauthorized.eth';
+                await expect(dataset.setName(ensRegistry.address, newENSName)).to.be.revertedWith(
+                    'caller is not the owner',
+                );
+            });
         });
     });
 
@@ -175,50 +215,91 @@ describe('Resources', () => {
             workerpool = Workerpool__factory.connect(workerpoolAddress, anyone);
         });
 
-        it('Should create a workerpool and verify its details', async () => {
-            expect(await workerpool.registry()).to.equal(workerpoolRegistry.address);
-            expect(await workerpool.owner()).to.equal(scheduler.address);
-            expect(await workerpool.m_workerpoolDescription()).to.equal(createWorkerpoolArgs[0]);
-            expect(await workerpool.m_workerStakeRatioPolicy()).to.equal(30);
-            expect(await workerpool.m_schedulerRewardRatioPolicy()).to.equal(1);
+        describe('creation and initialization', () => {
+            it('Should create a workerpool and verify its details', async () => {
+                expect(await workerpool.registry()).to.equal(workerpoolRegistry.address);
+                expect(await workerpool.owner()).to.equal(scheduler.address);
+                expect(await workerpool.m_workerpoolDescription()).to.equal(
+                    createWorkerpoolArgs[0],
+                );
+                expect(await workerpool.m_workerStakeRatioPolicy()).to.equal(30);
+                expect(await workerpool.m_schedulerRewardRatioPolicy()).to.equal(1);
+            });
+
+            it('Should not reinitialize created workerpool', async () => {
+                await expect(workerpool.initialize(...createWorkerpoolArgs)).to.be.revertedWith(
+                    'already initialized',
+                );
+            });
         });
 
-        it('Should allow the owner to configure the workerpool', async () => {
-            const previousWorkerStakeRatioPolicy = await workerpool.m_workerStakeRatioPolicy();
-            const previousSchedulerRewardRatioPolicy =
-                await workerpool.m_schedulerRewardRatioPolicy();
-            await expect(
+        describe('setName', () => {
+            it('Should set name for the ENS reverse registration of the workerpool', async () => {
+                const newENSName = 'myWorkerpool.eth';
+                const reverseRootNameHash = ethers.utils.namehash('addr.reverse');
+                const reverseRegistrarAddress = await ensRegistry.owner(reverseRootNameHash);
+                const reverseResolverAddress = await ReverseRegistrar__factory.connect(
+                    reverseRegistrarAddress,
+                    anyone,
+                ).defaultResolver();
+                const reverseResolver = PublicResolver__factory.connect(
+                    reverseResolverAddress,
+                    anyone,
+                );
+
                 await workerpool
                     .connect(scheduler)
-                    .changePolicy(35, 5, { from: scheduler.address }),
-            )
-                .to.emit(workerpool, 'PolicyUpdate')
-                .withArgs(
-                    previousWorkerStakeRatioPolicy,
-                    35,
-                    previousSchedulerRewardRatioPolicy,
-                    5,
+                    .setName(ensRegistry.address, newENSName)
+                    .then((tx) => tx.wait());
+
+                const nameHash = ethers.utils.namehash(
+                    `${workerpool.address.substring(2)}.addr.reverse`,
                 );
-            expect(await workerpool.m_workerStakeRatioPolicy()).to.equal(35);
-            expect(await workerpool.m_schedulerRewardRatioPolicy()).to.equal(5);
+                expect(await reverseResolver.name(nameHash)).to.equal(newENSName);
+            });
+
+            it('Should revert when a non-owner tries to set the ENS name', async () => {
+                const newENSName = 'unauthorized.eth';
+                await expect(
+                    workerpool.setName(ensRegistry.address, newENSName),
+                ).to.be.revertedWith('caller is not the owner');
+            });
         });
 
-        it('Should not reinitialize created workerpool', async () => {
-            await expect(workerpool.initialize(...createWorkerpoolArgs)).to.be.revertedWith(
-                'already initialized',
-            );
-        });
+        describe('changePolicy', () => {
+            it('Should allow the owner to configure the workerpool', async () => {
+                const previousWorkerStakeRatioPolicy = await workerpool.m_workerStakeRatioPolicy();
+                const previousSchedulerRewardRatioPolicy =
+                    await workerpool.m_schedulerRewardRatioPolicy();
+                await expect(
+                    await workerpool
+                        .connect(scheduler)
+                        .changePolicy(35, 5, { from: scheduler.address }),
+                )
+                    .to.emit(workerpool, 'PolicyUpdate')
+                    .withArgs(
+                        previousWorkerStakeRatioPolicy,
+                        35,
+                        previousSchedulerRewardRatioPolicy,
+                        5,
+                    );
+                expect(await workerpool.m_workerStakeRatioPolicy()).to.equal(35);
+                expect(await workerpool.m_schedulerRewardRatioPolicy()).to.equal(5);
+            });
 
-        it('Should reject configuration from non-owner', async () => {
-            await expect(
-                workerpool.changePolicy(0, 0, { from: anyone.address }),
-            ).to.be.revertedWith('caller is not the owner');
-        });
+            it('Should reject configuration from non-owner', async () => {
+                await expect(
+                    workerpool.changePolicy(0, 0, { from: anyone.address }),
+                ).to.be.revertedWith('caller is not the owner');
+            });
 
-        it('Should reject invalid configuration', async () => {
-            await expect(
-                workerpool.connect(scheduler).changePolicy(100, 150, { from: scheduler.address }),
-            ).to.be.revertedWithoutReason();
+            it('Should reject invalid configuration', async () => {
+                await expect(
+                    workerpool
+                        .connect(scheduler)
+                        .changePolicy(100, 150, { from: scheduler.address }),
+                ).to.be.revertedWithoutReason();
+            });
         });
     });
 });
