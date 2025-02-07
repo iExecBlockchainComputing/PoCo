f1efd705c3d1ab708ba72574884cb97208f75606
gfournieriExec <gabriel.fournier@iex.ec>
2025-02-07 16:29:22 +0100

    keep only usefull function in EthersDeployer class

--- utils/FactoryDeployer.ts
+++ utils/FactoryDeployer.ts
@@ -1,12 +1,11 @@
-// SPDX-FileCopyrightText: 2020-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
-
-import { Contract, ethers } from 'ethers';
-import hre from 'hardhat';
 
 import factoryJson from '@amxx/factory/deployments/GenericFactory.json';
 import factoryShanghaiJson from '@amxx/factory/deployments/GenericFactory_shanghai.json';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { Contract, ethers } from 'ethers';
+import hre from 'hardhat';
 import config from '../config/config.json';
 
 interface FactoryConfig {
@@ -15,33 +14,6 @@ interface FactoryConfig {
     cost: string;
     tx: string;
     abi: any[];
-}
-
-interface DeployOptions {
-    libraries?: any[];
-    salt?: string;
-    call?: string;
-    [key: string]: any;
-}
-
-interface Artefact {
-    contractName: string;
-    abi: any[];
-    bytecode: string;
-    at: (address: string) => Promise<any>;
-    setAsDeployed: (instance: any) => void;
-    _hArtifact: {
-        sourceName: string;
-        contractName: string;
-        linkReferences?: {
-            [key: string]: {
-                [key: string]: Array<{
-                    start: number;
-                    length: number;
-                }>;
-            };
-        };
-    };
 }
 
 const FACTORY: FactoryConfig =
@@ -56,10 +28,8 @@ async function waitTx(txPromise: Promise<ethers.ContractTransaction>): Promise<v
 export class EthersDeployer {
     private factory!: Contract;
     private factoryAsPromise: Promise<Contract>;
-    private options: DeployOptions;
 
-    constructor(wallet: SignerWithAddress, options: DeployOptions = {}) {
-        this.options = options;
+    constructor(wallet: SignerWithAddress) {
         this.factoryAsPromise = new Promise(async (resolve, reject) => {
             if ((await wallet.provider!.getCode(FACTORY.address)) !== '0x') {
                 console.debug(`→ Factory is available on this network`);
@@ -83,95 +53,6 @@ export class EthersDeployer {
 
     async ready(): Promise<void> {
         await this.factoryAsPromise;
-    }
-
-    async deploy(artefact: Artefact, ...extra: any[]): Promise<void> {
-        await this.ready();
-        console.log(`[factoryDeployer] ${artefact.contractName}`);
-
-        const constructorABI = artefact.abi.find((e) => e.type === 'constructor');
-        const argsCount = constructorABI ? constructorABI.inputs.length : 0;
-        const args = extra.slice(0, argsCount);
-        const options: DeployOptions = { ...this.options, ...extra[argsCount] };
-
-        let librariesLinkPlaceHolderAndAddress: any[] = [];
-        if (options.libraries) {
-            librariesLinkPlaceHolderAndAddress = await Promise.all(
-                options.libraries.map(async (library) => {
-                    const linkPlaceholder = this.getLinkPlaceholder(library, artefact);
-                    if (linkPlaceholder) {
-                        return {
-                            linkPlaceholder,
-                            address: (await library.deployed()).address,
-                        };
-                    }
-                    return undefined;
-                }),
-            );
-        }
-
-        let coreCode = artefact.bytecode;
-        librariesLinkPlaceHolderAndAddress
-            .filter(
-                (data): data is { linkPlaceholder: string; address: string } => data !== undefined,
-            )
-            .forEach((element) => {
-                coreCode = coreCode.replace(
-                    new RegExp(element.linkPlaceholder, 'g'),
-                    element.address.slice(2).toLowerCase(),
-                );
-            });
-
-        const argsCode = constructorABI
-            ? ethers.utils.defaultAbiCoder
-                  .encode(
-                      constructorABI.inputs.map((e: { type: string }) => e.type),
-                      args,
-                  )
-                  .slice(2)
-            : '';
-
-        const code = coreCode + argsCode;
-        const salt = options.salt || ethers.constants.HashZero;
-        const contractAddress = options.call
-            ? await this.factory.predictAddressWithCall(code, salt, options.call)
-            : await this.factory.predictAddress(code, salt);
-
-        if ((await this.factory.provider.getCode(contractAddress)) === '0x') {
-            console.log(`[factory] Preparing to deploy ${artefact.contractName} ...`);
-            await waitTx(
-                options.call
-                    ? this.factory.createContractAndCall(code, salt, options.call)
-                    : this.factory.createContract(code, salt),
-            );
-            console.log(
-                `[factory] ${artefact.contractName} successfully deployed at ${contractAddress}`,
-            );
-        } else {
-            console.log(
-                `[factory] ${artefact.contractName} already deployed at ${contractAddress}`,
-            );
-        }
-
-        const instance = await artefact.at(contractAddress);
-        artefact.setAsDeployed(instance);
-    }
-
-    getLinkPlaceholder(libraryArtefact: Artefact, contractArtefact: Artefact): string | undefined {
-        const hardhatLibraryArtifact = libraryArtefact._hArtifact;
-        const hardhatContractArtifact = contractArtefact._hArtifact;
-        if (hardhatContractArtifact.linkReferences) {
-            const linkSourceName =
-                hardhatContractArtifact.linkReferences[hardhatLibraryArtifact.sourceName];
-            if (linkSourceName) {
-                const firstLinkData = linkSourceName[hardhatLibraryArtifact.contractName][0];
-                return contractArtefact.bytecode.substr(
-                    firstLinkData.start * 2 + 2,
-                    firstLinkData.length * 2,
-                );
-            }
-        }
-        return undefined;
     }
 }
 
