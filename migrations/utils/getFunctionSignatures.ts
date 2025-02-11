3f13b7303b8d31a817a0138725fe10ba6984054b
gfournieriExec <gabriel.fournier@iex.ec>
2025-02-11 12:21:47 +0100

    removed tools banner

--- migrations/utils/getFunctionSignatures.ts
+++ migrations/utils/getFunctionSignatures.ts
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: 2023-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-/*****************************************************************************
- *                                   Tools                                   *
- *****************************************************************************/
-
 interface AbiParameter {
     type: string;
     components?: AbiParameter[];
