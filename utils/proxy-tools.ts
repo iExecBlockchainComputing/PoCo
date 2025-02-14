576ca71c18d966464e3c7e5719dfda3e94bc95d9
Le-Caignec <robinlecaignec@yahoo.fr>
2025-02-14 16:44:30 +0100

    rollback on proxy-tools.ts

--- utils/proxy-tools.ts
+++ utils/proxy-tools.ts
@@ -1,8 +1,13 @@
-// SPDX-FileCopyrightText: 2024-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
 import { ERC1538Update } from '../typechain';
 import { getBaseNameFromContractFactory } from '../utils/deploy-tools';
+
+interface AbiParameter {
+    type: string;
+    components?: AbiParameter[];
+}
 
 /**
  * Link a contract to an ERC1538 proxy.
@@ -19,11 +24,34 @@ export async function linkContractToProxy(
     await proxy
         .updateContract(
             contractAddress,
-            contractFactory.interface.functions,
+            // TODO: Use contractFactory.interface.functions when moving to ethers@v6
+            // https://github.com/ethers-io/ethers.js/issues/1069
+            getFunctionSignatures(contractFactory.constructor.abi),
             'Linking ' + contractName,
         )
         .then((tx) => tx.wait())
         .catch(() => {
             throw new Error(`Failed to link ${contractName}`);
         });
+}
+
+function getSerializedObject(entry: AbiParameter): string {
+    return entry.type === 'tuple'
+        ? `(${entry.components?.map(getSerializedObject).join(',') ?? ''})`
+        : entry.type;
+}
+
+function getFunctionSignatures(abi: any[]): string {
+    return [
+        ...abi.filter((entry) => entry.type === 'receive').map(() => 'receive;'),
+        ...abi.filter((entry) => entry.type === 'fallback').map(() => 'fallback;'),
+        ...abi
+            .filter((entry) => entry.type === 'function')
+            .map(
+                (entry) =>
+                    `${entry.name}(${entry.inputs?.map(getSerializedObject).join(',') ?? ''});`,
+            ),
+    ]
+        .filter(Boolean)
+        .join('');
 }
