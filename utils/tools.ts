264b87aa6a0a4d7a7ef89305caced71e66212cb5
Le-Caignec <robinlecaignec@yahoo.fr>
2025-02-18 13:16:30 +0100

    fix compactSignature

--- utils/tools.ts
+++ utils/tools.ts
@@ -1,14 +1,11 @@
 // SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-import { toBeHex, type BigNumberish } from 'ethers';
+import { Signature, toBeHex, type BigNumberish } from 'ethers';
 import { ethers } from 'hardhat';
 
 export function compactSignature(signature: string): string {
     return Signature.from(signature).compactSerialized;
-        vs[0] |= 0x80;
-    }
-    return ethers.toBeHex(ethers.concat([split.r, vs]));
 }
 
 export function BN2Address(bignumber: BigNumberish) {
