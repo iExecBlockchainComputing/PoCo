bf68e03c516bfd3a43fc02cb2bfbc509f5e40615
Le-Caignec <robinlecaignec@yahoo.fr>
2025-02-18 13:37:17 +0100

    refactor: rename BN2Address to bigintToAddress and update parameter type

--- utils/tools.ts
+++ utils/tools.ts
@@ -1,13 +1,13 @@
 // SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-import { Signature, toBeHex, type BigNumberish } from 'ethers';
+import { Signature } from 'ethers';
 import { ethers } from 'hardhat';
 
 export function compactSignature(signature: string): string {
     return Signature.from(signature).compactSerialized;
 }
 
-export function BN2Address(bignumber: BigNumberish) {
-    return ethers.getAddress(toBeHex(bignumber));
+export function bigintToAddress(bigint: bigint) {
+    return ethers.getAddress(ethers.toBeHex(bigint));
 }
