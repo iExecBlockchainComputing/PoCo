d69ce1d2ad3f6e9cdd83064a9e70ff84e7f4c569
Zied <26070035+zguesmi@users.noreply.github.com>
2025-02-20 18:14:08 +0100

    Update copyright dates

--- test/byContract/IexecEscrow/IexecEscrowToken.test.ts
+++ test/byContract/IexecEscrow/IexecEscrowToken.test.ts
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-FileCopyrightText: 2024-2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
 import { AddressZero } from '@ethersproject/constants';
