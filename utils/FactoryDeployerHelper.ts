a7a8ecea3c1b99798b44b0683c3d01fa29ec9057
Zied <26070035+zguesmi@users.noreply.github.com>
2025-02-21 18:35:04 +0100

    Refactor Factory deployer

--- utils/FactoryDeployerHelper.ts
+++ utils/FactoryDeployerHelper.ts
@@ -1,31 +1,23 @@
 // SPDX-FileCopyrightText: 2024-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
+import factoryJson from '@amxx/factory/deployments/GenericFactory.json';
+import factoryShanghaiJson from '@amxx/factory/deployments/GenericFactory_shanghai.json';
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
 import { ContractFactory } from 'ethers';
-import { deployments, ethers } from 'hardhat';
+import hre, { deployments, ethers } from 'hardhat';
 import { GenericFactory, GenericFactory__factory } from '../typechain';
+import config from './config';
 import { getBaseNameFromContractFactory } from './deploy-tools';
-const { EthersDeployer: Deployer, factoryAddress } = require('../utils/FactoryDeployer');
-
-// TODO merge FactoryDeployer and FactoryDeployerHelper here.
-// Use:
-// async getFactory() {
-//     if (!this.genericFactory) {
-//         await this.ready();
-//     }
-//     return this.genericFactory;
-// }
 
 export class FactoryDeployerHelper {
+    owner: SignerWithAddress;
     salt: string;
-    init: any;
-    genericFactory: GenericFactory;
+    genericFactory!: GenericFactory;
 
     constructor(owner: SignerWithAddress, salt: string) {
+        this.owner = owner;
         this.salt = salt;
-        this.init = new Deployer(owner);
-        this.genericFactory = GenericFactory__factory.connect(factoryAddress, owner);
     }
 
     /**
@@ -36,7 +28,7 @@ export class FactoryDeployerHelper {
         constructorArgs?: any[],
         call?: string,
     ) {
-        await this.init.ready(); // Deploy GenericFactory if not already done
+        await this.init();
         let bytecode = (await contractFactory.getDeployTransaction(...(constructorArgs ?? [])))
             .data;
         if (!bytecode) {
@@ -63,7 +55,49 @@ export class FactoryDeployerHelper {
             // abi field is not used but is a required arg. Empty abi would be fine
             abi: (contractFactory as any).constructor.abi,
             address: contractAddress,
+            bytecode: bytecode,
+            args: constructorArgs,
         });
         return contractAddress;
     }
+
+    private async init() {
+        if (this.genericFactory) {
+            // Already initialized.
+            return;
+        }
+        const factoryConfig: FactoryConfig =
+            !config.isNativeChain() && hre.network.name.includes('hardhat')
+                ? factoryShanghaiJson
+                : factoryJson;
+        this.genericFactory = GenericFactory__factory.connect(factoryConfig.address, this.owner);
+        if ((await this.owner.provider!.getCode(factoryConfig.address)) !== '0x') {
+            console.log(`→ Factory is available on this network`);
+            return;
+        }
+        try {
+            console.log(`→ Factory is not yet deployed on this network`);
+            await this.owner
+                .sendTransaction({
+                    to: factoryConfig.deployer,
+                    value: factoryConfig.cost,
+                })
+                .then((tx) => tx.wait());
+            await this.owner.provider
+                .broadcastTransaction(factoryConfig.tx)
+                .then((tx) => tx.wait());
+            console.log(`→ Factory successfully deployed`);
+        } catch (e) {
+            console.log(e);
+            throw new Error('→ Error deploying the factory');
+        }
+    }
+}
+
+interface FactoryConfig {
+    address: string;
+    deployer: string;
+    cost: string;
+    tx: string;
+    abi: any[];
 }
