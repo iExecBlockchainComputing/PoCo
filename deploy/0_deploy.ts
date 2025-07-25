40c7b7fffb92949085d28b048558a0fe9bf9c769
Zied <26070035+zguesmi@users.noreply.github.com>
2025-07-25 15:19:48 +0200

    Test deployment

--- deploy/0_deploy.ts
+++ deploy/0_deploy.ts
@@ -58,158 +58,158 @@ export default async function deploy() {
     const [owner] = await ethers.getSigners();
     const deploymentOptions = config.getChainConfigOrDefault(chainId);
     const factoryDeployer = new FactoryDeployer(owner, chainId);
-    // Deploy RLC
-    const isTokenMode = !config.isNativeChain(deploymentOptions);
-    let rlcInstanceAddress = isTokenMode
-        ? await getOrDeployRlc(deploymentOptions.token!, owner) // token
-        : ZeroAddress; // native
-    console.log(`RLC: ${rlcInstanceAddress}`);
+    // // Deploy RLC
+    // const isTokenMode = !config.isNativeChain(deploymentOptions);
+    // let rlcInstanceAddress = isTokenMode
+    //     ? await getOrDeployRlc(deploymentOptions.token!, owner) // token
+    //     : ZeroAddress; // native
+    // console.log(`RLC: ${rlcInstanceAddress}`);
     // Deploy ERC1538 proxy contracts
     const erc1538UpdateAddress = await factoryDeployer.deployContract(
         new ERC1538UpdateDelegate__factory(),
     );
-    const transferOwnershipCall = await Ownable__factory.connect(
-        ZeroAddress, // any is fine
-        owner, // any is fine
-    )
-        .transferOwnership.populateTransaction(owner.address)
-        .then((tx) => tx.data)
-        .catch(() => {
-            throw new Error('Failed to prepare transferOwnership data');
-        });
-    const erc1538ProxyAddress = await factoryDeployer.deployContract(
-        new ERC1538Proxy__factory(),
-        [erc1538UpdateAddress],
-        transferOwnershipCall,
-    );
-    const erc1538: ERC1538Update = ERC1538Update__factory.connect(erc1538ProxyAddress, owner);
-    console.log(`IexecInstance found at address: ${await erc1538.getAddress()}`);
-    // Deploy library & modules
-    const iexecLibOrdersAddress = await factoryDeployer.deployContract(
-        new IexecLibOrders_v5__factory(),
-    );
-    const iexecLibOrders = {
-        ['contracts/libs/IexecLibOrders_v5.sol:IexecLibOrders_v5']: iexecLibOrdersAddress,
-    };
-    const modules = [
-        new ERC1538QueryDelegate__factory(),
-        new IexecAccessorsDelegate__factory(),
-        new IexecAccessorsABILegacyDelegate__factory(),
-        new IexecCategoryManagerDelegate__factory(),
-        new IexecERC20Delegate__factory(),
-        isTokenMode
-            ? new IexecEscrowTokenDelegate__factory()
-            : new IexecEscrowNativeDelegate__factory(),
-        new IexecMaintenanceDelegate__factory(iexecLibOrders),
-        new IexecOrderManagementDelegate__factory(iexecLibOrders),
-        new IexecPoco1Delegate__factory(iexecLibOrders),
-        new IexecPoco2Delegate__factory(),
-        new IexecRelayDelegate__factory(),
-        new ENSIntegrationDelegate__factory(),
-        new IexecMaintenanceExtraDelegate__factory(),
-        new IexecPocoAccessorsDelegate__factory(iexecLibOrders),
-        new IexecPocoBoostDelegate__factory(iexecLibOrders),
-        new IexecPocoBoostAccessorsDelegate__factory(),
-    ];
-    for (const module of modules) {
-        const address = await factoryDeployer.deployContract(module);
-        await linkContractToProxy(erc1538, address, module);
-    }
-    // Verify linking on ERC1538Proxy
-    const erc1538QueryInstance: ERC1538Query = ERC1538Query__factory.connect(
-        erc1538ProxyAddress,
-        owner,
-    );
-    const functionCount = await erc1538QueryInstance.totalFunctions();
-    console.log(`The deployed ERC1538Proxy now supports ${functionCount} functions:`);
-    for (let i = 0; i < Number(functionCount); i++) {
-        const [method, , contract] = await erc1538QueryInstance.functionByIndex(i);
-        console.log(`[${i}] ${contract} ${method}`);
-    }
-    const appRegistryAddress = await factoryDeployer.deployContract(
-        new AppRegistry__factory(),
-        [],
-        transferOwnershipCall,
-    );
-    const datasetRegistryAddress = await factoryDeployer.deployContract(
-        new DatasetRegistry__factory(),
-        [],
-        transferOwnershipCall,
-    );
-    const workerpoolRegistryAddress = await factoryDeployer.deployContract(
-        new WorkerpoolRegistry__factory(),
-        [],
-        transferOwnershipCall,
-    );
+    // const transferOwnershipCall = await Ownable__factory.connect(
+    //     ZeroAddress, // any is fine
+    //     owner, // any is fine
+    // )
+    //     .transferOwnership.populateTransaction(owner.address)
+    //     .then((tx) => tx.data)
+    //     .catch(() => {
+    //         throw new Error('Failed to prepare transferOwnership data');
+    //     });
+    // const erc1538ProxyAddress = await factoryDeployer.deployContract(
+    //     new ERC1538Proxy__factory(),
+    //     [erc1538UpdateAddress],
+    //     transferOwnershipCall,
+    // );
+    // const erc1538: ERC1538Update = ERC1538Update__factory.connect(erc1538ProxyAddress, owner);
+    // console.log(`IexecInstance found at address: ${await erc1538.getAddress()}`);
+    // // Deploy library & modules
+    // const iexecLibOrdersAddress = await factoryDeployer.deployContract(
+    //     new IexecLibOrders_v5__factory(),
+    // );
+    // const iexecLibOrders = {
+    //     ['contracts/libs/IexecLibOrders_v5.sol:IexecLibOrders_v5']: iexecLibOrdersAddress,
+    // };
+    // const modules = [
+    //     new ERC1538QueryDelegate__factory(),
+    //     new IexecAccessorsDelegate__factory(),
+    //     new IexecAccessorsABILegacyDelegate__factory(),
+    //     new IexecCategoryManagerDelegate__factory(),
+    //     new IexecERC20Delegate__factory(),
+    //     isTokenMode
+    //         ? new IexecEscrowTokenDelegate__factory()
+    //         : new IexecEscrowNativeDelegate__factory(),
+    //     new IexecMaintenanceDelegate__factory(iexecLibOrders),
+    //     new IexecOrderManagementDelegate__factory(iexecLibOrders),
+    //     new IexecPoco1Delegate__factory(iexecLibOrders),
+    //     new IexecPoco2Delegate__factory(),
+    //     new IexecRelayDelegate__factory(),
+    //     new ENSIntegrationDelegate__factory(),
+    //     new IexecMaintenanceExtraDelegate__factory(),
+    //     new IexecPocoAccessorsDelegate__factory(iexecLibOrders),
+    //     new IexecPocoBoostDelegate__factory(iexecLibOrders),
+    //     new IexecPocoBoostAccessorsDelegate__factory(),
+    // ];
+    // for (const module of modules) {
+    //     const address = await factoryDeployer.deployContract(module);
+    //     await linkContractToProxy(erc1538, address, module);
+    // }
+    // // Verify linking on ERC1538Proxy
+    // const erc1538QueryInstance: ERC1538Query = ERC1538Query__factory.connect(
+    //     erc1538ProxyAddress,
+    //     owner,
+    // );
+    // const functionCount = await erc1538QueryInstance.totalFunctions();
+    // console.log(`The deployed ERC1538Proxy now supports ${functionCount} functions:`);
+    // for (let i = 0; i < Number(functionCount); i++) {
+    //     const [method, , contract] = await erc1538QueryInstance.functionByIndex(i);
+    //     console.log(`[${i}] ${contract} ${method}`);
+    // }
+    // const appRegistryAddress = await factoryDeployer.deployContract(
+    //     new AppRegistry__factory(),
+    //     [],
+    //     transferOwnershipCall,
+    // );
+    // const datasetRegistryAddress = await factoryDeployer.deployContract(
+    //     new DatasetRegistry__factory(),
+    //     [],
+    //     transferOwnershipCall,
+    // );
+    // const workerpoolRegistryAddress = await factoryDeployer.deployContract(
+    //     new WorkerpoolRegistry__factory(),
+    //     [],
+    //     transferOwnershipCall,
+    // );
 
-    const appRegistryInstance = AppRegistry__factory.connect(appRegistryAddress, owner);
-    const datasetRegistryInstance = DatasetRegistry__factory.connect(datasetRegistryAddress, owner);
-    const workerpoolRegistryInstance = WorkerpoolRegistry__factory.connect(
-        workerpoolRegistryAddress,
-        owner,
-    );
-    // Base URI configuration from config.json
-    const baseURIApp = config.registriesBaseUri.app;
-    const baseURIDataset = config.registriesBaseUri.dataset;
-    const baseURIWorkerpool = config.registriesBaseUri.workerpool;
-    // Check if registries have been initialized and set base URIs
-    if (!(await appRegistryInstance.initialized())) {
-        await appRegistryInstance
-            .initialize(deploymentOptions.v3.AppRegistry || ZeroAddress)
-            .then((tx) => tx.wait());
-        await appRegistryInstance.setBaseURI(`${baseURIApp}/${chainId}/`).then((tx) => tx.wait());
-    }
-    if (!(await datasetRegistryInstance.initialized())) {
-        await datasetRegistryInstance
-            .initialize(deploymentOptions.v3.DatasetRegistry || ZeroAddress)
-            .then((tx) => tx.wait());
-        await datasetRegistryInstance
-            .setBaseURI(`${baseURIDataset}/${chainId}/`)
-            .then((tx) => tx.wait());
-    }
-    if (!(await workerpoolRegistryInstance.initialized())) {
-        await workerpoolRegistryInstance
-            .initialize(deploymentOptions.v3.WorkerpoolRegistry || ZeroAddress)
-            .then((tx) => tx.wait());
-        await workerpoolRegistryInstance
-            .setBaseURI(`${baseURIWorkerpool}/${chainId}/`)
-            .then((tx) => tx.wait());
-    }
+    // const appRegistryInstance = AppRegistry__factory.connect(appRegistryAddress, owner);
+    // const datasetRegistryInstance = DatasetRegistry__factory.connect(datasetRegistryAddress, owner);
+    // const workerpoolRegistryInstance = WorkerpoolRegistry__factory.connect(
+    //     workerpoolRegistryAddress,
+    //     owner,
+    // );
+    // // Base URI configuration from config.json
+    // const baseURIApp = config.registriesBaseUri.app;
+    // const baseURIDataset = config.registriesBaseUri.dataset;
+    // const baseURIWorkerpool = config.registriesBaseUri.workerpool;
+    // // Check if registries have been initialized and set base URIs
+    // if (!(await appRegistryInstance.initialized())) {
+    //     await appRegistryInstance
+    //         .initialize(deploymentOptions.v3.AppRegistry || ZeroAddress)
+    //         .then((tx) => tx.wait());
+    //     await appRegistryInstance.setBaseURI(`${baseURIApp}/${chainId}/`).then((tx) => tx.wait());
+    // }
+    // if (!(await datasetRegistryInstance.initialized())) {
+    //     await datasetRegistryInstance
+    //         .initialize(deploymentOptions.v3.DatasetRegistry || ZeroAddress)
+    //         .then((tx) => tx.wait());
+    //     await datasetRegistryInstance
+    //         .setBaseURI(`${baseURIDataset}/${chainId}/`)
+    //         .then((tx) => tx.wait());
+    // }
+    // if (!(await workerpoolRegistryInstance.initialized())) {
+    //     await workerpoolRegistryInstance
+    //         .initialize(deploymentOptions.v3.WorkerpoolRegistry || ZeroAddress)
+    //         .then((tx) => tx.wait());
+    //     await workerpoolRegistryInstance
+    //         .setBaseURI(`${baseURIWorkerpool}/${chainId}/`)
+    //         .then((tx) => tx.wait());
+    // }
 
-    // Set main configuration
-    const iexecAccessorsInstance = IexecAccessors__factory.connect(erc1538ProxyAddress, owner);
-    const iexecInitialized = (await iexecAccessorsInstance.eip712domain_separator()) != ZeroHash;
-    if (!iexecInitialized) {
-        await IexecMaintenanceDelegate__factory.connect(erc1538ProxyAddress, owner)
-            .configure(
-                rlcInstanceAddress,
-                'Staked RLC',
-                'SRLC',
-                9, // TODO: generic ?
-                appRegistryAddress,
-                datasetRegistryAddress,
-                workerpoolRegistryAddress,
-                ZeroAddress,
-            )
-            .then((tx) => tx.wait());
-    }
-    // Set categories
-    const catCountBefore = await iexecAccessorsInstance.countCategory();
-    for (let i = Number(catCountBefore); i < config.categories.length; i++) {
-        const category = config.categories[i];
-        await IexecCategoryManager__factory.connect(erc1538ProxyAddress, owner)
-            .createCategory(
-                category.name,
-                JSON.stringify(category.description),
-                category.workClockTimeRef,
-            )
-            .then((tx) => tx.wait());
-    }
-    const catCountAfter = await iexecAccessorsInstance.countCategory();
-    console.log(`countCategory is now: ${catCountAfter} (was ${catCountBefore})`);
-    for (let i = 0; i < Number(catCountAfter); i++) {
-        console.log(`Category ${i}: ${await iexecAccessorsInstance.viewCategory(i)}`);
-    }
+    // // Set main configuration
+    // const iexecAccessorsInstance = IexecAccessors__factory.connect(erc1538ProxyAddress, owner);
+    // const iexecInitialized = (await iexecAccessorsInstance.eip712domain_separator()) != ZeroHash;
+    // if (!iexecInitialized) {
+    //     await IexecMaintenanceDelegate__factory.connect(erc1538ProxyAddress, owner)
+    //         .configure(
+    //             rlcInstanceAddress,
+    //             'Staked RLC',
+    //             'SRLC',
+    //             9, // TODO: generic ?
+    //             appRegistryAddress,
+    //             datasetRegistryAddress,
+    //             workerpoolRegistryAddress,
+    //             ZeroAddress,
+    //         )
+    //         .then((tx) => tx.wait());
+    // }
+    // // Set categories
+    // const catCountBefore = await iexecAccessorsInstance.countCategory();
+    // for (let i = Number(catCountBefore); i < config.categories.length; i++) {
+    //     const category = config.categories[i];
+    //     await IexecCategoryManager__factory.connect(erc1538ProxyAddress, owner)
+    //         .createCategory(
+    //             category.name,
+    //             JSON.stringify(category.description),
+    //             category.workClockTimeRef,
+    //         )
+    //         .then((tx) => tx.wait());
+    // }
+    // const catCountAfter = await iexecAccessorsInstance.countCategory();
+    // console.log(`countCategory is now: ${catCountAfter} (was ${catCountBefore})`);
+    // for (let i = 0; i < Number(catCountAfter); i++) {
+    //     console.log(`Category ${i}: ${await iexecAccessorsInstance.viewCategory(i)}`);
+    // }
 
     if (network.name !== 'hardhat' && network.name !== 'localhost') {
         console.log('Waiting for block explorer to index the contracts...');
