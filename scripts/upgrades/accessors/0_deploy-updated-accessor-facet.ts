8b261fd42e4eec4e60e1e9f5f939fb44673b8281
gfournieriExec <gabriel.fournier@iex.ec>
2025-09-22 09:03:45 +0200

    feat: refactor deployment script for IexecPocoAccessorsFacet

--- scripts/upgrades/accessors/0_deploy-updated-accessor-facet.ts
+++ scripts/upgrades/accessors/0_deploy-updated-accessor-facet.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-import { deployments, ethers } from 'hardhat';
+import { ethers } from 'hardhat';
 import { IexecPocoAccessorsFacet__factory } from '../../../typechain';
 import { FactoryDeployer } from '../../../utils/FactoryDeployer';
 import config from '../../../utils/config';
@@ -25,17 +25,8 @@ import { mineBlockIfOnLocalFork } from '../../../utils/mine';
             deploymentOptions.IexecLibOrders_v5,
     };
 
-    const factory = new IexecPocoAccessorsFacet__factory(iexecLibOrders);
-    const facetAddress = await factoryDeployer.deployContract(factory);
+    const facetFactory = new IexecPocoAccessorsFacet__factory(iexecLibOrders);
+    const facetAddress = await factoryDeployer.deployContract(facetFactory);
 
     console.log(`IexecPocoAccessorsFacet deployed at: ${facetAddress}`);
-
-    // Save deployment for later use
-    await deployments.save('IexecPocoAccessorsFacet', {
-        abi: factory.interface.fragments as any,
-        address: facetAddress,
-        bytecode: factory.bytecode,
-    });
-
-    console.log('Deployment saved as IexecPocoAccessorsFacet');
 })();
