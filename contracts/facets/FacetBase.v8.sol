2aefd8e54d7012b55d7b263126c39f6c07f62c25
gfournieriExec <gabriel.fournier@iex.ec>
2025-09-22 14:55:02 +0200

    refactor: change public constants to internal in FacetBase contract

--- contracts/facets/FacetBase.v8.sol
+++ contracts/facets/FacetBase.v8.sol
@@ -14,20 +14,20 @@ import {PocoStorageLib} from "../libs/PocoStorageLib.v8.sol";
  */
 abstract contract FacetBase {
     // Poco - Constants
-    uint256 public constant CONTRIBUTION_DEADLINE_RATIO = 7;
-    uint256 public constant REVEAL_DEADLINE_RATIO = 2;
-    uint256 public constant FINAL_DEADLINE_RATIO = 10;
-    uint256 public constant WORKERPOOL_STAKE_RATIO = 30;
-    uint256 public constant KITTY_RATIO = 10;
-    uint256 public constant KITTY_MIN = 1e9; // ADJUSTEMENT VARIABLE
+    uint256 internal constant CONTRIBUTION_DEADLINE_RATIO = 7;
+    uint256 internal constant REVEAL_DEADLINE_RATIO = 2;
+    uint256 internal constant FINAL_DEADLINE_RATIO = 10;
+    uint256 internal constant WORKERPOOL_STAKE_RATIO = 30;
+    uint256 internal constant KITTY_RATIO = 10;
+    uint256 internal constant KITTY_MIN = 1e9; // ADJUSTEMENT VARIABLE
 
     // Seized funds of workerpools that do not honor their deals are sent
     // out to this kitty address.
     // It is determined with address(uint256(keccak256(bytes('iExecKitty'))) - 1).
-    address public constant KITTY_ADDRESS = 0x99c2268479b93fDe36232351229815DF80837e23;
+    address internal constant KITTY_ADDRESS = 0x99c2268479b93fDe36232351229815DF80837e23;
 
     // Used with ERC-734 Key Manager identity contract for authorization management.
-    uint256 public constant GROUPMEMBER_PURPOSE = 4;
+    uint256 internal constant GROUPMEMBER_PURPOSE = 4;
 
     modifier onlyOwner() {
         require(_msgSender() == owner(), "Ownable: caller is not the owner");
