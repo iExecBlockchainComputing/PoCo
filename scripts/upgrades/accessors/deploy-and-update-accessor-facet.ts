a5e6e9060759a13b1d23b4f63ccd39d65fdedbaf
gfournieriExec <gabriel.fournier@iex.ec>
2025-09-23 17:42:32 +0200

    refactor: improve logging for diamond functions before upgrade in deploy-and-update-accessor-facet script

--- scripts/upgrades/accessors/deploy-and-update-accessor-facet.ts
+++ scripts/upgrades/accessors/deploy-and-update-accessor-facet.ts
@@ -69,6 +69,9 @@ import { printFunctions } from '../upgrade-helper';
         console.log(`  ${facet.facetAddress}: ${facet.functionSelectors.length} functions`);
     });
 
+    console.log('Diamond functions before upgrade:');
+    await printFunctions(diamondProxyAddress);
+
     const constantFacetAddress = '0x56CDC32332648b1220a89172191798852706EB35'; // Facet providing constant getters (IexecAccessorsABILegacyFacet)
     const oldAccessorFacets = [
         '0xEa232be31ab0112916505Aeb7A2a94b5571DCc6b', //IexecAccessorsFacet
@@ -89,10 +92,6 @@ import { printFunctions } from '../upgrade-helper';
     const constantFunctionsToRemove = constantFunctionSignatures.map((sig) =>
         ethers.id(sig).slice(0, 10),
     );
-
-    console.log('Diamond functions before upgrade:');
-    await printFunctions(diamondProxyAddress);
-
     const removalCuts: IDiamond.FacetCutStruct[] = [];
 
     // Remove ALL functions from the old accessor facets using diamondLoupe.facetFunctionSelectors() except of constant founctions
