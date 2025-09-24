ab33303af62ee422be099cfb423fc7adff7e0abf
gfournieriExec <gabriel.fournier@iex.ec>
2025-09-24 17:46:06 +0200

    refactor: remove unnecessary line breaks in IWorkerpool interface

--- contracts/registries/workerpools/IWorkerpool.v8.sol
+++ contracts/registries/workerpools/IWorkerpool.v8.sol
@@ -5,10 +5,7 @@ pragma solidity ^0.8.0;
 
 interface IWorkerpool {
     function owner() external view returns (address);
-
     function m_workerpoolDescription() external view returns (string memory);
-
     function m_schedulerRewardRatioPolicy() external view returns (uint256);
-
     function m_workerStakeRatioPolicy() external view returns (uint256);
 }
