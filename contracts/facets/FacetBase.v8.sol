01107ef514021c16413d2a2aff8b99a2adbf68de
gfournieriExec <gabriel.fournier@iex.ec>
2025-09-24 15:25:16 +0200

    refactor: remove owner function and onlyOwner modifier in FacetBase contract

--- contracts/facets/FacetBase.v8.sol
+++ contracts/facets/FacetBase.v8.sol
@@ -29,17 +29,6 @@ abstract contract FacetBase {
     // Used with ERC-734 Key Manager identity contract for authorization management.
     uint256 internal constant GROUPMEMBER_PURPOSE = 4;
 
-    modifier onlyOwner() {
-        require(_msgSender() == owner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    function owner() internal view returns (address) {
-        // TODO use LibDiamond.contractOwner() instead of an external call when migrating all contracts to v8.
-        // Make an external call to delegatecall the OwnershipFacet.
-        return IERC5313(address(this)).owner();
-    }
-
     function _msgSender() internal view returns (address) {
         return msg.sender;
     }
