241ddf35dae678ece569dbffa299ed1a9912b986
Zied <26070035+zguesmi@users.noreply.github.com>
2025-10-07 19:00:26 +0200

    refactor: Make v6 interfaces compatible with v8

--- contracts/interfaces/IexecEscrowNative.sol
+++ contracts/interfaces/IexecEscrowNative.sol
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-pragma solidity ^0.6.0;
+pragma solidity >=0.6.0;
 pragma experimental ABIEncoderV2;
 
 interface IexecEscrowNative {
