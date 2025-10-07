0dd2cc2d30f6d5ec25ef55e532396a3638b3a479
Zied <26070035+zguesmi@users.noreply.github.com>
2025-10-07 20:18:09 +0200

    chore: Use compiler v8 for IexecPoco2

--- contracts/interfaces/IexecPoco2.sol
+++ contracts/interfaces/IexecPoco2.sol
@@ -1,8 +1,7 @@
 // SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-pragma solidity >=0.6.0;
-pragma experimental ABIEncoderV2;
+pragma solidity ^0.8.0;
 
 interface IexecPoco2 {
     // TODO replace `taskid` by `taskId` and update the PoCo subgraph and check
