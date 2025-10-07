d1bf68be6ae40b1b35a49239fe958c30a429fb57
Zied <26070035+zguesmi@users.noreply.github.com>
2025-10-07 21:13:02 +0200

    chore: Clean

--- contracts/IexecInterfaceToken.sol
+++ contracts/IexecInterfaceToken.sol
@@ -25,7 +25,7 @@ import {IOwnable} from "./interfaces/IOwnable.sol";
  * Changing the name or the path would cause a breaking change in the SDK.
  */
 interface IexecInterfaceToken is
-    // TODO: Remove this interface when it's not used in the middelware anymore.
+    // TODO Remove this interface when it's not used in the middelware anymore.
     // https://github.com/iExecBlockchainComputing/iexec-commons-poco/blob/819cd008/generateContractWrappers#L7
     IexecAccessorsABILegacy,
     IexecCategoryManager,
--- contracts/interfaces/IexecPoco1.sol
+++ contracts/interfaces/IexecPoco1.sol
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
 pragma solidity >=0.6.0;
