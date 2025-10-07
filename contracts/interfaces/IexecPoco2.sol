3508af3c6f18e027d06dfe5c5f7e7b6ff59ad7a8
Zied <26070035+zguesmi@users.noreply.github.com>
2025-10-07 19:47:29 +0200

    refactor: Merge interface IexecPoco2 v6 and v8

--- contracts/interfaces/IexecPoco2.sol
+++ contracts/interfaces/IexecPoco2.sol
@@ -5,11 +5,8 @@ pragma solidity >=0.6.0;
 pragma experimental ABIEncoderV2;
 
 interface IexecPoco2 {
-    event Reward(address owner, uint256 amount, bytes32 ref);
-    event Seize(address owner, uint256 amount, bytes32 ref);
-    event Lock(address owner, uint256 amount);
-    event Unlock(address owner, uint256 amount);
-
+    // TODO replace `taskid` by `taskId` and update the PoCo subgraph and check
+    // the impact on SDKs and Middelware.
     event AccurateContribution(address indexed worker, bytes32 indexed taskid);
     event FaultyContribution(address indexed worker, bytes32 indexed taskid);
 
@@ -17,45 +14,52 @@ interface IexecPoco2 {
     event TaskContribute(bytes32 indexed taskid, address indexed worker, bytes32 hash);
     event TaskConsensus(bytes32 indexed taskid, bytes32 consensus);
     event TaskReveal(bytes32 indexed taskid, address indexed worker, bytes32 digest);
+    event TaskReopen(bytes32 indexed taskid);
     event TaskFinalize(bytes32 indexed taskid, bytes results);
     event TaskClaimed(bytes32 indexed taskid);
-    event TaskReopen(bytes32 indexed taskid);
 
-    function initialize(bytes32, uint256) external returns (bytes32);
+    function initialize(bytes32 dealId, uint256 index) external returns (bytes32);
 
-    function claim(bytes32) external;
+    function claim(bytes32 taskId) external;
 
     function contribute(
-        bytes32,
-        bytes32,
-        bytes32,
-        address,
-        bytes calldata,
-        bytes calldata
+        bytes32 taskId,
+        bytes32 resultHash,
+        bytes32 resultSeal,
+        address enclaveChallenge,
+        bytes calldata enclaveSign,
+        bytes calldata authorizationSign
     ) external;
 
     function contributeAndFinalize(
-        bytes32,
-        bytes32,
-        bytes calldata,
-        bytes calldata,
-        address,
-        bytes calldata,
-        bytes calldata
+        bytes32 taskId,
+        bytes32 resultDigest,
+        bytes calldata results,
+        bytes calldata resultsCallback, // Expansion - result separation
+        address enclaveChallenge,
+        bytes calldata enclaveSign,
+        bytes calldata authorizationSign
     ) external; // Expansion - result separation
 
-    function reveal(bytes32, bytes32) external;
+    function reveal(bytes32 taskId, bytes32 resultDigest) external;
 
-    function reopen(bytes32) external;
+    function reopen(bytes32 taskId) external;
 
-    function finalize(bytes32, bytes calldata, bytes calldata) external;
+    function finalize(
+        bytes32 taskId,
+        bytes calldata results,
+        bytes calldata resultsCallback
+    ) external;
 
-    function initializeArray(bytes32[] calldata, uint256[] calldata) external returns (bool);
+    function initializeArray(
+        bytes32[] calldata dealIds,
+        uint256[] calldata indexes
+    ) external returns (bool);
 
-    function claimArray(bytes32[] calldata) external returns (bool);
+    function claimArray(bytes32[] calldata taskIds) external returns (bool);
 
     function initializeAndClaimArray(
-        bytes32[] calldata,
-        uint256[] calldata
+        bytes32[] calldata dealIds,
+        uint256[] calldata indexes
     ) external returns (bool);
 }
