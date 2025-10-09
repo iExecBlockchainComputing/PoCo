72a3854158ff31387c4d0902e2d254b892806a6f
Zied Guesmi <26070035+zguesmi@users.noreply.github.com>
2025-10-09 10:34:08 +0200

    feat: Revert if dataset order is not compatible with deal (#278)

---
+++ contracts/interfaces/IexecPoco1Errors.sol
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2020-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+interface IexecPoco1Errors {
+    // TODO put this in `IexecPoco1` when it is migrated to Solidity v8.
+    error IncompatibleDatasetOrder(string reason);
+}
