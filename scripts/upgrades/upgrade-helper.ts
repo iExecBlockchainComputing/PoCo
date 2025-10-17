e81433eff22e84cf94ab996c5faccb29b4a03c77
gfournieriExec <gabriel.fournier@iex.ec>
2025-10-17 15:42:46 +0200

    feat: enhance printFunctions to decode function selectors

--- scripts/upgrades/upgrade-helper.ts
+++ scripts/upgrades/upgrade-helper.ts
@@ -1,10 +1,30 @@
 // SPDX-FileCopyrightText: 2024-2025 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-import { ContractFactory, ZeroAddress } from 'ethers';
+import { ContractFactory, Interface, ZeroAddress } from 'ethers';
 import { ethers } from 'hardhat';
 import { FacetCutAction } from 'hardhat-deploy/dist/types';
-import { DiamondCutFacet__factory, DiamondLoupeFacet__factory } from '../../typechain';
+import {
+    DiamondCutFacet__factory,
+    DiamondLoupeFacet__factory,
+    IexecAccessorsABILegacyFacet__factory,
+    IexecCategoryManagerFacet__factory,
+    IexecConfigurationExtraFacet__factory,
+    IexecConfigurationFacet__factory,
+    IexecERC20Facet__factory,
+    IexecEscrowNativeFacet__factory,
+    IexecEscrowTokenFacet__factory,
+    IexecEscrowTokenSwapFacet__factory,
+    IexecOrderManagementFacet__factory,
+    IexecPoco1Facet__factory,
+    IexecPoco2Facet__factory,
+    IexecPocoAccessorsFacet__factory,
+    IexecPocoBoostAccessorsFacet__factory,
+    IexecPocoBoostFacet__factory,
+    IexecPocoDepositAndMatchTokenFacet__factory,
+    IexecRelayFacet__factory,
+    OwnershipFacet__factory,
+} from '../../typechain';
 import { getFunctionSelectors } from '../../utils/proxy-tools';
 
 function encodeModuleProxyUpdate(contractFactory: ContractFactory, moduleAddress: string) {
@@ -43,6 +63,28 @@ async function printFunctions(diamondProxyAddress: string) {
     );
     const facets = await diamondLoupeInstance.facets();
 
+    const allInterfaces: Interface[] = [
+        OwnershipFacet__factory.createInterface(),
+        DiamondCutFacet__factory.createInterface(),
+        DiamondLoupeFacet__factory.createInterface(),
+        IexecAccessorsABILegacyFacet__factory.createInterface(),
+        IexecCategoryManagerFacet__factory.createInterface(),
+        IexecConfigurationExtraFacet__factory.createInterface(),
+        IexecConfigurationFacet__factory.createInterface(),
+        IexecERC20Facet__factory.createInterface(),
+        IexecEscrowNativeFacet__factory.createInterface(),
+        IexecEscrowTokenFacet__factory.createInterface(),
+        IexecEscrowTokenSwapFacet__factory.createInterface(),
+        IexecOrderManagementFacet__factory.createInterface(),
+        IexecPoco1Facet__factory.createInterface(),
+        IexecPoco2Facet__factory.createInterface(),
+        IexecPocoAccessorsFacet__factory.createInterface(),
+        IexecPocoBoostAccessorsFacet__factory.createInterface(),
+        IexecPocoBoostFacet__factory.createInterface(),
+        IexecPocoDepositAndMatchTokenFacet__factory.createInterface(),
+        IexecRelayFacet__factory.createInterface(),
+    ];
+
     let totalFunctions = 0;
     facets.forEach((facet) => {
         totalFunctions += facet.functionSelectors.length;
@@ -53,10 +95,27 @@ async function printFunctions(diamondProxyAddress: string) {
     let functionIndex = 0;
     for (const facet of facets) {
         for (const selector of facet.functionSelectors) {
-            // Try to decode the selector to a readable function signature
-            // Note: We can't easily get the full function signature from just the selector
-            // This is a limitation compared to ERC1538Query.functionByIndex
-            console.log(`[${functionIndex}] ${facet.facetAddress} ${selector}`);
+            let functionName = selector; // Default to selector if we can't decode
+            // Special cases for special functions
+            if (selector === '0x00000000') {
+                functionName = 'receive()';
+            } else if (selector === '0xffffffff') {
+                functionName = 'fallback()';
+            } else {
+                // Try to find the function in any of the interfaces
+                for (const iface of allInterfaces) {
+                    try {
+                        const fragment = iface.getFunction(selector);
+                        if (fragment) {
+                            functionName = fragment.name;
+                            break;
+                        }
+                    } catch (error) {
+                        // Continue searching in other interfaces
+                    }
+                }
+            }
+            console.log(`[${functionIndex}] ${facet.facetAddress} ${functionName}`);
             functionIndex++;
         }
     }
