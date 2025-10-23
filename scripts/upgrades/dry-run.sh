3d38bf82fb7518eef5487e030742c061872e6f59
Zied <26070035+zguesmi@users.noreply.github.com>
2025-10-23 14:49:56 +0200

    chore: Comment script

--- scripts/upgrades/dry-run.sh
+++ scripts/upgrades/dry-run.sh
@@ -2,8 +2,9 @@
 set -e
 
 # Usage:
-#   ARBITRUM_SEPOLIA_FORK=true UPGRADE_SCRIPT=<script_name> bash ./scripts/upgrades/dry-run.sh
-#   ARBITRUM_FORK=true UPGRADE_SCRIPT=<script_name> bash ./scripts/upgrades/dry-run.sh
+#   ARBITRUM_SEPOLIA_FORK=true \
+#   UPGRADE_SCRIPT=<script_name> \
+#       bash ./scripts/upgrades/dry-run.sh
 
 
 DEPLOYMENTS_FOLDER=''
@@ -12,13 +13,14 @@ if [ "${ARBITRUM_SEPOLIA_FORK}" == "true" ]; then
 elif [ "${ARBITRUM_FORK}" == "true" ]; then
     DEPLOYMENTS_FOLDER=arbitrum
 fi
-
+# Copy the forked network deployments to the hardhat network folder.
 rm -rf deployments/hardhat
 cp -r deployments/${DEPLOYMENTS_FOLDER} deployments/hardhat
+# Stage the old deployments to have a clean diff after the upgrade script run.
 cp .gitignore .gitignore.bak
 sed -i '/deployments\/hardhat/d' .gitignore
 git add deployments/hardhat
+# Run the upgrade and print the git diff.
 npx hardhat run scripts/upgrades/${UPGRADE_SCRIPT} --network hardhat
-# Print the changes made during the dry run
 git --no-pager diff --name-status
 mv .gitignore.bak .gitignore
