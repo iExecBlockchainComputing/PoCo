abfa39ef13006f877b6c145de6acbb27c7262f42
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-30 15:47:51 +0100

    Refactor _burn function to improve readability and maintainability by storing account balance in a variable

--- contracts/facets/IexecERC20Core.sol
+++ contracts/facets/IexecERC20Core.sol
@@ -38,13 +38,14 @@ contract IexecERC20Core is IexecERC20Common, FacetBase {
     function _burn(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: burn from the zero address");
         PocoStorageLib.PocoStorage storage $ = PocoStorageLib.getPocoStorage();
+        uint256 accountBalance = $.m_balances[account];
         // TEMPORARY MIGRATION FIX: Check balance to prevent underflow and revert without reason for backward compatibility
         // TODO: Remove this in the next major version
-        if ($.m_balances[account] < amount) {
+        if (accountBalance < amount) {
             revert();
         }
         $.m_totalSupply = $.m_totalSupply - amount;
-        $.m_balances[account] = $.m_balances[account] - amount;
+        $.m_balances[account] = accountBalance - amount;
         emit Transfer(account, address(0), amount);
     }
 
