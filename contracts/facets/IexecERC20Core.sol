fcbeb750365162863611a531f5aed549a72f3e44
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-30 15:49:39 +0100

    Refactor _transferUnchecked function to improve readability by clarifying balance retrieval

--- contracts/facets/IexecERC20Core.sol
+++ contracts/facets/IexecERC20Core.sol
@@ -12,7 +12,7 @@ contract IexecERC20Core is IexecERC20Common, FacetBase {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
         PocoStorageLib.PocoStorage storage $ = PocoStorageLib.getPocoStorage();
-        uint256 senderBalance = $.m_balances[sender]; // Read balance once
+        uint256 senderBalance = $.m_balances[sender];
         // TEMPORARY MIGRATION FIX: Check balance to prevent underflow and revert without reason for backward compatibility
         // TODO: Remove this in the next major version
         if (senderBalance < amount) {
