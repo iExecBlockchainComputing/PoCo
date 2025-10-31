7102dfad1f5dc5949e80de63a4cdfa0a0745e2e1
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-31 11:54:03 +0100

    chore: update comment for Solidity 0.8.21 in hardhat.config.ts to reflect PoCo contracts and clarify external dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -75,8 +75,8 @@ const v8Settings = {
 const config: HardhatUserConfig = {
     solidity: {
         compilers: [
-            { version: '0.8.21', settings: v8Settings }, // PoCo Boost
-            { version: '0.6.12', settings }, // PoCo contracts
+            { version: '0.8.21', settings: v8Settings }, // PoCo contracts
+            { version: '0.6.12', settings }, // External dependencies (@amxx/factory)
             { version: '0.4.26', settings }, // RLC contracts - '0.4.26' produces a standard ‘ast’ output
         ],
     },
