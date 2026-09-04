149342251fdad51259c645a7ab873839587b470d
Zied <26070035+zguesmi@users.noreply.github.com>
2026-09-04 11:18:30 +0200

    chore: clean facet base

--- contracts/abstract/FacetBase.sol
+++ contracts/abstract/FacetBase.sol
@@ -6,17 +6,11 @@ pragma solidity ^0.8.0;
 import {PocoStorageLib} from "../libs/PocoStorageLib.sol";
 import {IERC5313} from "@openzeppelin/contracts/interfaces/IERC5313.sol";
 
-// TODO use LibDiamond.contractOwner() when migrating all contracts to v8.
-
 /**
  * @title Base contract of all Facet contracts.
- * @dev Every facet must inherit from this contract.
+ * @dev Every facet should inherit from this contract.
  */
 abstract contract FacetBase {
-    // TODO move these constants to a ConstantsLib library and reference them as
-    // ConstantsLib.<NAME> in the next PR.
-    // CommonLib redeclares GROUPMEMBER_PURPOSE temporarily.
-
     // Poco - Constants
     uint256 internal constant CONTRIBUTION_DEADLINE_RATIO = 7;
     uint256 internal constant REVEAL_DEADLINE_RATIO = 2;
